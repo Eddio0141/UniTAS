d80be084f634898a29d9df4f486aebc50735839f
Eddio0141 <eddio0141@gmail.com>
2022-09-12 00:28:09 +0100

    Initial commit

---
+++ UniTASPlugin/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace UniTASPlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
