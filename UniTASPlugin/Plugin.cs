5112056a2722961fcf79ee4b7e540053a01d4a6f
Eddio0141 <eddio0141@gmail.com>
2022-09-12 03:26:54 +0100

    Overridden all legacy input methods

--- UniTASPlugin/Plugin.cs
+++ UniTASPlugin/Plugin.cs
@@ -1,12 +1,36 @@
 ﻿using BepInEx;
+using HarmonyLib;
 
 namespace UniTASPlugin;
 
-[BepInPlugin("____UniTASPlugin", "UniTASPlugin", "1.0.0")]
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    public const string PLUGIN_GUID = "____UniTASPlugin";
+    public const string PLUGIN_NAME = "UniTASPlugin";
+    public const string PLUGIN_VERSION = "1.0.0";
+
+    internal static BepInEx.Logging.ManualLogSource Log;
+
+#pragma warning disable IDE0051
     private void Awake()
+#pragma warning restore IDE0051
     {
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("____UniTASPluginHarmonyPatch");
+        harmony.PatchAll();
+
+        new TAS();
+
+        Log = Logger;
+
+        Logger.LogInfo($"Plugin UniTASPlu is loaded!");
+    }
+
+    private void Update()
+    {
+        if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.Comma))
+        {
+            TAS.Instance.Running = true;
+        }
     }
 }
