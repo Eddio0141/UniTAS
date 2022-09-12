d2bd827955c922046a39049b77f2b871d6077187
Eddio0141 <eddio0141@gmail.com>
2022-09-12 15:40:28 +0100

    RNG TAS patches

---
+++ UniTASPlugin/Patches/RNG/__System/__Random/RNGPatches.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using System;
+using UniTASPlugin.TAS;
+
+namespace UniTASPlugin.Patches.RNG.__System.__Random;
+
+[HarmonyPatch(typeof(Random), MethodType.Constructor, new Type[] { typeof(int) })]
+class Random__Seed
+{
+    static void Prefix(ref int Seed)
+    {
+        if (TASTool.Running)
+            Seed = TASTool.TimeSeed();
+    }
+}
+
+[HarmonyPatch(typeof(Random), "GenerateSeed")]
+class GenerateSeed
+{
+    static bool Prefix(ref int __result)
+    {
+        if (TASTool.Running)
+        {
+            __result = TASTool.TimeSeed();
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(Random), "GenerateGlobalSeed")]
+class GenerateGlobalSeed
+{
+    static bool Prefix(ref int __result)
+    {
+        if (TASTool.Running)
+        {
+            __result = TASTool.TimeSeed();
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ UniTASPlugin/Patches/RNG/__UnityEngine/__Random/RNGPatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UniTASPlugin.TAS;
+using UnityEngine;
+
+namespace UniTASPlugin.Patches.TASInput.__UnityEngine.__Random;
+
+[HarmonyPatch(typeof(Random), nameof(Random.InitState))]
+class InitState
+{
+    static void Prefix(ref int seed)
+    {
+        if (TASTool.Running)
+            seed = TASTool.TimeSeed();
+    }
+}
