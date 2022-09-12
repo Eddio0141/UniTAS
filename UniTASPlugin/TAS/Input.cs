bb033fd015888fc370438c3346ff392ae167e6d8
Eddio0141 <eddio0141@gmail.com>
2022-09-12 15:33:35 +0100

    moved TAS class to folder and split class

---
+++ UniTASPlugin/TAS/Input.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UniTASPlugin.TAS;
+
+public static class Input
+{
+    public static Vector3 mousePosition { get; private set; }
+
+    public static void Update()
+    {
+        // TODO remove this test
+        if (TASTool.Time < 3)
+        {
+            mousePosition = new Vector3();
+        }
+        else if (TASTool.Time < 8)
+        {
+            mousePosition = new Vector3(300, 720);
+        }
+        else
+        {
+            TASTool.Running = false;
+        }
+    }
+}
---
+++ UniTASPlugin/TAS/TASTool.cs
@@ -0,0 +1,49 @@
+﻿namespace UniTASPlugin.TAS;
+
+public static class TASTool
+{
+    private static bool _running;
+    public static bool Running
+    {
+        // TODO private set
+        get => _running; set
+        {
+            if (value)
+            {
+                // TODO set actual framerate
+                UnityEngine.Time.captureFramerate = 1500;
+            }
+            else
+            {
+                UnityEngine.Time.captureFramerate = 0;
+            }
+            _running = value;
+        }
+    }
+    public static double Time { get; private set; }
+
+    static TASTool()
+    {
+        // wait for TAS client to open
+        // set Running depending on this
+
+        Running = true;
+        Time = 0.0;
+    }
+
+    public static void Update(float deltaTime)
+    {
+        Time += deltaTime;
+
+        Input.Update();
+
+        if (!Running)
+            return;
+    }
+
+    public static int TimeSeed()
+    {
+        // TODO: work out seed calculation
+        return (int)(Time * 1000.0);
+    }
+}
