1181d4e4c911f31f5930b0ac2ebf7d4e3854dec5
Eddio0141 <eddio0141@gmail.com>
2022-09-13 21:03:51 +0100

    Removed unused RNG patch

--- UniTASPlugin/Patches/RNG/__System/Random.cs
+++ UniTASPlugin/Patches/RNG/__System/Random.cs
@@ -4,27 +4,14 @@ using UniTASPlugin.TAS;
 
 namespace UniTASPlugin.Patches.RNG.__System;
 
-[HarmonyPatch(typeof(Random), MethodType.Constructor, new Type[] { typeof(int) })]
-class Random__Seed
-{
-    static void Prefix(ref int Seed)
-    {
-        if (TASTool.Running)
-        {
-            Seed = TASTool.TimeSeed();
-            Plugin.Log.LogInfo($"System.Random initialize seed set to {Seed}");
-        }
-    }
-}
-
 [HarmonyPatch(typeof(Random), "GenerateSeed")]
 class GenerateSeed
 {
     static bool Prefix(ref int __result)
     {
-        if (TASTool.Running)
+        if (Main.Running)
         {
-            __result = TASTool.TimeSeed();
+            __result = Main.TimeSeed();
             Plugin.Log.LogInfo($"System.Random.GenerateSeed seed set to {__result}");
 
             return false;
@@ -39,9 +26,9 @@ class GenerateGlobalSeed
 {
     static bool Prefix(ref int __result)
     {
-        if (TASTool.Running)
+        if (Main.Running)
         {
-            __result = TASTool.TimeSeed();
+            __result = Main.TimeSeed();
             Plugin.Log.LogInfo($"System.Random.GenerateGlobalSeed seed set to {__result}");
 
             return false;
