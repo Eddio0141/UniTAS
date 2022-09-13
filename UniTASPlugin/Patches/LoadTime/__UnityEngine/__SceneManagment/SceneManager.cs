5351627fdddeb453212a1d2482812e53d6b167b9
Eddio0141 <eddio0141@gmail.com>
2022-09-13 19:14:44 +0100

    replace all async scene load with non-async

---
+++ UniTASPlugin/Patches/LoadTime/__UnityEngine/__SceneManagment/SceneManager.cs
@@ -0,0 +1,136 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UniTASPlugin.Patches.LoadTime.__UnityEngine.__SceneManagment;
+
+#pragma warning disable Publicizer001
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.LoadSceneAsync), new Type[] { typeof(int), typeof(LoadSceneParameters) })]
+class LoadSceneAsync__sceneBuildIndex__parameters
+{
+    static bool Prefix(int sceneBuildIndex, LoadSceneParameters parameters, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.LoadSceneAsyncNameIndexInternal(null, sceneBuildIndex, parameters, true);
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.LoadSceneAsync), new Type[] { typeof(string), typeof(LoadSceneParameters) })]
+class LoadSceneAsync__sceneName__parameters
+{
+    static bool Prefix(string sceneName, LoadSceneParameters parameters, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.LoadSceneAsyncNameIndexInternal(sceneName, -1, parameters, true);
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.UnloadSceneAsync), new Type[] { typeof(int) })]
+class UnloadSceneAsync__sceneBuildIndex
+{
+    static bool Prefix(int sceneBuildIndex, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.UnloadSceneNameIndexInternal("", sceneBuildIndex, true, UnloadSceneOptions.None, out _);
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.UnloadSceneAsync), new Type[] { typeof(string) })]
+class UnloadSceneAsync__sceneName
+{
+    static bool Prefix(string sceneName, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.UnloadSceneNameIndexInternal(sceneName, -1, true, UnloadSceneOptions.None, out _);
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.UnloadSceneAsync), new Type[] { typeof(Scene) })]
+class UnloadSceneAsync__scene
+{
+    static bool Prefix(ref Scene scene, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.UnloadSceneNameIndexInternal("", scene.buildIndex, true, UnloadSceneOptions.None, out _);
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.UnloadSceneAsync), new Type[] { typeof(int), typeof(UnloadSceneOptions) })]
+class UnloadSceneAsync__sceneBuildIndex__options
+{
+    static bool Prefix(int sceneBuildIndex, UnloadSceneOptions options, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.UnloadSceneNameIndexInternal("", sceneBuildIndex, true, options, out _);
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.UnloadSceneAsync), new Type[] { typeof(string), typeof(UnloadSceneOptions) })]
+class UnloadSceneAsync__sceneName__options
+{
+    static bool Prefix(string sceneName, UnloadSceneOptions options, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.UnloadSceneNameIndexInternal(sceneName, -1, true, options, out _);
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.UnloadSceneAsync), new Type[] { typeof(Scene), typeof(UnloadSceneOptions) })]
+class UnloadSceneAsync__scene__options
+{
+    static bool Prefix(ref Scene scene, UnloadSceneOptions options, ref AsyncOperation __result)
+    {
+        if (TAS.TASTool.Running)
+        {
+            __result = SceneManager.UnloadSceneNameIndexInternal("", scene.buildIndex, true, options, out _);
+
+            return false;
+        }
+
+        return true;
+    }
+}
