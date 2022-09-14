ac7aa84c7ff3cf3cdbf7fc06a679b8f48e0d92e0
Eddio0141 <eddio0141@gmail.com>
2022-09-14 10:23:05 +0100

    Virtual cursor base

---
+++ UniTASPlugin/Patches/VirtualCursor/__UnityEngine/Cursor.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace UniTASPlugin.Patches.VirtualCursor.__UnityEngine;
+
+#pragma warning disable IDE1006
+
+[HarmonyPatch(typeof(Cursor), nameof(Cursor.SetCursor_Injected))]
+class SetCursor_Injected
+{
+    static void Prefix(Texture2D texture, ref Vector2 hotspot)
+    {
+        TAS.Input.VirtualCursor.SetCursor(texture, hotspot);
+    }
+}
+
+[HarmonyPatch(typeof(Cursor), nameof(Cursor.visible), MethodType.Setter)]
+class visibleSetter
+{
+    static void Prefix(bool value)
+    {
+        // we ignore if the run is about to start since we set the visibility off
+        if (TAS.Main.RunInitOrStopping)
+        {
+            return;
+        }
+        TAS.Input.VirtualCursor.Visible = value;
+    }
+}
+
+[HarmonyPatch(typeof(Cursor), nameof(Cursor.visible), MethodType.Getter)]
+class visibleGetter
+{
+    static bool Prefix(ref bool __result)
+    {
+        if (TAS.Main.Running)
+        {
+            // force the hardware cursor to be off while TAS is running
+            __result = false;
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ UniTASPlugin/TAS/Input/Main.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UniTASPlugin.TAS.Input;
+
+public static class Main
+{
+    public static Dictionary<string, float> Axis { get; internal set; }
+
+    static Main()
+    {
+        Axis = new Dictionary<string, float>();
+    }
+
+    public static void Update()
+    {
+        // TODO remove this test
+        if (Axis.Count == 0)
+        {
+            Axis.Add("Mouse X", 0f);
+            Axis.Add("Mouse Y", 0f);
+        }
+        if (TAS.Main.Time < 2)
+        {
+            Mouse.Position = new Vector2(300, 700);
+        }
+        else if (TAS.Main.Time < 3)
+        {
+            Mouse.LeftClick = true;
+        }
+        else if (TAS.Main.Time < 3.1)
+        {
+            Mouse.LeftClick = false;
+        }
+        else if (TAS.Main.Time < 5)
+        {
+            Axis["Mouse X"] = 1f;
+            Axis["Mouse Y"] = 0.2f;
+        }
+        else if (TAS.Main.Time < 6)
+        {
+            Axis["Mouse X"] = -2f;
+            Axis["Mouse Y"] = 0f;
+        }
+        else if (TAS.Main.Time > 8 && TAS.Main.Running)
+        {
+            Plugin.Log.LogDebug("finished");
+            TAS.Main.Running = false;
+        }
+
+        Mouse.Update();
+        VirtualCursor.Update();
+    }
+}
