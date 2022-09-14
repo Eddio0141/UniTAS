2eb7a65638775bfa5e137d168359eed2f77ef7d1
Eddio0141 <eddio0141@gmail.com>
2022-09-14 20:34:13 +0100

    Patching time since startup

---
+++ UniTASPlugin/Patches/Time/__UnityEngine/Time.cs
@@ -0,0 +1,101 @@
+﻿using HarmonyLib;
+
+namespace UniTASPlugin.Patches.Time.__UnityEngine;
+
+#pragma warning disable IDE1006
+
+[HarmonyPatch(typeof(UnityEngine.Time), nameof(UnityEngine.Time.fixedUnscaledTime), MethodType.Getter)]
+class fixedUnscaledTimeGetter
+{
+    static bool Prefix(ref float __result)
+    {
+        if (TAS.Main.Running)
+        {
+            __result = (float)TAS.Main.Time;
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(UnityEngine.Time), nameof(UnityEngine.Time.fixedUnscaledTimeAsDouble), MethodType.Getter)]
+class fixedUnscaledTimeAsDoubleGetter
+{
+    static bool Prefix(ref double __result)
+    {
+        if (TAS.Main.Running)
+        {
+            __result = TAS.Main.Time;
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(UnityEngine.Time), nameof(UnityEngine.Time.frameCount), MethodType.Getter)]
+class frameCountGetter
+{
+    static bool Prefix(ref int __result)
+    {
+        if (TAS.Main.Running)
+        {
+            __result = (int)TAS.Main.FrameCount;
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(UnityEngine.Time), nameof(UnityEngine.Time.renderedFrameCount), MethodType.Getter)]
+class renderedFrameCountGetter
+{
+    static bool Prefix(ref int __result)
+    {
+        if (TAS.Main.Running)
+        {
+            __result = (int)TAS.Main.FrameCount;
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(UnityEngine.Time), nameof(UnityEngine.Time.realtimeSinceStartup), MethodType.Getter)]
+class realtimeSinceStartupGetter
+{
+    static bool Prefix(ref float __result)
+    {
+        if (TAS.Main.Running)
+        {
+            __result = (float)TAS.Main.Time;
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(UnityEngine.Time), nameof(UnityEngine.Time.realtimeSinceStartupAsDouble), MethodType.Getter)]
+class realtimeSinceStartupAsDoubleGetter
+{
+    static bool Prefix(ref double __result)
+    {
+        if (TAS.Main.Running)
+        {
+            __result = TAS.Main.Time;
+
+            return false;
+        }
+
+        return true;
+    }
+}
