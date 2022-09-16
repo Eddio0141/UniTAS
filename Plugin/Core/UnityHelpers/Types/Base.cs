e4719576a1477b42727d8d6c20aa4e61b95c5f08
Eddio0141 <eddio0141@gmail.com>
2022-09-16 17:06:58 +0100

    added InitByUnityVersion method

--- Plugin/Core/UnityHelpers/Types/Base.cs
+++ Plugin/Core/UnityHelpers/Types/Base.cs
@@ -5,11 +5,12 @@ namespace Core.UnityHelpers.Types;
 public abstract class Base
 {
     internal static Type ObjType { get; set; }
-    protected static UnityVersion Version { get; set; }
 
     public virtual void Init(Type objType, UnityVersion version)
     {
         ObjType = objType;
-        Version = version;
+        InitByUnityVersion(objType, version);
     }
+
+    protected abstract void InitByUnityVersion(Type objType, UnityVersion version);
 }
