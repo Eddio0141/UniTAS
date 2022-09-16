b9c74a503f625158aab61554063337142d41fd31
Eddio0141 <eddio0141@gmail.com>
2022-09-16 17:05:08 +0100

    Removed outdated helper

--- Plugin/Core/UnityHelpers/Types/MonoBehavior.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Core.UnityHelpers.Types;
-
-public interface MonoBehavior
-{
-    static void StopAllCoroutines(MonoBehavior instance) => throw new NotImplementedException();
-}
