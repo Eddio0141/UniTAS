6fd9c2d9f8be3e20e7bfc67420c06815b9cdb449
Eddio0141 <eddio0141@gmail.com>
2022-09-16 17:03:05 +0100

    Cursor helper

---
+++ Plugin/Core/UnityHelpers/Types/Cursor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace Core.UnityHelpers.Types;
+
+#pragma warning disable IDE1006
+
+public class Cursor : Base
+{
+    static MethodInfo visibleGetter;
+    static MethodInfo visibleSetter;
+
+    protected override void InitByUnityVersion(Type objType, UnityVersion version)
+    {
+        switch (version)
+        {
+            case UnityVersion.v2021_2_14:
+                visibleGetter = objType.GetMethod("visible", BindingFlags.GetField);
+                visibleSetter = objType.GetMethod("visible", BindingFlags.SetField);
+                break;
+        }
+    }
+
+    public static bool visible
+    {
+        get => (bool)visibleGetter.Invoke(null, new object[] { });
+        set => visibleSetter.Invoke(null, new object[] { value });
+    }
+}
