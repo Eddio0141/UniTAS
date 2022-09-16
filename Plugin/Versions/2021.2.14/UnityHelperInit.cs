bd23cf4b7cca083ae0beee485dc580947ebf188e
Eddio0141 <eddio0141@gmail.com>
2022-09-16 14:23:12 +0100

    Moved irrelevant files to core

--- Plugin/Versions/2021.2.14/UnityHelperInit.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-internal static class UnityHelperInit
-{
-    internal static void Init(Func<Type, object[]> findObjectsOfType, Func<object, int> getInstanceID, Type monoBehavior, Type keyCode, Type object_)
-    {
-        Core.UnityHelpers.Main.Init(findObjectsOfType, getInstanceID, monoBehavior, keyCode, object_);
-    }
-}
