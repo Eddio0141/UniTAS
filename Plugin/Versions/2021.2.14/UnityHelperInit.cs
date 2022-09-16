65bc8c1e819673ae851a53bff85ea0847808c62e
Eddio0141 <eddio0141@gmail.com>
2022-09-16 01:54:38 +0100

    Helper class to wrap unity functions

---
+++ Plugin/Versions/2021.2.14/UnityHelperInit.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+internal static class UnityHelperInit
+{
+    internal static void Init(Func<Type, object[]> findObjectsOfType, Func<object, int> getInstanceID, Type monoBehavior, Type keyCode, Type object_)
+    {
+        Core.UnityHelpers.Main.Init(findObjectsOfType, getInstanceID, monoBehavior, keyCode, object_);
+    }
+}
