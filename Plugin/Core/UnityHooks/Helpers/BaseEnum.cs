46b81d75ee96badee443eb982a3b11decb8574f5
Eddio0141 <eddio0141@gmail.com>
2022-09-17 03:26:36 +0100

    support for multiple enum versions

--- Plugin/Core/UnityHooks/Helpers/BaseEnum.cs
+++ Plugin/Core/UnityHooks/Helpers/BaseEnum.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Core.UnityHooks.Helpers;
 
-internal abstract class BaseEnum<T, E> : Base<T>
+internal abstract class BaseEnum<T> : Base<T>
 {
-    public static Type EnumType { get; protected set; }
+    public static List<Type> EnumTypes { get; protected set; }
 
     internal override void Init(Type objType, UnityVersion version)
     {
@@ -15,12 +16,32 @@ internal abstract class BaseEnum<T, E> : Base<T>
 
     void InitEnumInternal(Type objType)
     {
-        var enumType = typeof(E);
+        EnumTypes = new();
+        var enumTypesWithVersions = GetAllEnumTypes();
 
         var variants = Enum.GetValues(objType);
-        var variantsWrappers = Enum.GetValues(enumType);
+        var variantsWrappers = new List<Type>();
+        foreach (var enumTypeWithVersion in enumTypesWithVersions)
+        {
+            var version = enumTypeWithVersion.Key;
+            var enumType = enumTypeWithVersion.Value;
 
-        if (variants.Length != Enum.GetValues(enumType).Length)
+            // don't allow enum version higher than current version
+            if (version > PluginInfo.UnityVersion)
+            {
+                break;
+            }
+
+            EnumTypes.Add(enumType);
+
+            var enumVariants = Enum.GetValues(enumType);
+            foreach (var enumVariant in enumVariants)
+            {
+                variantsWrappers.Add((Type)enumVariant);
+            }
+        }
+
+        if (variants.Length != variantsWrappers.Count)
         {
             throw new Exception("Type variant count is not equal to dummy enum variant count");
         }
@@ -29,7 +50,7 @@ internal abstract class BaseEnum<T, E> : Base<T>
         for (int i = 0; i < variants.Length; i++)
         {
             var variant = variants.GetValue(i);
-            var variantWrapper = variantsWrappers.GetValue(i);
+            var variantWrapper = variantsWrappers[i];
 
             if (variant.ToString() != variantWrapper.ToString())
             {
@@ -38,12 +59,14 @@ internal abstract class BaseEnum<T, E> : Base<T>
         }
     }
 
+    protected abstract Dictionary<UnityVersion, Type> GetAllEnumTypes();
+
     /// <summary>
     /// Converts dummy enum to original type.
     /// </summary>
     /// <param name="value"></param>
     /// <returns></returns>
-    internal static object To(E value)
+    internal static object To(object value)
     {
         var valueVariants = Enum.GetValues(ObjType);
         var valueString = value.ToString();
@@ -64,17 +87,19 @@ internal abstract class BaseEnum<T, E> : Base<T>
     /// </summary>
     /// <param name="value"></param>
     /// <returns></returns>
-    internal static E From(object value)
+    internal static object From(object value)
     {
-        var valueVariants = Enum.GetValues(typeof(E));
         var valueString = value.ToString();
-
-        // get matching variant
-        foreach (var variant in valueVariants)
+        foreach (var enumType in EnumTypes)
         {
-            if (variant.ToString() == valueString)
+            var valueVariants = Enum.GetValues(enumType);
+            // get matching variant
+            foreach (var variant in valueVariants)
             {
-                return (E)variant;
+                if (variant.ToString() == valueString)
+                {
+                    return variant;
+                }
             }
         }
         throw new InvalidOperationException("Enum variant not found");
