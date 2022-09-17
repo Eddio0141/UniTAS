06679278d8a6d81e62f4d140a5939b4d29276763
Eddio0141 <eddio0141@gmail.com>
2022-09-17 19:22:08 +0100

    add mroe states

--- Plugin/Core/SaveState/Main.cs
+++ Plugin/Core/SaveState/Main.cs
@@ -23,19 +23,25 @@ internal static class Main
         var time = DateTime.Now;
         var frameCount = TAS.Main.FrameCount;
         var fixedUpdateIndex = TAS.Main.FixedUpdateIndex;
+        var cursorVisible = Cursor.visible;
+        var cursorLockState = Cursor.lockState;
+        var saveVersion = PluginInfo.UnityVersion;
 
-        Test = new State(sceneIndex, time, frameCount, fixedUpdateIndex);
+        Test = new State(sceneIndex, time, frameCount, fixedUpdateIndex, cursorVisible, cursorLockState, saveVersion);
+        Log.LogDebug("Saved test state");
     }
 
     public static void Load()
     {
+        Log.LogDebug("We are loading the test state");
         var state = Test;
         pendingLoad = true;
         pendingLoadFixedUpdateIndex = state.FixedUpdateIndex;
         pendingState = state;
+        Log.LogDebug($"Scene: {state.Scene}, Time: {state.Time}, FrameCount: {state.FrameCount}, FixedUpdateIndex: {state.FixedUpdateIndex}");
     }
 
-    public static void FixedUpdate()
+    public static void Update()
     {
         if (pendingLoad && TAS.Main.FixedUpdateIndex == pendingLoadFixedUpdateIndex)
         {
@@ -46,9 +52,12 @@ internal static class Main
 
     public static void LoadOperation()
     {
+        Log.LogDebug("Load operation starting");
         var scene = pendingState.Scene;
         var time = pendingState.Time;
         var frameCount = pendingState.FrameCount;
+        var cursorVisible = pendingState.CursorVisible;
+        var cursorLockState = pendingState.CursorLockState;
 
         if (Scene.buildIndex(SceneManager.GetActiveScene()) != scene)
         {
@@ -57,5 +66,9 @@ internal static class Main
         }
         TAS.Main.Time = time;
         TAS.Main.FrameCount = frameCount;
+        Cursor.visible = cursorVisible;
+        Cursor.lockState = cursorLockState;
+
+        Log.LogDebug($"Load operation finished, time: {DateTime.Now}, frameCount: {TAS.Main.FrameCount}");
     }
 }
--- Plugin/Core/SaveState/State.cs
+++ Plugin/Core/SaveState/State.cs
@@ -4,18 +4,29 @@ namespace Core.SaveState;
 
 internal class State
 {
+    // save state info itself
+    public readonly UnityVersion SaveVersion;
     // scene
     public readonly int Scene;
     // time stuff
     public readonly DateTime Time;
     public readonly ulong FrameCount;
     public readonly int FixedUpdateIndex;
+    // cursor stuff
+    public readonly bool CursorVisible;
+    public readonly UnityHooks.CursorLockModeType CursorLockState;
 
-    public State(int scene, DateTime time, ulong frameCount, int fixedUpdateIndex)
+    public State(int scene, DateTime time, ulong frameCount, int fixedUpdateIndex, bool cursorVisible, UnityHooks.CursorLockModeType cursorLockState, UnityVersion saveVersion)
     {
+        SaveVersion = saveVersion;
+
         Scene = scene;
+
         Time = time;
         FrameCount = frameCount;
         FixedUpdateIndex = fixedUpdateIndex;
+
+        CursorVisible = cursorVisible;
+        CursorLockState = cursorLockState;
     }
 }
