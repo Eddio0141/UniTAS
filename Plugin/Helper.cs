1c649a7d32e6a09b6d85ad077372427c7bcd6755
Eddio0141 <eddio0141@gmail.com>
2022-09-20 00:55:56 +0100

    added new helper

--- Plugin/Helper.cs
+++ Plugin/Helper.cs
@@ -1,4 +1,7 @@
-﻿namespace UniTASPlugin;
+﻿using System.Linq;
+using System.Reflection;
+
+namespace UniTASPlugin;
 
 internal static partial class Helper
 {
@@ -12,5 +15,45 @@ internal static partial class Helper
     public static bool ValueHasDecimalPoints(float value)
     {
         return value.ToString().Contains(".");
+    }
+
+    public static Assembly[] GetGameAssemblies()
+    {
+        var assemblies = System.AppDomain.CurrentDomain.GetAssemblies();
+        var resetIgnoreAssemblies = new string[] {
+            "mscorlib",
+            "BepInEx.Preloader",
+            "BepInEx",
+            "System.Core",
+            "0Harmony",
+            "System",
+            "HarmonyXInterop",
+            "System.Configuration",
+            "System.Xml",
+            "DemystifyExceptions",
+            "StartupProfiler",
+            "Purchasing.Common",
+            "netstandard",
+            "UniTASPlugin",
+        };
+        var resetIgnoreAssmelibes_startsWith = new string[]
+        {
+            "Unity.",
+            "UnityEngine.",
+            "Mono.",
+            "MonoMod.",
+            "HarmonyDTFAssembly",
+        };
+
+        return assemblies.Where((assembly) =>
+        {
+            foreach (var assemblyCheck in resetIgnoreAssmelibes_startsWith)
+                if (assembly.FullName.StartsWith(assemblyCheck))
+                    return false;
+            foreach (var assemblyCheck in resetIgnoreAssemblies)
+                if (assembly.FullName == assemblyCheck)
+                    return false;
+            return true;
+        }).ToArray();
     }
 }
