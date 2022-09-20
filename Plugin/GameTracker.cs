d6544f62b70b6f569a219a26735aad4d5db75444
Eddio0141 <eddio0141@gmail.com>
2022-09-20 23:10:37 +0100

    tracker class for important states

---
+++ Plugin/GameTracker.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniTASPlugin;
+
+public static class GameTracker
+{
+    /// <summary>
+    /// Scene loading count status. 0 means there are no scenes loading, 1 means there is one scene loading, 2 means there are two scenes loading, etc.
+    /// </summary>
+    public static int LoadingSceneCount { get; private set; } = 0;
+    /// <summary>
+    /// Scene unloading count status. 0 means there are no scenes unloading, 1 means there is one scene unloading, 2 means there are two scenes unloading, etc.
+    /// </summary>
+    public static int UnloadingSceneCount { get; private set; } = 0;
+    public static List<int> FirstObjIDs { get; } = new();
+    public static List<int> DontDestroyOnLoadIDs { get; private set; } = new();
+
+    public static void Init()
+    {
+        Object[] objs = Object.FindObjectsOfType(typeof(MonoBehaviour));
+        foreach (Object obj in objs)
+        {
+            int id = obj.GetInstanceID();
+            if (DontDestroyOnLoadIDs.Contains(id))
+            {
+                FirstObjIDs.Add(id);
+            }
+        }
+        var pluginObj = Object.FindObjectOfType(typeof(Plugin));
+        if (pluginObj == null)
+            Plugin.Log.LogError("Plugin object not found, this should never happen");
+        else
+            FirstObjIDs.Add(pluginObj.GetInstanceID());
+    }
+
+    public static void AsyncSceneLoad(AsyncOperation operation)
+    {
+        if (operation == null)
+            return;
+        if (Plugin.Instance == null)
+        {
+            Plugin.Log.LogError("Plugin is null, this should not happen, skipping scene load tracker");
+            LoadingSceneCount = 0;
+            return;
+        }
+        Plugin.Instance.StartCoroutine(AsyncSceneLoadWait(operation));
+    }
+
+    static System.Collections.IEnumerator AsyncSceneLoadWait(AsyncOperation operation)
+    {
+        while (!operation.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        LoadingSceneCount--;
+    }
+
+    public static void AsyncSceneUnload(AsyncOperation operation)
+    {
+        if (operation == null)
+            return;
+        if (Plugin.Instance == null)
+        {
+            Plugin.Log.LogError("Plugin is null, this should not happen, skipping scene unload tracker");
+            UnloadingSceneCount = 0;
+            return;
+        }
+        Plugin.Instance.StartCoroutine(AsyncSceneUnloadWait(operation));
+    }
+
+    static System.Collections.IEnumerator AsyncSceneUnloadWait(AsyncOperation operation)
+    {
+        while (!operation.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        UnloadingSceneCount--;
+    }
+
+    public static void DontDestroyOnLoadCall(Object @object)
+    {
+        if (@object == null)
+            return;
+        var id = @object.GetInstanceID();
+        if (DontDestroyOnLoadIDs.Contains(id))
+            return;
+        DontDestroyOnLoadIDs.Add(id);
+    }
+
+    public static void DestroyObject(Object @object)
+    {
+        if (@object == null)
+            return;
+        DontDestroyOnLoadIDs.Remove(@object.GetInstanceID());
+    }
+}
