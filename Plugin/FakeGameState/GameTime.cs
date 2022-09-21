0baa9ebb09017c085cdbf0fa8fa8501c2fe01bc6
Eddio0141 <eddio0141@gmail.com>
2022-09-22 00:25:52 +0100

    time based Seed return

--- Plugin/FakeGameState/GameTime.cs
+++ Plugin/FakeGameState/GameTime.cs
@@ -4,4 +4,9 @@ internal static class GameTime
 {
     public static System.DateTime Time { get; set; } = System.DateTime.MinValue;
     public static ulong FrameCount { get; set; } = 0;
+
+    public static long Seed()
+    {
+        return Time.Ticks;
+    }
 }
