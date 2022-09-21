ee964aa30e70e116aa349092b90b3a3e25e0f18d
Eddio0141 <eddio0141@gmail.com>
2022-09-21 22:38:49 +0100

    my version of deep clone methods

--- Plugin/Helper.cs
+++ Plugin/Helper.cs
@@ -1,5 +1,10 @@
-﻿using System.Linq;
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading;
 using UniTASPlugin.VersionSafeWrapper;
 
 namespace UniTASPlugin;
@@ -60,7 +65,7 @@ internal static partial class Helper
 
     public static string GameRootDir()
     {
-        return System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+        return AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
     }
 
     public static string GameName()
@@ -72,5 +77,151 @@ internal static partial class Helper
     {
         // TODO other platform support that's not windows
         return System.IO.Path.Combine(GameRootDir(), $"{GameName()}.exe");
+    }
+
+    /// <summary>
+    /// A cache for the <see cref="ICollection{T}.Add"/> or similar Add methods for different types.
+    /// </summary>
+    static readonly Dictionary<Type, FastInvokeHandler> addHandlerCache = new();
+
+    static readonly ReaderWriterLock addHandlerCacheLock = new();
+
+    /// <summary>Makes a deep copy of any object</summary>
+    /// <typeparam name="T">The type of the instance that should be created; for legacy reasons, this must be a class or interface</typeparam>
+    /// <param name="source">The original object</param>
+    /// <returns>A copy of the original object but of type T</returns>
+    ///
+    public static T MakeDeepCopy<T>(object source) where T : class
+    {
+        return MakeDeepCopy(source, typeof(T)) as T;
+    }
+
+    /// <summary>Makes a deep copy of any object</summary>
+    /// <typeparam name="T">The type of the instance that should be created</typeparam>
+    /// <param name="source">The original object</param>
+    /// <param name="result">[out] The copy of the original object</param>
+    /// <param name="processor">Optional value transformation function (taking a field name and src/dst <see cref="Traverse"/> instances)</param>
+    /// <param name="pathRoot">The optional path root to start with</param>
+    ///
+    public static void MakeDeepCopy<T>(object source, out T result, Func<string, Traverse, Traverse, object> processor = null, string pathRoot = "")
+    {
+        result = (T)MakeDeepCopy(source, typeof(T), processor, pathRoot);
+    }
+
+    /// <summary>Makes a deep copy of any object</summary>
+    /// <param name="source">The original object</param>
+    /// <param name="resultType">The type of the instance that should be created</param>
+    /// <param name="processor">Optional value transformation function (taking a field name and src/dst <see cref="Traverse"/> instances)</param>
+    /// <param name="pathRoot">The optional path root to start with</param>
+    /// <returns>The copy of the original object</returns>
+    ///
+    static int MakeDeepCopyRecursionDepth = 0;
+    const int MakeDeepCopyRecursionDepthLimit = 500;
+    public static object MakeDeepCopy(object source, Type resultType, Func<string, Traverse, Traverse, object> processor = null, string pathRoot = "")
+    {
+        MakeDeepCopyRecursionDepth++;
+        if (MakeDeepCopyRecursionDepth > MakeDeepCopyRecursionDepthLimit)
+        {
+            throw new Exception("MakeDeepCopy recursion depth limit exceeded");
+        }
+
+        if (source is null || resultType is null)
+        {
+            MakeDeepCopyRecursionDepth--;
+            return null;
+        }
+
+        resultType = Nullable.GetUnderlyingType(resultType) ?? resultType;
+        var type = source.GetType();
+
+        if (type.IsPrimitive)
+        {
+            MakeDeepCopyRecursionDepth--;
+            return source;
+        }
+
+        if (type.IsEnum)
+        {
+            MakeDeepCopyRecursionDepth--;
+            return Enum.ToObject(resultType, (int)source);
+        }
+
+        if (type.IsGenericType && resultType.IsGenericType)
+        {
+            addHandlerCacheLock.AcquireReaderLock(200);
+            try
+            {
+                if (!addHandlerCache.TryGetValue(resultType, out var addInvoker))
+                {
+                    var addOperation = AccessTools.FirstMethod(resultType, m => m.Name == "Add" && m.GetParameters().Length == 1);
+                    if (addOperation is object)
+                    {
+                        addInvoker = MethodInvoker.GetHandler(addOperation);
+                    }
+                    addHandlerCacheLock.UpgradeToWriterLock(200);
+                    addHandlerCacheLock.AcquireWriterLock(200);
+                    try
+                    {
+                        addHandlerCache[resultType] = addInvoker;
+                    }
+                    finally
+                    {
+                        addHandlerCacheLock.ReleaseWriterLock();
+                    }
+                }
+                if (addInvoker != null)
+                {
+                    var addableResult = Activator.CreateInstance(resultType);
+                    var newElementType = resultType.GetGenericArguments()[0];
+                    var i = 0;
+                    foreach (var element in source as IEnumerable)
+                    {
+                        var iStr = (i++).ToString();
+                        var path = pathRoot.Length > 0 ? pathRoot + "." + iStr : iStr;
+                        var newElement = MakeDeepCopy(element, newElementType, processor, path);
+                        _ = addInvoker(addableResult, new object[] { newElement });
+                    }
+                    MakeDeepCopyRecursionDepth--;
+                    return addableResult;
+                }
+            }
+            finally
+            {
+                addHandlerCacheLock.ReleaseReaderLock();
+            }
+        }
+
+        if (type.IsArray && resultType.IsArray)
+        {
+            var elementType = resultType.GetElementType();
+            var length = ((Array)source).Length;
+            var arrayResult = Activator.CreateInstance(resultType, new object[] { length }) as object[];
+            var originalArray = source as object[];
+            for (var i = 0; i < length; i++)
+            {
+                var iStr = i.ToString();
+                var path = pathRoot.Length > 0 ? pathRoot + "." + iStr : iStr;
+                arrayResult[i] = MakeDeepCopy(originalArray[i], elementType, processor, path);
+            }
+            MakeDeepCopyRecursionDepth--;
+            return arrayResult;
+        }
+
+        var ns = type.Namespace;
+        if (ns == "System" || (ns?.StartsWith("System.") ?? false))
+        {
+            MakeDeepCopyRecursionDepth--;
+            return source;
+        }
+
+        var result = AccessTools.CreateInstance(resultType == typeof(object) ? type : resultType);
+        Traverse.IterateFields(source, result, (name, src, dst) =>
+        {
+            var path = pathRoot.Length > 0 ? pathRoot + "." + name : name;
+            var value = processor is object ? processor(path, src, dst) : src.GetValue();
+            _ = dst.SetValue(MakeDeepCopy(value, dst.GetValueType(), processor, path));
+        });
+        MakeDeepCopyRecursionDepth--;
+        return result;
     }
 }
