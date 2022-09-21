1a15970886dcb9243d63eab594559a4f8bdc86f6
Eddio0141 <eddio0141@gmail.com>
2022-09-21 22:38:57 +0100

    just print message

--- Plugin/Patches/AuxilaryHelper.cs
+++ Plugin/Patches/AuxilaryHelper.cs
@@ -15,7 +15,7 @@ public static class AuxilaryHelper
     public static Exception Cleanup_IgnoreException(MethodBase original, Exception ex)
     {
         if (ex != null)
-            Plugin.Log.LogDebug($"Failed to patch: {original}, exception: {ex}");
+            Plugin.Log.LogDebug($"Failed to patch: {original}, exception: {ex.Message}");
         return null;
     }
 }
