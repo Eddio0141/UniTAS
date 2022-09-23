03c0d2768b5957b6a186c7db223fac5c36de2c7d
Eddio0141 <eddio0141@gmail.com>
2022-09-23 21:23:17 +0100

    added custom __UniTAS_UID field

---
+++ Patcher/Patcher.cs
@@ -0,0 +1,39 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+
+namespace UniTASPatcher
+{
+    public static class Patcher
+    {
+        static bool patchedAsyncOperation = false;
+
+        public static IEnumerable<string> TargetDLLs { get; } = new string[] { "UnityEngine.CoreModule.dll", "UnityEngine.dll" };
+
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            switch (assembly.Name.Name)
+            {
+                case "UnityEngine.CoreModule":
+                case "UnityEngine":
+                    {
+                        PatchAsyncOperation(assembly);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void PatchAsyncOperation(AssemblyDefinition assembly)
+        {
+            if (patchedAsyncOperation)
+                return;
+            var type = assembly.MainModule.GetType("UnityEngine", "AsyncOperation");
+            if (type == null)
+                return;
+            // add long UID field
+            type.Fields.Add(new FieldDefinition("__UniTAS_UID", FieldAttributes.Private, assembly.MainModule.ImportReference(typeof(ulong))));
+            patchedAsyncOperation = true;
+        }
+    }
+}
