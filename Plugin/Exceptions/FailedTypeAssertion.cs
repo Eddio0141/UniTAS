6236da27d09dfe487369a2883a95328711498bab
Eddio0141 <eddio0141@gmail.com>
2022-09-23 20:25:56 +0100

    field assertion stuff

---
+++ Plugin/Exceptions/FailedTypeAssertion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace UniTASPlugin.Exceptions;
+
+class MissingSomethingStatic : Exception
+{
+    public MissingSomethingStatic(Type type_, string name, string missingItem) : base($"Failed type get assertion, missing static {type_.FullName}.{name} {missingItem}") { }
+}
+
+class MissingSomethingInstance : Exception
+{
+    public MissingSomethingInstance(Type type_, string name, string missingItem) : base($"Failed type get assertion, missing instance {type_.FullName}.{name} {missingItem}") { }
+}
+
+internal class MissingStaticField : MissingSomethingStatic
+{
+    public MissingStaticField(Type type_, string fieldName) : base(type_, fieldName, "field") { }
+}
+
+internal class MissingInstanceField : MissingSomethingInstance
+{
+    public MissingInstanceField(Type type_, string fieldName) : base(type_, fieldName, "field") { }
+}
+
+internal class MissingStaticGetter : MissingSomethingStatic
+{
+    public MissingStaticGetter(Type type_, string getterName) : base(type_, getterName, "getter") { }
+}
+
+internal class MissingInstanceGetter : MissingSomethingInstance
+{
+    public MissingInstanceGetter(Type type_, string getterName) : base(type_, getterName, "getter") { }
+}
+
+internal class MissingStaticSetter : MissingSomethingStatic
+{
+    public MissingStaticSetter(Type type_, string setterName) : base(type_, setterName, "setter") { }
+}
+
+internal class MissingInstanceSetter : MissingSomethingInstance
+{
+    public MissingInstanceSetter(Type type_, string setterName) : base(type_, setterName, "setter") { }
+}
+
+internal class MissingStaticMethod : MissingSomethingStatic
+{
+    public MissingStaticMethod(Type type_, string methodName) : base(type_, methodName, "method") { }
+}
+
+internal class MissingInstanceMethod : MissingSomethingInstance
+{
+    public MissingInstanceMethod(Type type_, string methodName) : base(type_, methodName, "method") { }
+}
---
+++ Plugin/VersionSafeWrapper/WrapperAssertions.cs
@@ -0,0 +1,66 @@
+﻿using HarmonyLib;
+using System;
+
+namespace UniTASPlugin.VersionSafeWrapper;
+
+internal static class WrapperAssertions
+{
+    public static void AssertStaticField(Type type, string fieldName)
+    {
+        var field = AccessTools.Field(type, fieldName);
+        if (field == null || !field.IsStatic)
+            throw new Exceptions.MissingStaticField(type, fieldName);
+    }
+
+    public static void AssertInstanceField(Type type, string fieldName)
+    {
+        var field = AccessTools.Field(type, fieldName);
+        if (field == null || field.IsStatic)
+            throw new Exceptions.MissingInstanceField(type, fieldName);
+    }
+
+    public static void AssertStaticGetter(Type type, string propertyName)
+    {
+        var getter = AccessTools.PropertyGetter(type, propertyName);
+        if (getter == null || !getter.IsStatic)
+            throw new Exceptions.MissingStaticGetter(type, propertyName);
+    }
+
+    public static void AssertInstanceGetter(Type type, string propertyName)
+    {
+        var getter = AccessTools.PropertyGetter(type, propertyName);
+        if (getter == null || getter.IsStatic)
+            throw new Exceptions.MissingInstanceGetter(type, propertyName);
+    }
+
+    public static void AssertStaticSetter(Type type, string propertyName)
+    {
+        var setter = AccessTools.PropertySetter(type, propertyName);
+        if (setter == null || !setter.IsStatic)
+            throw new Exceptions.MissingStaticSetter(type, propertyName);
+    }
+
+    public static void AssertInstanceSetter(Type type, string propertyName)
+    {
+        var setter = AccessTools.PropertySetter(type, propertyName);
+        if (setter == null || setter.IsStatic)
+            throw new Exceptions.MissingInstanceSetter(type, propertyName);
+    }
+
+    // TODO
+    /*
+    public static void AssertStaticMethod(Type type, string methodName)
+    {
+        var methodTraverse = Traverse.Create(type).Method(methodName);
+        if (!methodTraverse.MethodExists())
+            throw new Exceptions.MissingStaticMethod(type, methodName);
+    }
+
+    public static void AssertInstanceMethod(Type type, string methodName)
+    {
+        var methodTraverse = Traverse.Create(instance).Method(methodName);
+        if (!methodTraverse.MethodExists())
+            throw new Exceptions.MissingInstanceMethod(instance, methodName);
+    }
+    */
+}
