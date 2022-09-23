ad029e65845e63322a2dbe53b123ddbbe1b16652
Eddio0141 <eddio0141@gmail.com>
2022-09-23 20:26:05 +0100

    wrapper for asyncoperation

---
+++ Plugin/VersionSafeWrapper/AsyncOperationWrap.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace UniTASPlugin.VersionSafeWrapper;
+
+internal class AsyncOperationWrap
+{
+    public AsyncOperation instance { get; private set; }
+
+    const string M_PTR_FIELD_NAME = "m_Ptr";
+
+    public AsyncOperationWrap(AsyncOperation asyncOperation)
+    {
+        // TODO earlier assertion
+        WrapperAssertions.AssertInstanceField(typeof(AsyncOperation), M_PTR_FIELD_NAME);
+        instance = asyncOperation ?? throw new ArgumentNullException(nameof(asyncOperation));
+    }
+
+    public IntPtr m_Ptr
+    {
+        get
+        {
+            var m_ptrField = Traverse.Create(instance).Field(M_PTR_FIELD_NAME);
+            return m_ptrField.GetValue<IntPtr>();
+        }
+    }
+}
