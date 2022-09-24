d0927a870a5f8ee15f1cc86682dcb34f8dc9add1
Eddio0141 <eddio0141@gmail.com>
2022-09-24 22:58:07 +0100

    wrote dummy patch base

--- Plugin/Patches/__UnityEngine/AssetBundle.cs
+++ Plugin/Patches/__UnityEngine/AssetBundle.cs
@@ -1,30 +1,95 @@
 ﻿using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace UniTASPlugin.Patches.__UnityEngine;
 
-class Dummy
+[HarmonyPatch(typeof(AssetBundle), "LoadFromFileAsync_Internal")]
+class LoadFromFileAsync_Internal
 {
-    internal static extern AssetBundleCreateRequest LoadFromFileAsync_Internal(string path, uint crc, ulong offset);
+    static Exception Cleanup(MethodBase original, Exception ex)
+    {
+        return AuxilaryHelper.Cleanup_IgnoreException(original, ex);
+    }
 
-    internal static extern AssetBundle LoadFromFile_Internal(string path, uint crc, ulong offset);
-
-    internal static extern AssetBundleCreateRequest LoadFromMemoryAsync_Internal(byte[] binary, uint crc);
-
-    internal static extern AssetBundle LoadFromMemory_Internal(byte[] binary, uint crc);
-
-    internal static extern AssetBundleCreateRequest LoadFromStreamAsyncInternal(Stream stream, uint crc, uint managedReadBufferSize);
-
-    internal static extern AssetBundle LoadFromStreamInternal(Stream stream, uint crc, uint managedReadBufferSize);
-
-    private extern Object LoadAsset_Internal(string name, Type type);
-
-    private extern AssetBundleRequest LoadAssetAsync_Internal(string name, Type type);
-
-    internal extern Object[] LoadAssetWithSubAssets_Internal(string name, Type type);
-
-    private extern AssetBundleRequest LoadAssetWithSubAssetsAsync_Internal(string name, Type type);
-
-    // theres no non-async alternative of this
-    // private static extern AssetBundleRecompressOperation RecompressAssetBundleAsync_Internal_Injected(string inputPath, string outputPath, ref BuildCompression method, uint expectedCRC, ThreadPriority priority);
+    static bool Prefix(string path, uint crc, ulong offset, AssetBundleCreateRequest __result)
+    {
+        // TODO handle return, this returns AssetBundleCreateRequest, sort it out with AsyncOperation in mind
+        var loadFromFile_Internal = Traverse.Create(typeof(AssetBundle)).Method("LoadFromFile_Internal", new Type[] { typeof(string), typeof(uint), typeof(ulong) });
+        var _ = loadFromFile_Internal.GetValue(new object[] { path, crc, offset });
+        return false;
+    }
 }
+
+[HarmonyPatch(typeof(AssetBundle), "LoadFromMemoryAsync_Internal")]
+class LoadFromMemoryAsync_Internal
+{
+    static Exception Cleanup(MethodBase original, Exception ex)
+    {
+        return AuxilaryHelper.Cleanup_IgnoreException(original, ex);
+    }
+
+    static bool Prefix(byte[] binary, uint crc, AssetBundleCreateRequest __result)
+    {
+        // TODO handle return, this returns AssetBundleCreateRequest, sort it out with AsyncOperation in mind
+        var loadFromFile_Internal = Traverse.Create(typeof(AssetBundle)).Method("LoadFromMemory_Internal", new Type[] { typeof(byte[]), typeof(uint) });
+        var _ = loadFromFile_Internal.GetValue(new object[] { binary, crc });
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(AssetBundle), "LoadFromStreamAsyncInternal")]
+class LoadFromStreamAsyncInternal
+{
+    static Exception Cleanup(MethodBase original, Exception ex)
+    {
+        return AuxilaryHelper.Cleanup_IgnoreException(original, ex);
+    }
+
+    static bool Prefix(Stream stream, uint crc, uint managedReadBufferSize, AssetBundleCreateRequest __result)
+    {
+        // TODO handle return, this returns AssetBundleCreateRequest, sort it out with AsyncOperation in mind
+        var loadFromFile_Internal = Traverse.Create(typeof(AssetBundle)).Method("LoadFromStreamInternal", new Type[] { typeof(Stream), typeof(uint), typeof(uint) });
+        var _ = loadFromFile_Internal.GetValue(new object[] { stream, crc, managedReadBufferSize });
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(AssetBundle), "LoadAssetAsync_Internal")]
+class LoadAssetAsync_Internal
+{
+    static Exception Cleanup(MethodBase original, Exception ex)
+    {
+        return AuxilaryHelper.Cleanup_IgnoreException(original, ex);
+    }
+
+    static bool Prefix(string name, Type type, AssetBundleRequest __result)
+    {
+        // TODO handle return, this returns AssetBundleRequest, sort it out with AsyncOperation in mind
+        var loadFromFile_Internal = Traverse.Create(typeof(AssetBundle)).Method("LoadAsset_Internal", new Type[] { typeof(string), typeof(Type) });
+        var _ = loadFromFile_Internal.GetValue(new object[] { name, type });
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(AssetBundle), "LoadAssetWithSubAssetsAsync_Internal")]
+class LoadAssetWithSubAssetsAsync_Internal
+{
+    static Exception Cleanup(MethodBase original, Exception ex)
+    {
+        return AuxilaryHelper.Cleanup_IgnoreException(original, ex);
+    }
+
+    static bool Prefix(string name, Type type, AssetBundleRequest __result)
+    {
+        // TODO handle return, this returns AssetBundleRequest, sort it out with AsyncOperation in mind
+        var loadFromFile_Internal = Traverse.Create(typeof(AssetBundle)).Method("LoadAssetWithSubAssets_Internal", new Type[] { typeof(string), typeof(Type) });
+        var _ = loadFromFile_Internal.GetValue(new object[] { name, type });
+        return false;
+    }
+}
+
+// TODO theres no non-async alternative of this
+// private static extern AssetBundleRecompressOperation RecompressAssetBundleAsync_Internal_Injected(string inputPath, string outputPath, ref BuildCompression method, uint expectedCRC, ThreadPriority priority);
