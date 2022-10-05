107ec89476acdc0a59774d3b9d2304c530bf63c2
Eddio0141 <eddio0141@gmail.com>
2022-10-05 17:10:21 +0100

    attributes on file type

--- Plugin/FakeGameState/GameFileSystem/File.cs
+++ Plugin/FakeGameState/GameFileSystem/File.cs
@@ -8,14 +8,18 @@ public class File : Entry
     public byte[] Data { get; set; }
     public string Text { get => Encoding.UTF8.GetString(Data); set => Encoding.UTF8.GetBytes(value); }
     public string Extension { get; }
+    public FileAttributes Attributes { get; set; }
 
-    public File(string name, string extension, Dir parent, byte[] data)
+    public File(string name, string extension, Dir parent, byte[] data, FileAttributes attributes)
     {
         Name = name;
         Parent = parent;
         Data = data;
         Extension = extension;
+        Attributes = attributes;
     }
+
+    public File(string name, string extension, Dir parent, byte[] data) : this(name, extension, parent, data, FileAttributes.Normal) { }
 
     public File(string name, string extension, Dir parent, string data) : this(name, extension, parent, Encoding.UTF8.GetBytes(data)) { }
 
