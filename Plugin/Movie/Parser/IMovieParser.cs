29c7d6c65fcab6bec0d15c8439964882e9403a12
Eddio0141 <eddio0141@gmail.com>
2022-10-15 21:01:26 +0100

    base for lexer / parsers

---
+++ Plugin/Movie/Parser/IMovieParser.cs
@@ -0,0 +1,9 @@
+﻿using UniTASPlugin.Movie.Lexer;
+using UniTASPlugin.Movie.Model.Script.LowLevel.OpCodes;
+
+namespace UniTASPlugin.Movie.Parser;
+
+public interface IMovieParser
+{
+    OpCodeBase[] ParseTokens(TokenBase[] tokens);
+}
