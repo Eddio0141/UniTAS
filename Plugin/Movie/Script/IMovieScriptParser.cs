f07f0113c5416caa6c8f19bb57fac5ea01022b74
Eddio0141 <eddio0141@gmail.com>
2022-10-18 22:08:31 +0100

    wrote most base for script parsing

--- Plugin/Movie/Script/IMovieScriptParser.cs
+++ Plugin/Movie/Script/IMovieScriptParser.cs
@@ -5,5 +5,11 @@ namespace UniTASPlugin.Movie.Script;
 
 public interface IMovieScriptParser
 {
-    IEnumerable<OpCodeBase> Parse(string input);
+    /// <summary>
+    /// Parses the given input into a list of OpCode list, where each list represents a method.
+    /// If not defined in method (basically the main method), the method name is null.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    IEnumerable<KeyValuePair<string, IEnumerable<OpCodeBase>>> Parse(string input);
 }
--- Plugin/Movie/Script/ScriptMethodModel.cs
+++ Plugin/Movie/Script/ScriptMethodModel.cs
@@ -1,8 +1,19 @@
-﻿using UniTASPlugin.MovieEngine.OpCodes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UniTASPlugin.MovieEngine.OpCodes;
 
 namespace UniTASPlugin.Movie.Script;
 
 public class ScriptMethodModel
 {
     public OpCodeBase[] OpCodes { get; }
+    public string Name { get; }
+
+    public ScriptMethodModel() : this(null, new OpCodeBase[0]) { }
+
+    public ScriptMethodModel(string name, IEnumerable<OpCodeBase> opCodes)
+    {
+        Name = name;
+        OpCodes = opCodes.ToArray();
+    }
 }
