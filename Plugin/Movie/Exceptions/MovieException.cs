c39a7d6dbdc033e6495b8ab942f0e90d1539ecea
Eddio0141 <eddio0141@gmail.com>
2022-10-18 14:48:54 +0100

    progress

---
+++ Plugin/Movie/Exceptions/MovieException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UniTASPlugin.Movie.Exceptions;
+
+public class MovieException : Exception
+{
+    public MovieException(string message) : base(message) { }
+}
--- Plugin/Movie/MovieModel.cs
+++ Plugin/Movie/MovieModel.cs
@@ -1,4 +1,5 @@
-﻿using Sprache;
+﻿using System.Linq;
+using UniTASPlugin.Movie.Exceptions;
 using UniTASPlugin.Movie.Properties;
 using UniTASPlugin.Movie.Script;
 
@@ -6,13 +7,20 @@ namespace UniTASPlugin.Movie;
 
 public class MovieModel
 {
-    public PropertiesModel Properties { get; }
+    public PropertiesModel Properties { get; private set; }
     public ScriptModel Script { get; }
 
-    private readonly Parser<string> propertyParser = ;
-
-    public MovieModel ParseFromText(string input, IMoviePropertyParser propertyParser, IMovieScriptParser scriptParser)
+    public MovieModel ParseFromText(string input, IMovieSectionSplitter sectionSplitter, IMoviePropertyParser propertyParser, IMovieScriptParser scriptParser)
     {
+        var splitSections = sectionSplitter.Split(input);
+        switch (splitSections.Count())
+        {
+            case 0:
+                throw new MissingMoviePropertiesException();
+            case 1:
+                throw new MissingMovieScriptException();
+        }
 
+        Properties = propertyParser.Parse(splitSections.ElementAt(0));
     }
 }
