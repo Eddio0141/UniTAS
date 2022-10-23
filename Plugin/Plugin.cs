de980749a1e2794699677c0f3afff73587c82dd2
Eddio0141 <eddio0141@gmail.com>
2022-10-23 01:55:26 +0100

    added ioc container nuget

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -54,6 +54,8 @@ public class Plugin : BaseUnityPlugin
         Log.LogInfo($"Plugin {Name} is loaded!");
     }
 
+    private IHostBuilder
+
     // unity execution order is Awake() -> FixedUpdate() -> Update()
     private void Update()
     {
