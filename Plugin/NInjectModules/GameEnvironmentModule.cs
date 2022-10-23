3203f834a6bf3c672bf0854a3f774071ceb6abc4
Eddio0141 <eddio0141@gmail.com>
2022-10-23 06:43:59 +0100

    added kernel and use of modules

---
+++ Plugin/NInjectModules/GameEnvironmentModule.cs
@@ -0,0 +1,15 @@
+﻿using Ninject.Modules;
+using UniTASPlugin.GameEnvironment.Interfaces;
+
+namespace UniTASPlugin.NInjectModules;
+
+public class GameEnvironmentModule : NinjectModule
+{
+    public override void Load()
+    {
+        Bind<IRunVirtualEnvironmentProperty>().To<GameEnvironment.GameEnvironment>();
+        Bind<IInputStateProperty>().To<GameEnvironment.GameEnvironment>();
+
+        Bind<GameEnvironment.GameEnvironment>().ToSelf().InSingletonScope();
+    }
+}
