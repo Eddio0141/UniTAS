0dd70ed64b5ee218474f623661d16b2c3e932e7b
Eddio0141 <eddio0141@gmail.com>
2022-11-03 22:51:03 +0000

    method define compiles

--- Plugin/Movie/DefaultParsers/DefaultMovieScriptParser/DefaultGrammarListenerCompiler.cs
+++ Plugin/Movie/DefaultParsers/DefaultMovieScriptParser/DefaultGrammarListenerCompiler.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using UniTASPlugin.Movie.Models.Script;
+using UniTASPlugin.Movie.ScriptEngine;
 using UniTASPlugin.Movie.ScriptEngine.OpCodes;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Method;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.VariableSet;
 
 namespace UniTASPlugin.Movie.DefaultParsers.DefaultMovieScriptParser;
 
 public class DefaultGrammarListenerCompiler : MovieScriptDefaultGrammarBaseListener
 {
-    private List<OpCodeBase> _mainBuilder = new();
-    private List<KeyValuePair<string, List<OpCodeBase>>> _builtMethods = new();
-    private Stack<KeyValuePair<string, List<OpCodeBase>>> _methodBuilders = new();
-    private bool _buildingMethod = false;
+    private readonly List<OpCodeBase> _mainBuilder = new();
+    private readonly List<KeyValuePair<string, List<OpCodeBase>>> _builtMethods = new();
+    private readonly Stack<KeyValuePair<string, List<OpCodeBase>>> _methodBuilders = new();
+    private bool _buildingMethod;
 
-    private void AddOpCodes(IEnumerable<OpCodeBase> opCodes)
+    public ScriptModel Compile()
+    {
+        return new ScriptModel(new(null, _mainBuilder),
+            new List<ScriptMethodModel>(_builtMethods.Select(x => new ScriptMethodModel(x.Key, x.Value))));
+    }
+
+    private void AddOpCode(OpCodeBase opCode)
     {
         if (_buildingMethod)
         {
-            _methodBuilders.Peek().Value.AddRange(opCodes);
+            _methodBuilders.Peek().Value.Add(opCode);
         }
         else
         {
-            _mainBuilder.AddRange(opCodes);
+            _mainBuilder.Add(opCode);
         }
     }
 
@@ -27,8 +38,21 @@ public class DefaultGrammarListenerCompiler : MovieScriptDefaultGrammarBaseListe
         var methodName = context.IDENTIFIER_STRING().GetText();
         _buildingMethod = true;
         _methodBuilders.Push(new KeyValuePair<string, List<OpCodeBase>>(methodName, new()));
+    }
+    
+    public override void ExitMethodDef(MovieScriptDefaultGrammarParser.MethodDefContext context)
+    {
+        _builtMethods.Add(_methodBuilders.Pop());
+        if (_methodBuilders.Count == 0)
+        {
+            _buildingMethod = false;
+        }
+    }
 
-        // args
-        context.methodDefArgs()
+    public override void ExitMethodDefArgs(MovieScriptDefaultGrammarParser.MethodDefArgsContext context)
+    {
+        var argName = context.IDENTIFIER_STRING().GetText();
+        AddOpCode(new PopArgOpCode(RegisterType.Temp));
+        AddOpCode(new NewVariableOpCode(RegisterType.Temp, argName));
     }
 }
