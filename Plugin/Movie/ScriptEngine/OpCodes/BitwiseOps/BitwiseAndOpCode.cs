1bdb9a4a47407398e2448bdf1e7c0171a2ddd255
Eddio0141 <eddio0141@gmail.com>
2022-11-04 20:58:43 +0000

    added bitwise opcodes

---
+++ Plugin/Movie/ScriptEngine/OpCodes/BitwiseOps/BitwiseAndOpCode.cs
@@ -0,0 +1,8 @@
+﻿namespace UniTASPlugin.Movie.ScriptEngine.OpCodes.BitwiseOps;
+
+public class BitwiseAndOpCode : BitwiseBase
+{
+    public BitwiseAndOpCode(RegisterType resultRegister, RegisterType left, RegisterType right) : base(resultRegister, left, right)
+    {
+    }
+}
---
+++ Plugin/Movie/ScriptEngine/OpCodes/BitwiseOps/BitwiseShiftRightOpCode.cs
@@ -0,0 +1,8 @@
+﻿namespace UniTASPlugin.Movie.ScriptEngine.OpCodes.BitwiseOps;
+
+public class BitwiseShiftRightOpCode : BitwiseBase
+{
+    public BitwiseShiftRightOpCode(RegisterType resultRegister, RegisterType left, RegisterType right) : base(resultRegister, left, right)
+    {
+    }
+}
