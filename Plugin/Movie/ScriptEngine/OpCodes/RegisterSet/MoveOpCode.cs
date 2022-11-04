8fac233aa2a079e516af38f730fd1bb420d9df2a
Eddio0141 <eddio0141@gmail.com>
2022-11-04 04:01:15 +0000

    added opcode

---
+++ Plugin/Movie/ScriptEngine/OpCodes/RegisterSet/MoveOpCode.cs
@@ -0,0 +1,11 @@
+﻿namespace UniTASPlugin.Movie.ScriptEngine.OpCodes.RegisterSet;
+
+public class MoveOpCode : RegisterSetBase
+{
+    public RegisterType RegisterDest { get; }
+
+    public MoveOpCode(RegisterType register, RegisterType registerDest) : base(register)
+    {
+        RegisterDest = registerDest;
+    }
+}
