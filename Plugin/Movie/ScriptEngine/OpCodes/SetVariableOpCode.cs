a621870aacb3d9c4f11b568255a39d3487d36b0c
Eddio0141 <eddio0141@gmail.com>
2022-11-05 06:05:41 +0000

    progress writing var assign

---
+++ Plugin/Movie/ScriptEngine/OpCodes/SetVariableOpCode.cs
@@ -0,0 +1,13 @@
+﻿namespace UniTASPlugin.Movie.ScriptEngine.OpCodes;
+
+public class SetVariableOpCode : OpCodeBase
+{
+    public string Name { get; }
+    public RegisterType Register { get; }
+
+    public SetVariableOpCode(string name, RegisterType register)
+    {
+        Name = name;
+        Register = register;
+    }
+}
