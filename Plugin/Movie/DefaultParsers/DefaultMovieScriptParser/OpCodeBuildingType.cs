cec7a69c05f239ab7548bec7eea4c804bb3d1152
Eddio0141 <eddio0141@gmail.com>
2022-11-15 14:10:50 +0000

    wrote everything for if else statement

---
+++ Plugin/Movie/DefaultParsers/DefaultMovieScriptParser/OpCodeBuildingType.cs
@@ -0,0 +1,6 @@
+public enum OpCodeBuildingType
+{
+    BuildingMethod,
+    BuildingMethodArgs,
+    BuildingMainMethod,
+}
--- Plugin/Movie/ScriptEngine/OpCodes/Jump/JumpIfFalse.cs
+++ Plugin/Movie/ScriptEngine/OpCodes/Jump/JumpIfFalse.cs
@@ -1,6 +1,11 @@
 ﻿namespace UniTASPlugin.Movie.ScriptEngine.OpCodes.Jump;
 
-public class JumpIfFalse
+public class JumpIfFalse : JumpBase
 {
     public RegisterType Register { get; }
+
+    public JumpIfFalse(int offset, RegisterType register) : base(offset)
+    {
+        Register = register;
+    }
 }
