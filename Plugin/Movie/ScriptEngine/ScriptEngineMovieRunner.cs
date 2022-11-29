793b123ca3e73e47a00b3ef78ecfd998d0b9f933
Eddio0141 <eddio0141@gmail.com>
2022-11-29 00:13:16 +0000

    progress writing opcodes

--- Plugin/Movie/ScriptEngine/ScriptEngineMovieRunner.cs
+++ Plugin/Movie/ScriptEngine/ScriptEngineMovieRunner.cs
@@ -5,7 +5,18 @@ using UniTASPlugin.GameEnvironment.Interfaces;
 using UniTASPlugin.Movie.ScriptEngine.EngineMethods;
 using UniTASPlugin.Movie.ScriptEngine.MovieModels.Script;
 using UniTASPlugin.Movie.ScriptEngine.OpCodes;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.BitwiseOps;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Jump;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Logic;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Loop;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Maths;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Method;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.RegisterSet;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Scope;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.StackOp;
+using UniTASPlugin.Movie.ScriptEngine.OpCodes.Tuple;
 using UniTASPlugin.Movie.ScriptEngine.ParseInterfaces;
+using UniTASPlugin.Movie.ScriptEngine.ValueTypes;
 
 namespace UniTASPlugin.Movie.ScriptEngine;
 
@@ -13,7 +24,10 @@ public class ScriptEngineMovieRunner : IMovieRunner
 {
     private Register[] _registers;
     private OpCodeBase[] _mainMethod;
-    private Dictionary<string, OpCodeBase[]> _methods;
+    private ScriptMethodModel[] _methods;
+    private ulong _pc;
+    private int _currentMethod;
+
     public bool MovieEnd { get; private set; }
 
     private readonly IMovieParser _parser;
@@ -58,12 +72,179 @@ public class ScriptEngineMovieRunner : IMovieRunner
     {
         _registers = new Register[Enum.GetNames(typeof(RegisterType)).Length];
         MovieEnd = false;
+        _pc = 0;
+        _currentMethod = -1;
 
         _mainMethod = script.MainMethod.OpCodes;
-        _methods = new Dictionary<string, OpCodeBase[]>();
-        foreach (var scriptMethodModel in script.Methods)
+        _methods = script.Methods.ToArray();
+    }
+
+    private void ProcessUntilStop()
+    {
+        var opCodes = _currentMethod < 0 ? _mainMethod : _methods[_currentMethod].OpCodes;
+
+        while (true)
         {
-            _methods.Add(scriptMethodModel.Name, scriptMethodModel.OpCodes);
+            var opCode = opCodes[_pc];
+
+            switch (opCode)
+            {
+                case BitwiseAndOpCode bitwiseAndOpCode:
+                {
+                    var and = (_registers[(int)bitwiseAndOpCode.Left].InnerValue as IntValueType).Value &
+                              (_registers[(int)bitwiseAndOpCode.Right].InnerValue as IntValueType).Value;
+                    _registers[(int)bitwiseAndOpCode.ResultRegister].InnerValue = new IntValueType(and);
+                    _pc++;
+                    break;
+                }
+                case BitwiseOrOpCode bitwiseOrOpCode:
+                {
+                    var or = (_registers[(int)bitwiseOrOpCode.Left].InnerValue as IntValueType).Value &
+                             (_registers[(int)bitwiseOrOpCode.Right].InnerValue as IntValueType).Value;
+                    _registers[(int)bitwiseOrOpCode.ResultRegister].InnerValue = new IntValueType(or);
+                    _pc++;
+                    break;
+                }
+                case BitwiseShiftLeftOpCode bitwiseShiftLeftOpCode:
+                {
+                    var shiftLeft = (_registers[(int)bitwiseShiftLeftOpCode.Left].InnerValue as IntValueType).Value <<
+                                    (_registers[(int)bitwiseShiftLeftOpCode.Right].InnerValue as IntValueType).Value;
+                    _registers[(int)bitwiseShiftLeftOpCode.ResultRegister].InnerValue = new IntValueType(shiftLeft);
+                    _pc++;
+                    break;
+                }
+                case BitwiseShiftRightOpCode bitwiseShiftRightOpCode:
+                {
+                    var shiftRight = (_registers[(int)bitwiseShiftRightOpCode.Left].InnerValue as IntValueType).Value >>
+                                     (_registers[(int)bitwiseShiftRightOpCode.Right].InnerValue as IntValueType).Value;
+                    _registers[(int)bitwiseShiftRightOpCode.ResultRegister].InnerValue = new IntValueType(shiftRight);
+                    _pc++;
+                    break;
+                }
+                case BitwiseXorOpCode bitwiseXorOpCode:
+                {
+                    var xor = (_registers[(int)bitwiseXorOpCode.Left].InnerValue as IntValueType).Value ^
+                              (_registers[(int)bitwiseXorOpCode.Right].InnerValue as IntValueType).Value;
+                    _registers[(int)bitwiseXorOpCode.ResultRegister].InnerValue = new IntValueType(xor);
+                    _pc++;
+                    break;
+                }
+                case CastOpCode castOpCode:
+                {
+                    var sourceString = _registers[(int)castOpCode.Source].ToString();
+                    _registers[(int)castOpCode.Dest].InnerValue = castOpCode.ValueType switch
+                    {
+                        BasicValueType.Bool => new BoolValueType(bool.Parse(sourceString)),
+                        BasicValueType.Float => new FloatValueType(float.Parse(sourceString)),
+                        BasicValueType.Int => new IntValueType(int.Parse(sourceString)),
+                        BasicValueType.String => new StringValueType(sourceString),
+                        _ => throw new NotImplementedException()
+                    };
+                    break;
+                }
+                case FrameAdvanceOpCode frameAdvanceOpCode:
+                    break;
+                case JumpIfEqOpCode jumpIfEqOpCode:
+                    break;
+                case JumpIfEqZero jumpIfEqZero:
+                    break;
+                case JumpIfFalse jumpIfFalse:
+                    break;
+                case JumpIfGtEqOpCode jumpIfGtEqOpCode:
+                    break;
+                case JumpIfGtOpCode jumpIfGtOpCode:
+                    break;
+                case JumpIfLtEqOpCode jumpIfLtEqOpCode:
+                    break;
+                case JumpIfLtOpCode jumpIfLtOpCode:
+                    break;
+                case JumpIfNEqOpCode jumpIfNEqOpCode:
+                    break;
+                case JumpIfTrue jumpIfTrue:
+                    break;
+                case JumpOpCode jumpOpCode:
+                    break;
+                case AndOpCode andOpCode:
+                    break;
+                case EqualOpCode equalOpCode:
+                    break;
+                case GreaterEqualOpCode greaterEqualOpCode:
+                    break;
+                case GreaterOpCode greaterOpCode:
+                    break;
+                case LessEqualOpCode lessEqualOpCode:
+                    break;
+                case LessOpCode lessOpCode:
+                    break;
+                case NotEqualOpCode notEqualOpCode:
+                    break;
+                case NotOpCode notOpCode:
+                    break;
+                case OrOpCode orOpCode:
+                    break;
+                case BreakOpCode breakOpCode:
+                    break;
+                case ContinueOpCode continueOpCode:
+                    break;
+                case AddOpCode addOpCode:
+                    break;
+                case DivOpCode divOpCode:
+                    break;
+                case FlipNegativeOpCode flipNegativeOpCode:
+                    break;
+                case ModOpCode modOpCode:
+                    break;
+                case MultOpCode multOpCode:
+                    break;
+                case SubOpCode subOpCode:
+                    break;
+                case GotoMethodOpCode gotoMethodOpCode:
+                    break;
+                case PopArgOpCode popArgOpCode:
+                    break;
+                case PushArgOpCode pushArgOpCode:
+                    break;
+                case ReturnOpCode returnOpCode:
+                    break;
+                case PushListOpCode pushListOpCode:
+                    break;
+                case ConstToRegisterOpCode constToRegisterOpCode:
+                    break;
+                case MoveOpCode moveOpCode:
+                    break;
+                case VarToRegisterOpCode varToRegisterOpCode:
+                    break;
+                case EnterScopeOpCode enterScopeOpCode:
+                    break;
+                case ExitScopeOpCode exitScopeOpCode:
+                    break;
+                case SetVariableOpCode setVariableOpCode:
+                    break;
+                case PopStackOpCode popStackOpCode:
+                    break;
+                case PushStackOpCode pushStackOpCode:
+                    break;
+                case PopTupleOpCode popTupleOpCode:
+                    break;
+                case PushTupleOpCode pushTupleOpCode:
+                    break;
+                case BitwiseBase bitwiseBase:
+                    break;
+                case JumpCompareBase jumpCompareBase:
+                    break;
+                case LogicComparisonBase logicComparisonBase:
+                    break;
+                case MathOpBase mathOpBase:
+                    break;
+                case RegisterSetBase registerSetBase:
+                    break;
+                case JumpBase jumpBase:
+                    break;
+                case LogicBase logicBase:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(opCode));
+            }
         }
     }
 
