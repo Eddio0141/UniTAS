cc852ee3eff51236ed4c6118b2e484ab96a1a457
Eddio0141 <eddio0141@gmail.com>
2022-12-02 10:10:01 +0000

    added tostring as a requirement

--- Plugin/Movie/ScriptEngine/ValueTypes/ValueType.cs
+++ Plugin/Movie/ScriptEngine/ValueTypes/ValueType.cs
@@ -5,4 +5,5 @@ namespace UniTASPlugin.Movie.ScriptEngine.ValueTypes;
 public abstract class ValueType : ICloneable
 {
     public abstract object Clone();
+    public abstract override string ToString();
 }
