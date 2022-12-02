f1e96d95fec0beb2f31424284ecd812b4e80ad18
Eddio0141 <eddio0141@gmail.com>
2022-12-02 10:42:54 +0000

    adding more tests

--- UniTASPluginTest/MovieParsing/ScriptEngineLowLevelTests.cs
+++ UniTASPluginTest/MovieParsing/ScriptEngineLowLevelTests.cs
@@ -5,44 +5,48 @@ using UniTASPlugin.Movie.ScriptEngine.EngineMethods;
 using UniTASPlugin.Movie.ScriptEngine.LowLevelEngine;
 using UniTASPlugin.Movie.ScriptEngine.MovieModels.Script;
 using UniTASPlugin.Movie.ScriptEngine.Parsers.MovieScriptParser;
+using UniTASPlugin.Movie.ScriptEngine.ValueTypes;
 using ValueType = UniTASPlugin.Movie.ScriptEngine.ValueTypes.ValueType;
 
 namespace UniTASPluginTest.MovieParsing;
 
 public class ScriptEngineLowLevelTests
 {
-    private interface ITestInvoke
+    private class TestExternGetArgs : EngineExternalMethodBase
     {
-        List<string> UpdateArgsList();
-    }
+        public List<string> Args { get; } = new();
 
-    private class TestThing : ITestInvoke
-    {
-        public List<string> ArgTestList { get; } = new();
-
-        public List<string> UpdateArgsList()
+        public TestExternGetArgs() : base("get_args", -1)
         {
-            return ArgTestList;
-        }
-    }
-
-    private class TestExternMethod : EngineExternalMethodBase
-    {
-        private readonly ITestInvoke _testInvoke;
-
-        public TestExternMethod(ITestInvoke testInvoke) : base("test", 1)
-        {
-            _testInvoke = testInvoke;
         }
 
         public override List<ValueType> Invoke(IEnumerable<IEnumerable<ValueType>> args)
         {
             foreach (var argTuple in args)
             {
-                _testInvoke.UpdateArgsList().Add(argTuple.First().ToString() ?? throw new InvalidOperationException());
+                foreach (var arg in argTuple)
+                {
+                    Args.Add(arg.ToString());
+                }
             }
 
             return new();
+        }
+    }
+
+    private class TestExternReturnValues : EngineExternalMethodBase
+    {
+        public TestExternReturnValues() : base("return_values", argReturnCount: 4)
+        {
+        }
+
+        public override List<ValueType> Invoke(IEnumerable<IEnumerable<ValueType>> args)
+        {
+            return new()
+            {
+                new IntValueType(2), new StringValueType("testing stuff!"), new BoolValueType(false),
+                new FloatValueType(-10.29f)
+            };
         }
     }
 
@@ -68,17 +72,31 @@ public class ScriptEngineLowLevelTests
     [Fact]
     public void TestInvoke()
     {
-        var testList = new TestThing();
-        var externMethod = new TestExternMethod(testList);
-
+        var externMethod = new TestExternGetArgs();
         var engine = Setup(@"$loop_index = 0
 loop 10 {
-    test($loop_index)
+    get_args($loop_index)
     $loop_index += 1
 }", new EngineExternalMethodBase[] { externMethod });
 
         engine.ExecUntilStop();
+        externMethod.Args.Should().BeEquivalentTo("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    }
 
-        testList.ArgTestList.Should().BeEquivalentTo("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    [Fact]
+    public void TestTuple()
+    {
+        var externGetArgs = new TestExternGetArgs();
+        var engine = Setup(
+@"$tuple = (59, ""hello world"", false, -10.3)
+$(var, var2, var3, var4) = $tuple
+get_args($var) | get_args($var2) | get_args($var3) | get_args($var4)
+$(var, var2, var3, var4) = (""a"", ""b"", ""c"", ""d"")
+get_args($var, $var2, $var3, $var4)",
+            new EngineExternalMethodBase[] { externGetArgs });
+
+        engine.ExecUntilStop();
+        externGetArgs.Args.Should()
+            .BeEquivalentTo(new[] { "59", "hello world", "false", "-10.3", "a", "b", "c", "d" });
     }
 }
