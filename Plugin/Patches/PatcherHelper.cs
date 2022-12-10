54690cb369d50266cfc6e59615ee0b68dd4b1b81
Eddio0141 <eddio0141@gmail.com>
2022-12-10 14:02:07 +0000

    method to check hardcoded reverse patches

--- Plugin/Patches/PatcherHelper.cs
+++ Plugin/Patches/PatcherHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 
 namespace UniTASPlugin.Patches;
@@ -10,5 +12,19 @@ public static class PatcherHelper
         if (ex != null)
             Plugin.Log.LogDebug($"Failed to patch: {original}, exception: {ex}");
         return null;
+    }
+
+    public static bool InvokedFromCriticalNamespace()
+    {
+        var rev = Plugin.Kernel.Resolve<PatchReverseInvoker>();
+        var trace = rev.Invoke(() => new StackTrace());
+
+        var criticalNamespaces = new[]
+        {
+            "BepInEx"
+        };
+
+        return trace.GetFrames()
+            .Any(f => criticalNamespaces.Any(n => f.GetMethod().DeclaringType.FullName.StartsWith(n)));
     }
 }
