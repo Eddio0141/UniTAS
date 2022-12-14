db2b3779e97b3db947a348e1876a50753799a5fe
Eddio0141 <eddio0141@gmail.com>
2022-12-14 00:05:20 +0000

    removed unused opcode

--- Plugin/Movie/ScriptEngine/OpCodes/Jump/JumpIfEqOpCode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UniTASPlugin.Movie.ScriptEngine.OpCodes.Jump;
-
-public class JumpIfEqOpCode : JumpCompareBase
-{
-    public JumpIfEqOpCode(int offset, RegisterType left, RegisterType right) : base(offset, left, right)
-    {
-    }
-}
