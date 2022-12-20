a7f8e95213e5d2cba647584f91e8bc03653e8c4e
Eddio0141 <eddio0141@gmail.com>
2022-12-20 04:21:30 +0000

    added mouse click

---
+++ Plugin/Movie/EngineMethods/MouseClickLeftMethod.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using UniTASPlugin.GameEnvironment;
+using UniTASPlugin.Movie.ValueTypes;
+
+namespace UniTASPlugin.Movie.EngineMethods;
+
+public class MouseClickLeftMethod : EngineExternalMethod
+{
+    private readonly IVirtualEnvironmentFactory _virtualEnvironmentFactory;
+
+    public MouseClickLeftMethod(IVirtualEnvironmentFactory virtualEnvironmentFactory) : base("left_click", 1)
+    {
+        _virtualEnvironmentFactory = virtualEnvironmentFactory;
+    }
+
+    public override List<ValueType> Invoke(IEnumerable<IEnumerable<ValueType>> args, MovieRunner runner)
+    {
+        var arg = args.First().First();
+        if (arg is not BoolValueType boolValue) return new();
+
+        var env = _virtualEnvironmentFactory.GetVirtualEnv();
+        env.InputState.MouseState.LeftClick = boolValue.Value;
+
+        return new();
+    }
+}
