226f56cd18733753709a13782d00e1eeb81cfe9f
Eddio0141 <eddio0141@gmail.com>
2022-12-24 05:03:17 +0000

    added patch types

---
+++ Plugin/Patches/PatchTypes/PatchType.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace UniTASPlugin.Patches.PatchTypes;
+
+public abstract class PatchType : Attribute
+{
+}
---
+++ Plugin/Patches/PatchTypes/UnityPatch.cs
@@ -0,0 +1,5 @@
+namespace UniTASPlugin.Patches.PatchTypes;
+
+public class UnityPatch : PatchType
+{
+}
