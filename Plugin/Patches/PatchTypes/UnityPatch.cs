33737cc13714adaa590d70d83383d166b992c2f8
Eddio0141 <eddio0141@gmail.com>
2022-12-24 06:24:03 +0000

    let module patch multiple groups

--- Plugin/Patches/PatchTypes/UnityPatch.cs
+++ Plugin/Patches/PatchTypes/UnityPatch.cs
@@ -2,4 +2,11 @@ namespace UniTASPlugin.Patches.PatchTypes;
 
 public class UnityPatch : PatchType
 {
+    public UnityPatch(bool patchAllGroups) : base(patchAllGroups)
+    {
+    }
+
+    public UnityPatch()
+    {
+    }
 }
