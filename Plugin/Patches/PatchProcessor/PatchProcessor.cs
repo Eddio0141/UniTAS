56f038a849cdcbac7d9e342d2a910706b141ef78
Eddio0141 <eddio0141@gmail.com>
2022-12-24 06:24:03 +0000

    let module patch multiple groups

--- Plugin/Patches/PatchProcessor/PatchProcessor.cs
+++ Plugin/Patches/PatchProcessor/PatchProcessor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UniTASPlugin.Logger;
+using UniTASPlugin.Patches.PatchTypes;
 
 namespace UniTASPlugin.Patches.PatchProcessor;
 
@@ -22,76 +23,99 @@ public abstract class PatchProcessor
         // TODO replace this later
         var pluginAssembly = typeof(Plugin).Assembly;
 
-        var patchGroups = new List<Type>();
+        // list of patch groups, and PatchType
+        var targetModulesAndPatchGroups = new List<KeyValuePair<List<Type>, PatchType>>();
         foreach (var type in pluginAssembly.GetTypes())
         {
             var attributes = type.GetCustomAttributes(TargetPatchType, false);
             if (attributes.Length == 0) continue;
 
+            var patchType = (PatchType)attributes[0];
+            var patchGroups = new List<Type>();
+
             foreach (var innerType in type.GetNestedTypes())
             {
-                var patchGroupAttributes = innerType.GetCustomAttributes(typeof(PatchGroup), false);
-                if (patchGroupAttributes.Length == 0) continue;
+                if (innerType.GetCustomAttributes(typeof(PatchGroup), false).Length == 0) continue;
 
                 patchGroups.Add(innerType);
             }
+
+            targetModulesAndPatchGroups.Add(new(patchGroups, patchType));
         }
 
         var version = VersionStringToNumber(Version);
-        Type chosenPatch = null;
-        var chosenRangeStart = 0ul;
-        var chosenRangeEnd = 0ul;
 
-        foreach (var patchGroup in patchGroups)
+        foreach (var targetModulesAndPatchGroup in targetModulesAndPatchGroups)
         {
-            var foundAttribute = patchGroup.GetCustomAttributes(typeof(PatchGroup), false).First();
-            if (foundAttribute is not PatchGroup patchGroupAttribute) continue;
-            var versionStart = VersionStringToNumber(patchGroupAttribute.RangeStart);
-            var versionEnd = VersionStringToNumber(patchGroupAttribute.RangeEnd);
+            var patchGroups = targetModulesAndPatchGroup.Key;
+            var patchType = targetModulesAndPatchGroup.Value;
 
-            if (chosenPatch == null)
+            // different process
+            if (patchType is { PatchAllGroups: true })
             {
-                chosenPatch = patchGroup;
-                chosenRangeStart = versionStart;
-                chosenRangeEnd = versionEnd;
+                foreach (var group in patchGroups)
+                {
+                    PatchAllInGroup(group);
+                }
+
                 continue;
             }
 
-            if (versionStart <= version && version <= versionEnd)
+            Type chosenGroup = null;
+            var chosenRangeStart = 0ul;
+            var chosenRangeEnd = 0ul;
+
+            foreach (var patchGroup in patchGroups)
             {
-                chosenPatch = patchGroup;
-                break;
+                var patchGroupAttribute = (PatchGroup)patchGroup.GetCustomAttributes(typeof(PatchGroup), false).First();
+                var versionStart = VersionStringToNumber(patchGroupAttribute.RangeStart);
+                var versionEnd = VersionStringToNumber(patchGroupAttribute.RangeEnd);
+
+                if (chosenGroup == null)
+                {
+                    chosenGroup = patchGroup;
+                    chosenRangeStart = versionStart;
+                    chosenRangeEnd = versionEnd;
+                    continue;
+                }
+
+                if (versionStart <= version && version <= versionEnd)
+                {
+                    chosenGroup = patchGroup;
+                    break;
+                }
+
+                // prioritise the patch group that is closest to the current version, but lower than version
+
+                if (versionEnd < version && versionEnd > chosenRangeEnd)
+                {
+                    chosenGroup = patchGroup;
+                    chosenRangeStart = versionStart;
+                    chosenRangeEnd = versionEnd;
+                    continue;
+                }
+
+                if (versionStart > version && versionStart < chosenRangeStart)
+                {
+                    chosenGroup = patchGroup;
+                    chosenRangeStart = versionStart;
+                    chosenRangeEnd = versionEnd;
+                }
             }
 
-            // prioritise the patch group that is closest to the current version, but lower than version
+            if (chosenGroup == null) continue;
 
-            if (versionEnd < version && versionEnd > chosenRangeEnd)
-            {
-                chosenPatch = patchGroup;
-                chosenRangeStart = versionStart;
-                chosenRangeEnd = versionEnd;
-                continue;
-            }
-
-            if (versionStart > version && versionStart < chosenRangeStart)
-            {
-                chosenPatch = patchGroup;
-                chosenRangeStart = versionStart;
-                chosenRangeEnd = versionEnd;
-            }
+            // patch everything in the chosen patch group
+            PatchAllInGroup(chosenGroup);
         }
+    }
 
-        if (chosenPatch == null)
+    private void PatchAllInGroup(Type patchGroup)
+    {
+        foreach (var patch in patchGroup.GetNestedTypes())
         {
-            return;
-        }
-
-        // patch everything in the chosen patch group
-        // bepInEx will take care of the rest
-        foreach (var patchType in chosenPatch.GetNestedTypes())
-        {
-            _logger.LogDebug($"Attempting patch to {patchType.Name}");
-            HarmonyLib.Harmony.CreateAndPatchAll(patchType);
+            _logger.LogDebug($"Attempting patch for {patch.Name}");
+            HarmonyLib.Harmony.CreateAndPatchAll(patch);
         }
     }
 
--- Plugin/Patches/PatchTypes/MscorlibPatch.cs
+++ Plugin/Patches/PatchTypes/MscorlibPatch.cs
@@ -2,4 +2,11 @@ namespace UniTASPlugin.Patches.PatchTypes;
 
 public class MscorlibPatch : PatchType
 {
+    public MscorlibPatch(bool patchAllGroups) : base(patchAllGroups)
+    {
+    }
+
+    public MscorlibPatch()
+    {
+    }
 }
