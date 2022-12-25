bbe8a4900067c358ee75e14a324e6b1be6ec09ec
Eddio0141 <eddio0141@gmail.com>
2022-12-25 20:58:41 +0000

    added required methods

---
+++ Plugin/GameEnvironment/InnerState/FileSystem/IFileSystemManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+
+namespace UniTASPlugin.GameEnvironment.InnerState.FileSystem;
+
+public interface IFileSystemManager
+{
+    // TODO error reporting
+    void CreateDirectory(string path);
+    void DeleteDirectory(string path);
+    string[] GetFileSystemEntries(string path, string pathWithPattern, int attrs, int mask);
+    string CurrentDirectory { get; set; }
+
+    void MoveFile(string sourceFileName, string destFileName);
+    void CopyFile(string sourceFileName, string destFileName, bool overwrite);
+    void DeleteFile(string path);
+
+    void ReplaceFile(string sourceFileName, string destinationFileName, string destinationBackupFileName,
+        bool ignoreMetadataErrors);
+
+    FileAttributes GetFileAttributes(string path);
+    void SetFileAttributes(string path, FileAttributes fileAttributes);
+
+    FileType GetFileType(IntPtr handle);
+    FileStat GetFileStat(string path);
+
+    IntPtr Open(string path, FileMode mode, FileAccess access, FileShare share, FileOptions options);
+    void Close(IntPtr handle);
+
+    int Read(IntPtr handle, byte[] dest, int destOffset, int count);
+    int Write(IntPtr handle, in byte[] src, int srcOffset, int count);
+    long Seek(IntPtr handle, long offset, SeekOrigin origin);
+    void Flush(IntPtr handle);
+
+    long GetLength(IntPtr handle);
+    void SetLength(IntPtr handle, long length);
+
+    void SetFileTime(IntPtr handle, long creationTime, long lastAccessTime, long lastWriteTime);
+}
