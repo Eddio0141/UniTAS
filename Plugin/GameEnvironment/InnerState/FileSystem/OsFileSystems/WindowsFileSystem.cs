9be7a6dd5ad790da1896651e5648bc2408b89fa6
Eddio0141 <eddio0141@gmail.com>
2022-12-26 13:29:16 +0000

    updated invalid path chars

--- Plugin/GameEnvironment/InnerState/FileSystem/OsFileSystems/WindowsFileSystem.cs
+++ Plugin/GameEnvironment/InnerState/FileSystem/OsFileSystems/WindowsFileSystem.cs
@@ -18,15 +18,9 @@ public class WindowsFileSystem : FileSystem
 
     public override char[] InvalidPathChars => new[]
     {
-        '\u0022', '\u003C', '\u003E', '\u007C',
-        '\u0000', '\u0001', '\u0002', '\u0003',
-        '\u0004', '\u0005', '\u0006', '\u0007',
-        '\u0008', '\u0009', '\u000A', '\u000B',
-        '\u000C', '\u000D', '\u000E', '\u000F',
-        '\u0010', '\u0011', '\u0012', '\u0013',
-        '\u0014', '\u0015', '\u0016', '\u0017',
-        '\u0018', '\u0019', '\u001A', '\u001B',
-        '\u001C', '\u001D', '\u001E', '\u001F'
+        '"', '<', '>', '|', char.MinValue, '\u0001', '\u0002', '\u0003', '\u0004', '\u0005', '\u0006', '\a', '\b', '\t',
+        '\n', '\v', '\f', '\r', '\u000E', '\u000F', '\u0010', '\u0011', '\u0012', '\u0013', '\u0014', '\u0015',
+        '\u0016', '\u0017', '\u0018', '\u0019', '\u001A', '\u001B', '\u001C', '\u001D', '\u001E', '\u001F'
     };
 
     public override bool DirEqualsVolume => DirectorySeparatorChar == VolumeSeparatorChar;
