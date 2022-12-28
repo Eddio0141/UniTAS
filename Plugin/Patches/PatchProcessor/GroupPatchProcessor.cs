4add0a1cc689df6bdf600eefefaaafbfe8a4efd5
Eddio0141 <eddio0141@gmail.com>
2022-12-28 01:52:09 +0000

    added file

---
+++ Plugin/Patches/PatchProcessor/GroupPatchProcessor.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using HarmonyLib;
+using UniTASPlugin.Logger;
+using UniTASPlugin.Patches.PatchGroups;
+using UniTASPlugin.Patches.PatchTypes;
+
+namespace UniTASPlugin.Patches.PatchProcessor;
+
+public abstract class GroupPatchProcessor : PatchProcessor
+{
+    private readonly ILogger _logger;
+
+    protected GroupPatchProcessor(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    protected abstract Type TargetPatchType { get; }
+
+    /// Method to determine if the patch group should be used
+    protected abstract IEnumerable<int> ChoosePatch(ModulePatchType patchType, PatchGroup[] patchGroups);
+
+    public override IEnumerable<KeyValuePair<int, Type>> ProcessModules()
+    {
+        // TODO replace this later
+        var pluginAssembly = typeof(Plugin).Assembly;
+
+        // list of patch groups, patch group attributes, and PatchTypes for each modules
+        var patchGroupsAttributesAndPatchType =
+            new List<KeyValuePair<KeyValuePair<Type[], PatchGroup[]>, ModulePatchType>>();
+        foreach (var type in pluginAssembly.GetTypes())
+        {
+            var attributes = type.GetCustomAttributes(TargetPatchType, false);
+            if (attributes.Length == 0) continue;
+
+            _logger.LogInfo($"Found patch module {type.FullName}");
+
+            var patchGroups = new List<Type>();
+            var patchGroupAttributes = new List<PatchGroup>();
+
+            foreach (var innerType in type.GetNestedTypes(AccessTools.all))
+            {
+                var innerAttributes = innerType.GetCustomAttributes(typeof(PatchGroup), false);
+                if (innerAttributes.Length == 0) continue;
+
+                patchGroups.Add(innerType);
+                patchGroupAttributes.Add(innerAttributes[0] as PatchGroup);
+            }
+
+            patchGroupsAttributesAndPatchType.Add(new(new(patchGroups.ToArray(), patchGroupAttributes.ToArray()),
+                (ModulePatchType)attributes[0]));
+        }
+
+        foreach (var patchGroupsAndPatchType in patchGroupsAttributesAndPatchType)
+        {
+            var patchType = patchGroupsAndPatchType.Value;
+            var patchGroupsAndAttributes = patchGroupsAndPatchType.Key;
+
+            var chosenIndexes = ChoosePatch(patchType, patchGroupsAndAttributes.Value);
+
+            foreach (var chosenIndex in chosenIndexes)
+            {
+                var patchGroup = patchGroupsAndAttributes.Key[chosenIndex];
+
+                _logger.LogInfo($"Using patch group {patchGroup.FullName}");
+
+                yield return new(patchType.Priority, patchGroup);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Helper function to convert a version string to a comparable integer
+    /// </summary>
+    /// <param name="version">Version number, all characters that is a . is removed, all non-numeric characters are set to 0</param>
+    /// <param name="fallback">Fallback value in case the operation "fails"</param>
+    /// <returns>A equal version number</returns>
+    protected static ulong VersionStringToNumber(string version, ulong fallback = 0)
+    {
+        if (version == null) return fallback;
+
+        version = version.Replace(".", "");
+
+        // replace all non-numeric characters with 0
+        var versionNumber = ulong.Parse(Regex.Replace(version, "[^0-9]", "0"));
+
+        return versionNumber;
+    }
+}
