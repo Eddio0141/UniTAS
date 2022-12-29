66a9094f97c0ac3585114b4313a3a04609e203a2
Eddio0141 <eddio0141@gmail.com>
2022-12-29 21:41:24 +0000

    added lambda method log patch

--- Plugin/Patches/RawPatches/BepInExLoggingReversePatch.cs
+++ Plugin/Patches/RawPatches/BepInExLoggingReversePatch.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Reflection;
 using BepInEx.Logging;
 using HarmonyLib;
@@ -23,13 +24,31 @@ public class BepInExLoggingReversePatch
     {
         private static IEnumerable<MethodBase> TargetMethods()
         {
-            // ConsoleLogListener.LogEvent
-            // DiskLogListener.LogEvent
-            // DiskLogListener.Dispose
-            // UnityLogListener.LogEvent
+            // try find the lambda method in DiskLogListener
+            // private instance void '<.ctor>b__16_0'(object o)
+            var diskLogListenerLambdas = AccessTools.GetDeclaredMethods(typeof(DiskLogListener)).Where(m =>
+                m.IsPrivate && !m.IsStatic && m.ReturnType == typeof(void) && m.GetParameters().Length == 1 &&
+                m.GetParameters()[0].ParameterType == typeof(object)).ToList();
+            if (diskLogListenerLambdas.Count == 1)
+            {
+                yield return diskLogListenerLambdas[0];
+            }
+            else
+            {
+                // further filter by name
+                var diskLogListenerLambda = diskLogListenerLambdas.FirstOrDefault(m => m.Name == "<.ctor>b__16_0");
+                if (diskLogListenerLambda != null)
+                {
+                    yield return diskLogListenerLambda;
+                }
+                else
+                {
+                    yield return diskLogListenerLambdas.FirstOrDefault(m => m.Name.StartsWith("<.ctor>b__"));
+                }
+            }
 
-            yield return AccessTools.Method(typeof(ConsoleLogListener), nameof(ConsoleLogListener.LogEvent));
             yield return AccessTools.Method(typeof(DiskLogListener), nameof(DiskLogListener.LogEvent));
+            yield return AccessTools.Method(typeof(ConsoleLogListener), nameof(ConsoleLogListener.LogEvent));
             yield return AccessTools.Method(typeof(DiskLogListener), nameof(DiskLogListener.Dispose));
             yield return AccessTools.Method(typeof(UnityLogListener), nameof(UnityLogListener.LogEvent));
         }
