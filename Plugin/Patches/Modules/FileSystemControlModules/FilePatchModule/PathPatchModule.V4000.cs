342eab14ca318d3c6555004f5df54d3869cae5c5
Eddio0141 <eddio0141@gmail.com>
2023-01-04 23:37:09 +0000

    commented out test patches

--- Plugin/Patches/Modules/FileSystemControlModules/FilePatchModule/PathPatchModule.V4000.cs
+++ Plugin/Patches/Modules/FileSystemControlModules/FilePatchModule/PathPatchModule.V4000.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Reflection;
@@ -72,24 +71,24 @@ public partial class PathPatchModule
             }
         }
 
-        [HarmonyPatch]
-        private class Test
-        {
-            private static MethodBase TargetMethod()
-            {
-                return AccessTools.Constructor(typeof(FileStream), new[]
-                {
-                    typeof(string), typeof(FileMode), typeof(FileAccess), typeof(FileShare), typeof(int), typeof(bool),
-                    typeof(FileOptions)
-                });
-            }
-
-            private static void Prefix(string path)
-            {
-                Plugin.Log.LogDebug(
-                    $"path: {path}, invalid chars count: {Path.InvalidPathChars.Length}, check: {path.IndexOfAny(Path.InvalidPathChars)}");
-            }
-        }
+        // [HarmonyPatch]
+        // private class Test
+        // {
+        //     private static MethodBase TargetMethod()
+        //     {
+        //         return AccessTools.Constructor(typeof(FileStream), new[]
+        //         {
+        //             typeof(string), typeof(FileMode), typeof(FileAccess), typeof(FileShare), typeof(int), typeof(bool),
+        //             typeof(FileOptions)
+        //         });
+        //     }
+        //
+        //     private static void Prefix(string path)
+        //     {
+        //         Plugin.Log.LogDebug(
+        //             $"path: {path}, invalid chars count: {Path.InvalidPathChars.Length}, check: {path.IndexOfAny(Path.InvalidPathChars)}");
+        //     }
+        // }
 
         // [HarmonyPatch]
         // private class Test5
@@ -123,70 +122,70 @@ public partial class PathPatchModule
         //     }
         // }
         //
-        [HarmonyPatch]
-        private class Test3
-        {
-            private static MethodBase TargetMethod()
-            {
-                return AccessTools.Method(typeof(Path), "WindowsDriveAdjustment");
-            }
-
-            private static bool Prefix(string path, ref string __result)
-            {
-                Plugin.Log.LogDebug(
-                    $"before WindowsDriveAdjustment: {path}, path invalid char count: {Path.InvalidPathChars.Length}");
-                Plugin.Log.LogDebug(new StackTrace());
-
-                if (path.Length < 2)
-                {
-                    __result = path.Length == 1 && (path[0] == '\\' || path[0] == '/')
-                        ? Path.GetPathRoot(Directory.GetCurrentDirectory())
-                        : path;
-                    Plugin.Log.LogDebug($"after WindowsDriveAdjustment: {__result}");
-                    return false;
-                }
-
-                if (path[1] != ':' || !char.IsLetter(path[0]))
-                {
-                    __result = path;
-                    Plugin.Log.LogDebug($"after WindowsDriveAdjustment: {__result}");
-                    return false;
-                }
-
-                var currentDirectory = (string)typeof(Directory)
-                    .GetMethod("InsecureGetCurrentDirectory", BindingFlags.Static | BindingFlags.NonPublic)
-                    .Invoke(null, null);
-
-                var getFullPathName = AccessTools.Method(typeof(Path), "GetFullPathName", new[] { typeof(string) });
-
-                if (path.Length == 2)
-                {
-                    MonoIOPatchModule.Log.Add("WindowsDriveAdjustment: path.Length == 2");
-                    path = currentDirectory[0] != path[0]
-                        ? (string)getFullPathName.Invoke(null, new object[] { path })
-                        : currentDirectory;
-                }
-                else if (path[2] != Path.DirectorySeparatorChar &&
-                         path[2] != Path.AltDirectorySeparatorChar)
-                {
-                    MonoIOPatchModule.Log.Add(
-                        "WindowsDriveAdjustment: path[2] != Path.DirectorySeparatorChar && path[2] != Path.AltDirectorySeparatorChar");
-                    path = currentDirectory[0] != path[0]
-                        ? (string)getFullPathName.Invoke(null, new object[] { path })
-                        : Path.Combine(currentDirectory, path.Substring(2, path.Length - 2));
-                }
-
-                __result = path;
-                Plugin.Log.LogDebug($"after WindowsDriveAdjustment: {__result}");
-                return false;
-            }
-
-            private static void Postfix(ref string __result)
-            {
-                Plugin.Log.LogDebug(
-                    $"after WindowsDriveAdjustment: {__result}, path invalid char count: {Path.InvalidPathChars.Length}");
-            }
-        }
+        // [HarmonyPatch]
+        // private class Test3
+        // {
+        //     private static MethodBase TargetMethod()
+        //     {
+        //         return AccessTools.Method(typeof(Path), "WindowsDriveAdjustment");
+        //     }
+        //
+        //     private static bool Prefix(string path, ref string __result)
+        //     {
+        //         Plugin.Log.LogDebug(
+        //             $"before WindowsDriveAdjustment: {path}, path invalid char count: {Path.InvalidPathChars.Length}");
+        //         Plugin.Log.LogDebug(new StackTrace());
+        //
+        //         if (path.Length < 2)
+        //         {
+        //             __result = path.Length == 1 && (path[0] == '\\' || path[0] == '/')
+        //                 ? Path.GetPathRoot(Directory.GetCurrentDirectory())
+        //                 : path;
+        //             Plugin.Log.LogDebug($"after WindowsDriveAdjustment: {__result}");
+        //             return false;
+        //         }
+        //
+        //         if (path[1] != ':' || !char.IsLetter(path[0]))
+        //         {
+        //             __result = path;
+        //             Plugin.Log.LogDebug($"after WindowsDriveAdjustment: {__result}");
+        //             return false;
+        //         }
+        //
+        //         var currentDirectory = (string)typeof(Directory)
+        //             .GetMethod("InsecureGetCurrentDirectory", BindingFlags.Static | BindingFlags.NonPublic)
+        //             .Invoke(null, null);
+        //
+        //         var getFullPathName = AccessTools.Method(typeof(Path), "GetFullPathName", new[] { typeof(string) });
+        //
+        //         if (path.Length == 2)
+        //         {
+        //             MonoIOPatchModule.Log.Add("WindowsDriveAdjustment: path.Length == 2");
+        //             path = currentDirectory[0] != path[0]
+        //                 ? (string)getFullPathName.Invoke(null, new object[] { path })
+        //                 : currentDirectory;
+        //         }
+        //         else if (path[2] != Path.DirectorySeparatorChar &&
+        //                  path[2] != Path.AltDirectorySeparatorChar)
+        //         {
+        //             MonoIOPatchModule.Log.Add(
+        //                 "WindowsDriveAdjustment: path[2] != Path.DirectorySeparatorChar && path[2] != Path.AltDirectorySeparatorChar");
+        //             path = currentDirectory[0] != path[0]
+        //                 ? (string)getFullPathName.Invoke(null, new object[] { path })
+        //                 : Path.Combine(currentDirectory, path.Substring(2, path.Length - 2));
+        //         }
+        //
+        //         __result = path;
+        //         Plugin.Log.LogDebug($"after WindowsDriveAdjustment: {__result}");
+        //         return false;
+        //     }
+        //
+        //     private static void Postfix(ref string __result)
+        //     {
+        //         Plugin.Log.LogDebug(
+        //             $"after WindowsDriveAdjustment: {__result}, path invalid char count: {Path.InvalidPathChars.Length}");
+        //     }
+        // }
         //
         // [HarmonyPatch]
         // private class Test4
