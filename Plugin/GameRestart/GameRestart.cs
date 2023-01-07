4342bdf003a49f51e84fde7ac685461e701e50a2
Eddio0141 <eddio0141@gmail.com>
2023-01-07 01:18:48 +0000

    added generic class field discovery

--- Plugin/GameRestart/GameRestart.cs
+++ Plugin/GameRestart/GameRestart.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
+using Mono.Cecil;
+using MonoMod.Utils;
 using UniTASPlugin.FixedUpdateSync;
 using UniTASPlugin.GameEnvironment;
 using UniTASPlugin.Interfaces.StartEvent;
@@ -77,10 +79,11 @@ public class GameRestart : IGameRestart, IOnAwake, IOnEnable, IOnStart, IOnFixed
         }
     }
 
-    // TODO remove this when we have a better way to handle static fields
     private void StoreStaticFields()
     {
         var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var genericType = new List<Type>();
+
         foreach (var assembly in assemblies)
         {
             if (!gameAssemblyNames.Contains(assembly.GetName().Name))
@@ -92,12 +95,98 @@ public class GameRestart : IGameRestart, IOnAwake, IOnEnable, IOnStart, IOnFixed
 
             foreach (var type in types)
             {
-                var fields = type.GetFields(AccessTools.all).Where(x => x.IsStatic && !x.IsLiteral).ToArray();
+                // if type has generic parameters
+                if (type.IsGenericType)
+                {
+                    genericType.Add(type);
+                    continue;
+                }
+
+                var fields = AccessTools.GetDeclaredFields(type).Where(x =>
+                    x.IsStatic && !x.IsLiteral).ToArray();
+
                 if (fields.Length == 0) continue;
 
                 _logger.LogDebug(
                     $"type: {type.FullName} has {fields.Length} static fields, and static constructor: {type.TypeInitializer != null}");
                 _staticFields.Add(new(fields, type.TypeInitializer));
+            }
+        }
+
+        if (genericType.Count == 0) return;
+
+        // find all types for each generic type
+        var assemblyDefinitions = new List<AssemblyDefinition>();
+        foreach (var assembly in assemblies)
+        {
+            try
+            {
+                assemblyDefinitions.Add(AssemblyDefinition.ReadAssembly(assembly.Location));
+            }
+            catch (Exception e)
+            {
+                // ignore
+                _logger.LogWarning($"Failed to read assembly {assembly.FullName} with error: {e.Message}");
+            }
+        }
+
+        // Link of generic types, and list of generic args (so a list of lists)
+        var genericTypeUsedGenericTypes = new Dictionary<Type, List<List<Type>>>();
+
+        // this is cursed but it works
+        // iterates through all il instructions in all methods in all types in all assemblies to find usage of generic types
+        foreach (var instruction in from assemblyDefinition in assemblyDefinitions
+                 from moduleDefinition in assemblyDefinition.Modules
+                 from typeDefinition in moduleDefinition.Types
+                 from methodDefinition in typeDefinition.Methods
+                 where methodDefinition.HasBody
+                 from instruction in methodDefinition.Body.Instructions
+                 select instruction)
+        {
+            if (instruction.Operand is not MemberReference
+                {
+                    DeclaringType: GenericInstanceType genericInstanceType
+                }) continue;
+
+            // if the generic type is not in the list of generic types we are looking for, skip
+            var actualType = genericInstanceType.ElementType.ResolveReflection();
+            var genericTypeFound = genericType.FirstOrDefault(x => x == actualType);
+
+            if (genericTypeFound == null) continue;
+
+            var allGenericTypes = genericInstanceType.GenericArguments
+                .Select(x => x.ResolveReflection()).ToList();
+
+            // add if allGenericTypes is not already in the list
+            if (!genericTypeUsedGenericTypes.TryGetValue(genericTypeFound, out var list))
+            {
+                genericTypeUsedGenericTypes.Add(genericTypeFound, new() { allGenericTypes });
+            }
+            else
+            {
+                if (list.Any(x => x.SequenceEqual(allGenericTypes))) continue;
+                genericTypeUsedGenericTypes[genericTypeFound].Add(allGenericTypes);
+            }
+        }
+
+        // now we have a list of all generic types and their generic args
+        // iterate through each generic type and go through the static field process again
+        foreach (var typeUsedGenericTypes in genericTypeUsedGenericTypes)
+        {
+            var type = typeUsedGenericTypes.Key;
+            var genericArgs = typeUsedGenericTypes.Value;
+
+            foreach (var args in genericArgs)
+            {
+                var genericTypeDefinition = type.MakeGenericType(args.ToArray());
+                var fields = AccessTools.GetDeclaredFields(genericTypeDefinition).Where(x =>
+                    x.IsStatic && !x.IsLiteral).ToArray();
+
+                if (fields.Length == 0) continue;
+
+                _logger.LogDebug(
+                    $"type: {genericTypeDefinition.FullName} has {fields.Length} static fields, and static constructor: {genericTypeDefinition.TypeInitializer != null}");
+                _staticFields.Add(new(fields, genericTypeDefinition.TypeInitializer));
             }
         }
     }
@@ -109,10 +198,14 @@ public class GameRestart : IGameRestart, IOnAwake, IOnEnable, IOnStart, IOnFixed
         {
             foreach (var field in staticFields.Fields)
             {
+                _logger.LogDebug(
+                    $"Setting static field: {field.DeclaringType?.FullName ?? "unknown_type"}.{field.Name} to null");
                 field.SetValue(null, null);
             }
 
-            staticFields.StaticConstructor?.Invoke(null);
+            _logger.LogDebug(
+                $"Invoking static constructor for {staticFields.StaticConstructor?.DeclaringType?.FullName ?? "unknown_type"}");
+            staticFields.StaticConstructor?.Invoke(null, null);
         }
     }
 
@@ -124,8 +217,8 @@ public class GameRestart : IGameRestart, IOnAwake, IOnEnable, IOnStart, IOnFixed
     {
         PendingRestart = true;
         _softRestartTime = time;
-        DestroyDontDestroyOnLoads();
         StopScriptExecution();
+        DestroyDontDestroyOnLoads();
         SetStaticFields();
         OnGameRestart();
         _syncFixedUpdate.OnSync(SoftRestartOperation, 1);
