3ff5434be01d8adb9f5cf165679ea8c17d646980
Eddio0141 <eddio0141@gmail.com>
2023-01-13 19:17:30 +0000

    fix build files

--- build.sh
+++ build.sh
@@ -8,44 +8,18 @@ else
     BUILD_TYPE=$1
 fi
 
-# Dotnet build projects paths
-PROJECTS=(
-    "Plugin"
-    "Patcher"
-)
+OUTPUT_DIR="build/$BUILD_TYPE"
+OUTPUT_PLUGIN_DIR="$OUTPUT_DIR/plugins"
+SOURCE_PLUGIN_EXTERNS_DIR="Plugin/Extern-Assemblies"
 
-# Build projects
-for PROJECT in "${PROJECTS[@]}"; do
-    echo "Building $PROJECT"
-
-    # If patcher and $BUILD_TYPE is ReleaseTrace, build with Release
-    if [ "$PROJECT" = "Patcher" ] && [ "$BUILD_TYPE" = "ReleaseTrace" ]; then
-        dotnet build "$PROJECT" -c "Release"
-    else
-        dotnet build "$PROJECT" -c $BUILD_TYPE
-    fi
-done
+# Dotnet builds
+dotnet build "Plugin" -c $BUILD_TYPE
 
 echo "Copying dlls to output folders"
 
-OUTPUT_DIR="build/$BUILD_TYPE"
-OUTPUT_PLUGIN_DIR="$OUTPUT_DIR/Plugin"
-OUTPUT_PATCHER_DIR="$OUTPUT_DIR/Patcher"
-
-SOURCE_PLUGIN_DIR="Plugin/bin/$BUILD_TYPE/net35"
-if [ "$BUILD_TYPE" = "ReleaseTrace" ]; then
-    SOURCE_PATCHER_DIR="Patcher/bin/Release/net35"
-else
-    SOURCE_PATCHER_DIR="Patcher/bin/$BUILD_TYPE/net35"
-fi
-
-SOURCE_PLUGIN_EXTERNS_DIR="Plugin/Extern-Assemblies"
-
 # Create output directories
 mkdir -p "$OUTPUT_PLUGIN_DIR"
-mkdir -p "$OUTPUT_PATCHER_DIR"
 
 # Only copy dlls
-cp "$SOURCE_PLUGIN_DIR"/*.dll "$OUTPUT_PLUGIN_DIR"
-cp "$SOURCE_PATCHER_DIR"/*.dll "$OUTPUT_PATCHER_DIR"
+cp "Plugin/bin/$BUILD_TYPE/net35"/*.dll "$OUTPUT_PLUGIN_DIR"
 cp "$SOURCE_PLUGIN_EXTERNS_DIR"/*.dll "$OUTPUT_PLUGIN_DIR"
