e3c4a5ebca71c8d11477d836307df7346395c017
Eddio0141 <eddio0141@gmail.com>
2023-01-21 01:57:36 +0000

    implemented stop coroutine all

--- Plugin/MonoBehCoroutineEndOfFrameTracker/IEndOfFrameTracker.cs
+++ Plugin/MonoBehCoroutineEndOfFrameTracker/IEndOfFrameTracker.cs
@@ -8,7 +8,9 @@ public interface IEndOfFrameTracker
         string stringCoroutineMethodName = null);
 
     void MoveNextInvoke(IEnumerator enumerator);
+
     void CoroutineEnd(IEnumerator enumerator);
     void CoroutineEnd(object monoBehaviourInstance, string coroutineMethodName);
     void CoroutineEnd(object coroutine);
+    void CoroutineEndAll(object monoBehaviourInstance);
 }
