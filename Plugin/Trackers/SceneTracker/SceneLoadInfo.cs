3adb416d5f8680a58383cbf923c90a353bd5f8a7
Eddio0141 <eddio0141@gmail.com>
2023-01-25 09:24:04 +0000

    add extra info on scene load callback

---
+++ Plugin/Trackers/SceneTracker/SceneLoadInfo.cs
@@ -0,0 +1,13 @@
+namespace UniTASPlugin.Trackers.SceneTracker;
+
+public class SceneLoadInfo
+{
+    public SceneInfo SceneInfo { get; }
+    public bool Additive { get; }
+
+    public SceneLoadInfo(SceneInfo sceneInfo, bool additive)
+    {
+        SceneInfo = sceneInfo;
+        Additive = additive;
+    }
+}
