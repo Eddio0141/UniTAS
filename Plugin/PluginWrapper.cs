4d7ff1de98dd4d2050ff01edea20cd7eaf942b1c
Eddio0141 <eddio0141@gmail.com>
2023-01-27 07:51:08 +0000

    moved update logic to different class

--- Plugin/PluginWrapper.cs
+++ Plugin/PluginWrapper.cs
@@ -2,49 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using HarmonyLib;
+using UniTASPlugin.GameEnvironment;
+using UniTASPlugin.GameInfo;
 using UniTASPlugin.GameRestart;
-using UniTASPlugin.Interfaces.StartEvent;
-using UniTASPlugin.Interfaces.Update;
-using UniTASPlugin.Patches.PatchProcessor;
+using UniTASPlugin.LegacySafeWrappers;
+using UniTASPlugin.Logger;
 using UniTASPlugin.ReverseInvoker;
-#if TRACE
-using UniTASPlugin.Patches.Modules.FileSystemControlModules.FilePatchModule;
-#endif
+using UnityEngine;
+using PatchProcessor = UniTASPlugin.Patches.PatchProcessor.PatchProcessor;
 
 namespace UniTASPlugin;
 
 // ReSharper disable once ClassNeverInstantiated.Global
 public class PluginWrapper
 {
-    private bool _updated;
-    private bool _calledPreUpdate;
-
-    private readonly IOnUpdate[] _onUpdates;
-    private readonly IOnFixedUpdate[] _onFixedUpdates;
-    private readonly IOnAwake[] _onAwakes;
-    private readonly IOnStart[] _onStarts;
-    private readonly IOnEnable[] _onEnables;
-    private readonly IOnPreUpdates[] _onPreUpdates;
-
-    public PluginWrapper(IEnumerable<IOnUpdate> onUpdates, IEnumerable<IOnFixedUpdate> onFixedUpdates,
-        IEnumerable<IOnAwake> onAwakes, IEnumerable<IOnStart> onStarts, IEnumerable<IOnEnable> onEnables,
-        IEnumerable<IOnPreUpdates> onPreUpdates,
-        IEnumerable<PatchProcessor> patchProcessors, IEnumerable<IOnGameRestart> onGameRestarts,
-        IReverseInvokerFactory reverseInvokerFactory)
+    public PluginWrapper(IEnumerable<PatchProcessor> patchProcessors, IEnumerable<IOnGameRestart> onGameRestarts,
+        IReverseInvokerFactory reverseInvokerFactory, IGameInfo gameInfo, ILogger logger,
+        IVirtualEnvironmentFactory virtualEnvironmentFactory)
     {
-        _onFixedUpdates = onFixedUpdates.ToArray();
-        _onAwakes = onAwakes.ToArray();
-        _onStarts = onStarts.ToArray();
-        _onEnables = onEnables.ToArray();
-        _onPreUpdates = onPreUpdates.ToArray();
-        _onUpdates = onUpdates.ToArray();
+        logger.LogInfo($"Internally found unity version: {gameInfo.UnityVersion}");
+        logger.LogInfo($"Game product name: {gameInfo.ProductName}");
+        logger.LogDebug($"Mscorlib version: {gameInfo.MscorlibVersion}");
+        logger.LogDebug($"Netstandard version: {gameInfo.NetStandardVersion}");
+        // TODO complete fixing this
+        var companyNameProperty = Traverse.Create(typeof(Application)).Property("companyName");
+        if (companyNameProperty.PropertyExists())
+            logger.LogInfo(
+                $"Game company name: {companyNameProperty.GetValue<string>()}"); //product name: {Application.productName}, version: {Application.version}");
 
-        Trace.Write($"Registered OnUpdate count: {_onUpdates.Length}");
-        Trace.Write($"Registered OnFixedUpdate count: {_onFixedUpdates.Length}");
-        Trace.Write($"Registered OnAwake count: {_onAwakes.Length}");
-        Trace.Write($"Registered OnStart count: {_onStarts.Length}");
-        Trace.Write($"Registered OnEnable count: {_onEnables.Length}");
-        Trace.Write($"Registered OnPreUpdate count: {_onPreUpdates.Length}");
+        // TODO all axis names for help
+
+        // init random seed
+        // TODO make this happen after 
+        var env = virtualEnvironmentFactory.GetVirtualEnv();
+        RandomWrap.InitState((int)env.Seed);
+
+        logger.LogInfo($"System time: {DateTime.Now}");
+        logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
 
         var rev = reverseInvokerFactory.GetReverseInvoker();
         var actualTime = rev.Invoke(() => DateTime.Now);
@@ -58,6 +53,7 @@ public class PluginWrapper
             .Select(x => x.Value);
         foreach (var patch in sortedPatches)
         {
+            Trace.Write($"Patching {patch} post game restart");
             Plugin.Harmony.PatchAll(patch);
         }
 
@@ -68,96 +64,33 @@ public class PluginWrapper
         // AccessTools.Constructor(typeof(System.IO.Path), searchForStatic: true).Invoke(null, null);
     }
 
-    // calls awake before any other script
-    public void Awake()
-    {
-        foreach (var onAwake in _onAwakes)
-        {
-            onAwake.Awake();
-        }
-    }
-
-    // calls onEnable before any other script
-    public void OnEnable()
-    {
-        foreach (var onEnable in _onEnables)
-        {
-            onEnable.OnEnable();
-        }
-    }
-
-    // calls start before any other script
-    public void Start()
-    {
-        foreach (var onStart in _onStarts)
-        {
-            onStart.Start();
-        }
-    }
-
-    public void Update()
-    {
-        if (_updated) return;
-        _updated = true;
-
-        CallOnPreUpdate();
-
-        foreach (var update in _onUpdates)
-        {
-            update.Update();
-        }
-
-        //Overlay.Update();
-        //GameCapture.Update();
-    }
-
-#if TRACE
-    private static void MonoIOPatchModuleTracePrints()
-    {
-        var logCount = MonoIOPatchModule.Log.Count;
-        for (var i = 0; i < logCount; i++)
-        {
-            var log = MonoIOPatchModule.Log[i];
-            Trace.Write(log);
-        }
-
-        MonoIOPatchModule.Log.RemoveRange(0, logCount);
-    }
-#endif
-
-    // right now I don't call this update before other scripts so I don't need to check if it was already called
-    public void LateUpdate()
-    {
-        _updated = false;
-        _calledPreUpdate = false;
-    }
-
-    public void PreFixedUpdate()
-    {
-        CallOnPreUpdate();
-    }
-
-    // right now I don't call this update before other scripts so I don't need to check if it was already called
-    public void FixedUpdate()
-    {
-        foreach (var update in _onFixedUpdates)
-        {
-            update.FixedUpdate();
-        }
-    }
-
-    private void CallOnPreUpdate()
-    {
-        if (_calledPreUpdate) return;
-        _calledPreUpdate = true;
-
-#if TRACE
-        MonoIOPatchModuleTracePrints();
-#endif
-
-        foreach (var onPreUpdate in _onPreUpdates)
-        {
-            onPreUpdate.PreUpdate();
-        }
-    }
+// #if TRACE
+//     private static void MonoIOPatchModuleTracePrints()
+//     {
+//         var logCount = MonoIOPatchModule.Log.Count;
+//         for (var i = 0; i < logCount; i++)
+//         {
+//             var log = MonoIOPatchModule.Log[i];
+//             Trace.Write(log);
+//         }
+//
+//         MonoIOPatchModule.Log.RemoveRange(0, logCount);
+//     }
+// #endif
+//     
+//
+//     private void CallOnPreUpdate()
+//     {
+//         if (_calledPreUpdate) return;
+//         _calledPreUpdate = true;
+//
+// #if TRACE
+//         MonoIOPatchModuleTracePrints();
+// #endif
+//
+//         foreach (var onPreUpdate in _onPreUpdates)
+//         {
+//             onPreUpdate.PreUpdate();
+//         }
+//     }
 }
