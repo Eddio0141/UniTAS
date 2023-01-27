b3cb567be1eb29dd99805db37949902d7285fb80
Eddio0141 <eddio0141@gmail.com>
2023-01-27 07:50:42 +0000

    patch processor for when plugin load

---
+++ Plugin/Patches/PatchProcessor/OnPluginInitProcessor.cs
@@ -0,0 +1,5 @@
+namespace UniTASPlugin.Patches.PatchProcessor;
+
+public abstract class OnPluginInitProcessor : PatchProcessor
+{
+}
---
+++ Plugin/Patches/PatchTypes/RawPatchOnPluginInit.cs
@@ -0,0 +1,8 @@
+namespace UniTASPlugin.Patches.PatchTypes;
+
+public class RawPatchOnPluginInit : PatchType
+{
+    public RawPatchOnPluginInit(int priority = 0) : base(priority)
+    {
+    }
+}
