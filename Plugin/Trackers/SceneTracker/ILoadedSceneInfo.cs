6e968e2310679c6bf4f2784657b01950304b724f
Eddio0141 <eddio0141@gmail.com>
2023-01-27 03:54:21 +0000

    added extra callback

--- Plugin/Trackers/SceneTracker/ILoadedSceneInfo.cs
+++ Plugin/Trackers/SceneTracker/ILoadedSceneInfo.cs
@@ -5,4 +5,5 @@ namespace UniTASPlugin.Trackers.SceneTracker;
 public interface ILoadedSceneInfo
 {
     void SubscribeOnSceneLoad(Action<SceneLoadInfo> callback);
+    void SubscribeOnSceneUnload(Action<SceneInfo> callback);
 }
