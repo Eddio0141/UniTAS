b9eb982abb69a64564910b7f3c27551306089ac2
Eddio0141 <eddio0141@gmail.com>
2023-01-31 14:29:02 +0000

    get local versions in dir

---
+++ setup_gui/src/utils/local_versions.rs
@@ -0,0 +1,44 @@
+//! Get version info for semantically versioned dir names in a dir.
+
+use std::{fmt::Display, path::Path, str::FromStr};
+
+use semver::Version;
+
+pub struct LocalVersions {
+    pub versions: Vec<Version>,
+}
+
+impl LocalVersions {
+    pub fn from_dir(dir: &Path) -> crate::prelude::Result<Self> {
+        let mut versions = Vec::new();
+
+        for entry in dir.read_dir()? {
+            let path = entry?.path();
+
+            if path.is_dir() {
+                let Some(file_name) = path.file_name() else {
+                    continue;
+                };
+                let Some(file_name) = file_name.to_str() else {
+                    continue;
+                };
+                let version = Version::from_str(file_name)?;
+                versions.push(version);
+            }
+        }
+
+        versions.sort();
+
+        Ok(Self { versions })
+    }
+}
+
+impl Display for LocalVersions {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for version in &self.versions {
+            writeln!(f, "{}", version)?;
+        }
+
+        Ok(())
+    }
+}
