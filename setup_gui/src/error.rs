deb11a7b319d911f8e713e104e2b0e342104bb7f
Eddio0141 <eddio0141@gmail.com>
2023-01-31 15:24:10 +0000

    added new errors

--- setup_gui/src/error.rs
+++ setup_gui/src/error.rs
@@ -1,6 +1,15 @@
 //! Main errors
 
+use crate::utils::{history, paths};
+
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Semver error: {0}")]
+    Semver(#[from] semver::Error),
+    #[error(transparent)]
+    PathError(#[from] paths::error::Error),
+    #[error(transparent)]
+    HistoryError(#[from] history::error::Error),
 }
