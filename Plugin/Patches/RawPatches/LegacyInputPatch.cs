88099b79e903be128e88a89e6d13a5005c07dea0
Eddio0141 <eddio0141@gmail.com>
2023-02-01 05:00:54 +0000

    moved patches over

--- Plugin/Patches/RawPatches/LegacyInputPatch.cs
+++ Plugin/Patches/RawPatches/LegacyInputPatch.cs
@@ -140,4 +140,206 @@ public class LegacyInputPatch
             return false;
         }
     }
+
+    // above gets called from gyro { get; }
+    /*
+    public static Gyroscope gyro
+    {
+        get
+        {
+            if (Input.m_MainGyro == null)
+            {
+                Input.m_MainGyro = new Gyroscope(Input.mainGyroIndex_Internal());
+            }
+            return Input.m_MainGyro;
+        }
+    }
+    */
+
+
+    [HarmonyPatch(typeof(Input), nameof(Input.GetAxis))]
+    private class GetAxis
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(string axisName, ref float __result)
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            if (env.InputState.AxisState.Values.TryGetValue(axisName, out var value))
+            {
+                __result = value;
+            }
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.GetAxisRaw))]
+    private class GetAxisRaw
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(string axisName, ref float __result)
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            if (env.InputState.AxisState.Values.TryGetValue(axisName, out var value))
+            {
+                // TODO whats diff between Raw and normal
+                __result = value;
+            }
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.mousePosition), MethodType.Getter)]
+    private class mousePositionGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref Vector3 __result)
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            var mouseState = env.InputState.MouseState;
+            __result = new(mouseState.XPos, mouseState.YPos);
+            return false;
+        }
+    }
+
+#pragma warning disable Harmony002 // why does unity name things like this
+    [HarmonyPatch(typeof(Input), "get_mousePosition_Injected", MethodType.Normal)]
+#pragma warning restore Harmony002
+    private class get_mousePosition_Injected
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref Vector3 ret)
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            var mouseState = env.InputState.MouseState;
+            ret = new(mouseState.XPos, mouseState.YPos);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.GetMouseButton))]
+    private class GetMouseButton
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result, int button)
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            __result = button switch
+            {
+                0 => env.InputState.MouseState.LeftClick,
+                1 => env.InputState.MouseState.RightClick,
+                2 => env.InputState.MouseState.MiddleClick,
+                _ => false
+            };
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.GetMouseButtonDown))]
+    private class GetMouseButtonDown
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result, int button)
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            __result = button switch
+            {
+                0 => env.InputState.MouseState.LeftClickDown,
+                1 => env.InputState.MouseState.RightClickDown,
+                2 => env.InputState.MouseState.MiddleClickDown,
+                _ => false
+            };
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.GetMouseButtonUp))]
+    private class GetMouseButtonUp
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result, int button)
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            __result = button switch
+            {
+                0 => env.InputState.MouseState.LeftClickUp,
+                1 => env.InputState.MouseState.RightClickUp,
+                2 => env.InputState.MouseState.MiddleClickUp,
+                _ => false
+            };
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.ResetInputAxes))]
+    private class ResetInputAxes
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            if (ReverseInvokerFactory.GetReverseInvoker().Invoking)
+                return true;
+            // TODO make this work
+            // Resets all input. After ResetInputAxes all axes return to 0 and all buttons return to 0 for one frame.
+            // TODO also make sure movie overwrites input on the same frame after reset
+            var env = VirtualEnvironmentFactory.GetVirtualEnv();
+            if (!env.RunVirtualEnvironment) return true;
+            env.InputState.AxisState.Values.Clear();
+            return false;
+        }
+    }
 }
