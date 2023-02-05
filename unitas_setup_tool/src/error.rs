64c36a6769fc2776e92bb45e90e1b20fa726e8b5
Eddio0141 <eddio0141@gmail.com>
2023-02-05 02:10:59 +0000

    cleaned up unused dependencies

--- unitas_setup_tool/src/error.rs
+++ unitas_setup_tool/src/error.rs
@@ -6,8 +6,6 @@ use crate::utils::*;
 pub enum Error {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
-    #[error("Semver error: {0}")]
-    Semver(#[from] semver::Error),
     #[error(transparent)]
     PathError(#[from] paths::error::Error),
     #[error(transparent)]
