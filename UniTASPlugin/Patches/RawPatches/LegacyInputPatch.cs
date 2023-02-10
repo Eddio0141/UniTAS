2bab85badb07e41671f65c0bc95ac1ad3bdca592
Eddio0141 <eddio0141@gmail.com>
2023-02-10 20:08:09 +0000

    moved input patching

--- UniTASPlugin/Patches/RawPatches/LegacyInputPatch.cs
+++ UniTASPlugin/Patches/RawPatches/LegacyInputPatch.cs
@@ -469,4 +469,351 @@ public class LegacyInputPatch
             ReverseInvoker.Return();
         }
     }
+
+    // TODO not sure what this is
+    /*
+    [HarmonyPrefix]
+    [HarmonyPatch("CheckDisabled")]
+    static void Prefix_CheckDisabled(ref int __result)
+    {
+    }
+    */
+
+    [HarmonyPatch(typeof(Input), "penEventCount", MethodType.Getter)]
+    private class penEventCountGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref int __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO not 100% sure what this is
+            __result = 0;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), "mousePresent", MethodType.Getter)]
+    private class mousePresentGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            __result = VirtualEnvironment.InputState.MouseState.MousePresent;
+            return false;
+        }
+    }
+
+    // TODO
+    [HarmonyPatch(typeof(Input), "GetPenEvent_Injected", typeof(int))]
+    private class GetPenEvent_Injected
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            return !VirtualEnvironment.RunVirtualEnvironment;
+        }
+    }
+
+    // above calls GetPenEvent
+
+    // TODO
+    [HarmonyPatch(typeof(Input), nameof(Input.mainGyroIndex_Internal))]
+    private class mainGyroIndex_Internal
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            return !VirtualEnvironment.RunVirtualEnvironment;
+        }
+    }
+
+    // TODO what is this function call
+    [HarmonyPatch(typeof(Input), nameof(Input.GetPosition))]
+    private class GetPosition
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            return !VirtualEnvironment.RunVirtualEnvironment;
+        }
+    }
+
+    // TODO
+    // this gets called in accelerationEvents getter, check when implementing
+    [HarmonyPatch(typeof(Input), nameof(Input.GetAccelerationEvent))]
+    private class GetAccelerationEvent
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            return !VirtualEnvironment.RunVirtualEnvironment;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.anyKey), MethodType.Getter)]
+    private class anyKeyGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            var inputState = VirtualEnvironment.InputState;
+            __result = inputState.KeyboardState.Keys.Count > 0 || inputState.MouseState.LeftClick ||
+                       inputState.MouseState.RightClick || inputState.MouseState.MiddleClick;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.anyKeyDown), MethodType.Getter)]
+    private class anyKeyDownGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            var inputState = VirtualEnvironment.InputState;
+            __result = inputState.KeyboardState.KeysDown.Count > 0 || inputState.MouseState.LeftClickDown ||
+                       inputState.MouseState.RightClickDown || inputState.MouseState.MiddleClickDown;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.inputString), MethodType.Getter)]
+    private class inputStringGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            return !VirtualEnvironment.RunVirtualEnvironment;
+            // Returns the keyboard input entered this frame
+            // Only ASCII characters are contained in the inputString.
+            // Character "\n" represents return or enter.
+            // TODO
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.multiTouchEnabled), MethodType.Getter)]
+    private class multiTouchEnabledGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO
+            __result = false;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.multiTouchEnabled), MethodType.Setter)]
+    private class multiTouchEnabledSetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            return !VirtualEnvironment.RunVirtualEnvironment;
+            // TODO handle this
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.isGyroAvailable), MethodType.Getter)]
+    private class isGyroAvailableGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref bool __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO
+            __result = false;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.deviceOrientation), MethodType.Getter)]
+    private class deviceOrientationGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref DeviceOrientation __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO
+            __result = DeviceOrientation.Unknown;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.acceleration), MethodType.Getter)]
+    private class accelerationGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref Vector3 __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO
+            __result = Vector3.zero;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.accelerationEventCount), MethodType.Getter)]
+    private class accelerationEventCountGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref int __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO
+            // this gets called in accelerationEvents getter, check there if implementing
+            __result = 0;
+            return false;
+        }
+    }
+
+    /*
+    public static AccelerationEvent[] accelerationEvents
+    {
+        get
+        {
+            if (reverseInvokerFactory.GetReverseInvoker() .Invoking)
+                return true;
+            int accelerationEventCount = Input.accelerationEventCount;
+            AccelerationEvent[] array = new AccelerationEvent[accelerationEventCount];
+            for (int i = 0; i < accelerationEventCount; i++)
+            {
+                array[i] = Input.GetAccelerationEvent(i);
+            }
+            return array;
+        }
+    }
+    */
+
+    [HarmonyPatch(typeof(Input), nameof(Input.touchCount), MethodType.Getter)]
+    private class touchCountGetter
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref int __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO
+            // this gets called in touches getter, check if implementing
+            __result = 0;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Input), nameof(Input.GetTouch))]
+    private class GetTouch
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix()
+        {
+            return !VirtualEnvironment.RunVirtualEnvironment;
+            // TODO
+            // this gets called in touches getter, check if implementing
+        }
+    }
+
+    /*
+    public static Touch[] touches
+    {
+        get
+        {
+            if (reverseInvokerFactory.GetReverseInvoker() .Invoking)
+                return true;
+            int touchCount = Input.touchCount;
+            Touch[] array = new Touch[touchCount];
+            for (int i = 0; i < touchCount; i++)
+            {
+                array[i] = Input.GetTouch(i);
+            }
+            return array;
+        }
+    }
+    */
+
+    [HarmonyPatch(typeof(Input), nameof(Input.GetRotation))]
+    private class GetRotation
+    {
+        private static Exception Cleanup(MethodBase original, Exception ex)
+        {
+            return PatchHelper.CleanupIgnoreFail(original, ex);
+        }
+
+        private static bool Prefix(ref Vector3 __result)
+        {
+            if (!VirtualEnvironment.RunVirtualEnvironment) return true;
+            // TODO what is this call
+            __result = Vector3.zero;
+            return false;
+        }
+    }
 }
