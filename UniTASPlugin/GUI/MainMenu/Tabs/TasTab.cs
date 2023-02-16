2ea6133abb0c42d4ae362a420bac6f202eef23f8
Eddio0141 <eddio0141@gmail.com>
2023-02-16 10:50:25 +0000

    added some GUI elements

--- UniTASPlugin/GUI/MainMenu/Tabs/TasTab.cs
+++ UniTASPlugin/GUI/MainMenu/Tabs/TasTab.cs
@@ -4,10 +4,43 @@ namespace UniTASPlugin.GUI.MainMenu.Tabs;
 
 public class TasTab : IMainMenuTab
 {
-    public void Render(int windowID)
-    {
-        GUILayout.Label("Hello World!");
-    }
+    private string _tasPath = string.Empty;
 
     public string Name => "TAS";
+
+    public void Render(int windowID)
+    {
+        TASPath();
+        OperationButtons();
+        TASRunInfo();
+    }
+
+    private void TASPath()
+    {
+        _tasPath = GUILayout.TextField(_tasPath);
+    }
+
+    private void OperationButtons()
+    {
+        GUILayout.BeginHorizontal();
+        
+        if (GUILayout.Button("Browse"))
+        {
+        }
+        
+        if (GUILayout.Button("Recent"))
+        {
+        }
+        
+        if (GUILayout.Button("Run"))
+        {
+        }
+        
+        GUILayout.EndHorizontal();
+    }
+
+    private void TASRunInfo()
+    {
+        // TODO: Show TAS run info
+    }
 }
