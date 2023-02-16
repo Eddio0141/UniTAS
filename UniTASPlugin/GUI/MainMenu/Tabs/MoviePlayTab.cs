b17c045e126d62b2c15da71a14d718cf2140ef37
Eddio0141 <eddio0141@gmail.com>
2023-02-16 11:41:56 +0000

    finished layout of tab

--- UniTASPlugin/GUI/MainMenu/Tabs/MoviePlayTab.cs
+++ UniTASPlugin/GUI/MainMenu/Tabs/MoviePlayTab.cs
@@ -8,16 +8,25 @@ public class MoviePlayTab : IMainMenuTab
 
     public string Name => "Movie Play";
 
+    private string _tasRunInfo = string.Empty;
+
     public void Render(int windowID)
     {
+        GUILayout.BeginVertical();
         TASPath();
         OperationButtons();
         TASRunInfo();
+        GUILayout.EndVertical();
     }
 
     private void TASPath()
     {
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Movie Path", GUILayout.ExpandWidth(false));
         _tasPath = GUILayout.TextField(_tasPath);
+
+        GUILayout.EndHorizontal();
     }
 
     private void OperationButtons()
@@ -41,6 +50,6 @@ public class MoviePlayTab : IMainMenuTab
 
     private void TASRunInfo()
     {
-        // TODO: Show TAS run info
+        GUILayout.TextArea(_tasRunInfo, GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
     }
 }
