24e827c09cb34ed1649ebadf5e42325bb84216f4
Eddio0141 <eddio0141@gmail.com>
2023-02-16 11:24:43 +0000

    automatically resize tab width

--- UniTASPlugin/GUI/MainMenu/MainMenu.cs
+++ UniTASPlugin/GUI/MainMenu/MainMenu.cs
@@ -11,6 +11,7 @@ public class MainMenu : IOnGUI
 {
     private readonly IMainMenuTab[] _tabs;
     private readonly string[] _tabNames;
+    private int _maxTabNameLength;
     private int _currentTab;
     private Vector2 _scrollPosition;
 
@@ -24,6 +25,12 @@ public class MainMenu : IOnGUI
 
     public void OnGUI()
     {
+        if (_maxTabNameLength == 0)
+        {
+            // tab length in pixels
+            _maxTabNameLength = _tabNames.Max(name => (int)UnityEngine.GUI.skin.label.CalcSize(new(name)).x) + 20;
+        }
+
         _windowRect = GUILayout.Window(0, _windowRect, Window, $"{MyPluginInfo.PLUGIN_NAME} Menu");
     }
 
@@ -38,7 +45,7 @@ public class MainMenu : IOnGUI
     private void RenderTab(int id)
     {
         GUILayout.BeginHorizontal();
-        _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, GUILayout.Width(100));
+        _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, GUILayout.Width(_maxTabNameLength));
         _currentTab = GUILayout.SelectionGrid(_currentTab, _tabNames, 1);
         GUILayout.EndScrollView();
 
