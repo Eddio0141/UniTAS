33c97420dffc10f34999ddb8ad2c4f48ff6c78c3
Eddio0141 <eddio0141@gmail.com>
2023-02-21 23:59:54 +0000

    writing patch

--- UniTAS/Patcher/Tracker.cs
+++ UniTAS/Patcher/Tracker.cs
@@ -1,10 +1,9 @@
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace UniTAS.Patcher;
 
 public static class Tracker
 {
     // ReSharper disable once UnusedMember.Global
-    public static List<GameObject> DontDestroyOnLoadObjects { get; } = new();
+    public static List<object> DontDestroyOnLoadObjects { get; set; } = new();
 }
