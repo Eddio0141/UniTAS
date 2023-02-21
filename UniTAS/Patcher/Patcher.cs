eb00e6314c0daaec23758b6bfc8c757d4ab2d075
Eddio0141 <eddio0141@gmail.com>
2023-02-21 18:32:17 +0000

    set up patcher project base

---
+++ UniTAS/Patcher/Patcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Mono.Cecil;
+
+namespace UniTAS.Patcher;
+
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+public static class Patcher
+{
+    // List of assemblies to patch
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static IEnumerable<string> TargetDLLs { get; } = new[] { "" };
+
+    // Patches the assemblies
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static void Patch(AssemblyDefinition assembly)
+    {
+    }
+}
