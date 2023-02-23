239aae37dc920357633f82289c4e3b7a3a7e9d0d
Eddio0141 <eddio0141@gmail.com>
2023-02-23 11:09:59 +0000

    got harmony patch to work early

--- UniTAS/Patcher/Patcher.cs
+++ UniTAS/Patcher/Patcher.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace UniTAS.Patcher;
 
@@ -9,19 +11,47 @@ public static class Patcher
 {
     // List of assemblies to patch
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
-    public static IEnumerable<string> TargetDLLs { get; } = new string[0];
+    public static IEnumerable<string> TargetDLLs { get; } = new[] { "UnityEngine.CoreModule.dll", "UnityEngine.dll" };
+
+    private static bool _patchedHarmonyEarlyPatcher;
 
     // Patches the assemblies
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public static void Patch(ref AssemblyDefinition assembly)
     {
-    }
+        if (_patchedHarmonyEarlyPatcher) return;
 
-    // Invoked after all assemblies are patched
-    [SuppressMessage("ReSharper", "UnusedMember.Global")]
-    public static void Finish()
-    {
-        var harmony = new HarmonyLib.Harmony("UniTAS.Patcher");
-        harmony.PatchAll();
+        // find MonoBehaviour
+        var monoBehaviour = assembly.MainModule.GetType("UnityEngine.MonoBehaviour");
+
+        if (monoBehaviour == null) return;
+        _patchedHarmonyEarlyPatcher = true;
+
+        // find static ctor
+        var staticCtor = monoBehaviour.Methods.FirstOrDefault(m => m.IsConstructor && m.IsStatic);
+
+        // add static ctor if not found
+        if (staticCtor == null)
+        {
+            staticCtor = new(".cctor",
+                MethodAttributes.Static | MethodAttributes.Private | MethodAttributes.HideBySig
+                | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
+                assembly.MainModule.ImportReference(typeof(void)));
+
+            monoBehaviour.Methods.Add(staticCtor);
+            var il = staticCtor.Body.GetILProcessor();
+            il.Append(il.Create(OpCodes.Ret));
+        }
+
+        // harmony early patcher must be invoked before any MonoBehaviour events are invoked
+        var earlyPatcher = typeof(HarmonyEarlyPatcher);
+        var invoke =
+            assembly.MainModule.ImportReference(earlyPatcher.GetMethod(nameof(HarmonyEarlyPatcher.PatchHarmony)));
+
+        var firstInstruction = staticCtor.Body.Instructions.First();
+        var ilProcessor = staticCtor.Body.GetILProcessor();
+
+        // insert call to harmony early patcher
+        ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create(OpCodes.Call, invoke));
     }
 }
--- UniTAS/Patcher/Patches/Harmony/Unity/ObjectPatch.cs
+++ UniTAS/Patcher/Patches/Harmony/Unity/ObjectPatch.cs
@@ -10,12 +10,12 @@ namespace UniTAS.Patcher.Patches.Harmony.Unity;
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public class ObjectPatch
 {
-    // [HarmonyPatch(typeof(Object), nameof(Object.DontDestroyOnLoad))]
-    // private class DontDestroyOnLoadPatch
-    // {
-    //     private static void Postfix(Object target, bool __runOriginal)
-    //     {
-    //         Trace.Write($"DontDestroyOnLoad called!, target name: {target.name}, target type: {target.GetType()}, run original: {__runOriginal}");
-    //     }
-    // }
+    [HarmonyPatch(typeof(Object), nameof(Object.DontDestroyOnLoad))]
+    private class DontDestroyOnLoadPatch
+    {
+        private static void Postfix(Object target, bool __runOriginal)
+        {
+            Trace.Write($"DontDestroyOnLoad called!, target name: {target.name}, target type: {target.GetType()}, run original: {__runOriginal}");
+        }
+    }
 }
