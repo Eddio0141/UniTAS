574640d634b462479b6a1ec5ba857883a553e3eb
Eddio0141 <eddio0141@gmail.com>
2023-02-23 15:25:56 +0000

    test to make sure string ext is working

---
+++ UniTAS/Plugin.Tests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,15 @@
+﻿using UniTAS.Plugin.Extensions;
+
+namespace UniTAS.Plugin.Tests.Extensions;
+
+public class StringExtensionsTest
+{
+    [Fact]
+    public void EqualsExact()
+    {
+        const string str = "test";
+        const string pattern = "test";
+        
+        Assert.True(str.Like(pattern));
+    }
+}
