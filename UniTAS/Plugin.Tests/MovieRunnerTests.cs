fe429d9b19dc48cf20a1bf5e1548f5519b96d84b
Eddio0141 <eddio0141@gmail.com>
2023-02-27 04:10:55 +0000

    added tests for warns

--- UniTAS/Plugin.Tests/MovieRunnerTests.cs
+++ UniTAS/Plugin.Tests/MovieRunnerTests.cs
@@ -1,5 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
+using BepInEx.Logging;
 using MoonSharp.Interpreter;
+using StructureMap;
+using UniTAS.Plugin.Logger;
 using UniTAS.Plugin.Movie.Engine;
 using UniTAS.Plugin.Movie.MovieModels.Properties;
 using UniTAS.Plugin.Movie.Parsers.Exceptions;
@@ -10,14 +13,40 @@ namespace UniTAS.Plugin.Tests;
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Local")]
 public class MovieRunnerTests
 {
-    private static (IMovieEngine, PropertiesModel) Setup(string input)
+    private class DummyLogger : IMovieLogger
+    {
+        public List<string> Warns { get; } = new();
+
+        public void LogError(object data)
+        {
+        }
+
+        public void LogInfo(object data)
+        {
+        }
+
+        public void LogWarning(object data)
+        {
+            Warns.Add(data.ToString() ?? string.Empty);
+        }
+
+        public event EventHandler<LogEventArgs>? OnLog;
+    }
+
+    private static (IMovieEngine, PropertiesModel, IContainer) Setup(string input)
     {
         var kernel = ContainerRegister.Init();
+
+        kernel.Configure(x =>
+        {
+            x.ForSingletonOf<DummyLogger>().Use<DummyLogger>();
+            x.For<IMovieLogger>().Use(y => y.GetInstance<DummyLogger>());
+        });
 
         var parser = kernel.GetInstance<IMovieParser>();
         var parsed = parser.Parse(input);
 
-        return (parsed.Item1, parsed.Item2);
+        return (parsed.Item1, parsed.Item2, kernel);
     }
 
     [Fact]
@@ -131,5 +160,32 @@ frametime = 1/60
 
         Assert.Equal(new(2021, 03, 28, 12, 00, 00), properties.StartupProperties.StartTime);
         Assert.Equal(1 / 60f, properties.StartupProperties.FrameTime);
+    }
+
+    [Fact]
+    public void FpsFrametimeConflict()
+    {
+        const string input = @"
+START_TIME = ""28/03/2021 12:00:00""
+fps = 60
+frametime = 1/50
+";
+
+        var (_, properties, kernel) = Setup(input);
+
+        Assert.Equal(1 / 50f, properties.StartupProperties.FrameTime);
+        Assert.Equal("frametime and fps are both defined, using frametime", kernel.GetInstance<DummyLogger>().Warns[0]);
+    }
+
+    [Fact]
+    public void MissingStartTime()
+    {
+        const string input = @"
+fps = 60
+";
+
+        var (_, _, kernel) = Setup(input);
+
+        Assert.Equal("START_TIME is not defined, using default value of 01/01/0001 00:00:00", kernel.GetInstance<DummyLogger>().Warns[0]);
     }
 }
