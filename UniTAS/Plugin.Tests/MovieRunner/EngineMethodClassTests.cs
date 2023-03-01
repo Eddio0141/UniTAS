0b965a522e61406bf1ef72e3a7ed07a65bba98ef
Eddio0141 <eddio0141@gmail.com>
2023-03-01 17:48:41 +0000

    added test if method name is as expected

--- UniTAS/Plugin.Tests/MovieRunner/EngineMethodClassTests.cs
+++ UniTAS/Plugin.Tests/MovieRunner/EngineMethodClassTests.cs
@@ -13,4 +13,15 @@ class_name = concurrent.ClassName
 
         Assert.Throws<ScriptRuntimeException>(() => Utils.Setup(input));
     }
+
+    [Fact]
+    public void UnderscoreMembers()
+    {
+        var script = Utils.Setup("move_rel = mouse.move_rel").Item1;
+        script.Update();
+
+        var moveRel = script.Script.Globals.Get("move_rel");
+        Assert.NotNull(moveRel);
+        Assert.NotEqual(DataType.Nil, moveRel.Type);
+    }
 }
