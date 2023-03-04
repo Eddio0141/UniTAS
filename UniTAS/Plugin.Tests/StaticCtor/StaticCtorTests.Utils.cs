6818a4aa0cf35840af6c92a686567861b9d2997c
Eddio0141 <eddio0141@gmail.com>
2023-03-04 00:29:29 +0000

    tests to see invoke order of static ctors

---
+++ UniTAS/Plugin.Tests/StaticCtor/StaticCtorTests.Utils.cs
@@ -0,0 +1,81 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace UniTAS.Plugin.Tests.StaticCtor;
+
+[SuppressMessage("ReSharper", "UnusedMember.Local")]
+public partial class StaticCtorTests
+{
+    private class StaticCtorTest
+    {
+        public static int StaticField = Assign(nameof(StaticField), 5);
+        public static int StaticField2;
+
+        static StaticCtorTest()
+        {
+            Console.WriteLine("StaticCtorTest");
+            StaticField2 = Assign(nameof(StaticField2), StaticField);
+        }
+
+        private static int Assign(string msg, int value)
+        {
+            Console.WriteLine(msg);
+            return value;
+        }
+    }
+
+    private class StaticCtorTestBase
+    {
+        public static int StaticFieldBase = Assign(nameof(StaticFieldBase), 5);
+        public static int StaticFieldBase2 = Assign(nameof(StaticFieldBase2), 10);
+
+        static StaticCtorTestBase()
+        {
+            Console.WriteLine("StaticCtorTest Base");
+        }
+
+        private static int Assign(string msg, int value)
+        {
+            Console.WriteLine(msg);
+            return value;
+        }
+    }
+
+    private class StaticCtorTestDerived : StaticCtorTestBase
+    {
+        public static int StaticFieldDerived = Assign(nameof(StaticFieldDerived), 15);
+        public static int StaticFieldDerived2 = Assign(nameof(StaticFieldDerived2), 20);
+
+        static StaticCtorTestDerived()
+        {
+            Console.WriteLine("StaticCtorTest Derived");
+        }
+
+        private static int Assign(string msg, int value)
+        {
+            Console.WriteLine(msg);
+            return value;
+        }
+    }
+
+    private class StaticCtorTestDerived2 : StaticCtorTestBase
+    {
+        public static int StaticField2Derived = Assign(nameof(StaticField2Derived), 15);
+        public static int StaticField2Derived2 = Assign(nameof(StaticField2Derived2), 20);
+
+        static StaticCtorTestDerived2()
+        {
+            // access StaticCtorTestBase
+#pragma warning disable CS1717
+            StaticFieldBase = StaticFieldBase;
+#pragma warning restore CS1717
+            Console.WriteLine("Accessing StaticCtorTestBase");
+            Console.WriteLine("StaticCtorTest Derived 2");
+        }
+
+        private static int Assign(string msg, int value)
+        {
+            Console.WriteLine(msg);
+            return value;
+        }
+    }
+}
---
+++ UniTAS/Plugin.Tests/StaticCtor/StaticCtorTests.cs
@@ -0,0 +1,73 @@
+namespace UniTAS.Plugin.Tests.StaticCtor;
+
+public partial class StaticCtorTests
+{
+    private static StringWriter Setup()
+    {
+        var stdout = new StringWriter();
+        Console.SetOut(stdout);
+
+        return stdout;
+    }
+
+    [Fact]
+    public void StaticCtorInvokeOrder()
+    {
+        var stdout = Setup();
+
+        var _ = new StaticCtorTest();
+
+        var final = stdout.ToString();
+        var expected = new[]
+        {
+            "StaticField",
+            "StaticCtorTest",
+            "StaticField2",
+        };
+
+        Assert.Equal(expected, final.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries));
+    }
+
+    [Fact]
+    public void StaticCtorInvokeOrderDerived()
+    {
+        var stdout = Setup();
+
+        var _ = new StaticCtorTestDerived();
+
+        var final = stdout.ToString();
+        var expected = new[]
+        {
+            "StaticFieldDerived",
+            "StaticFieldDerived2",
+            "StaticCtorTest Derived",
+            "StaticFieldBase",
+            "StaticFieldBase2",
+            "StaticCtorTest Base",
+        };
+
+        Assert.Equal(expected, final.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries));
+    }
+
+    [Fact]
+    public void StaticCtorInvokeOrderDerived2()
+    {
+        var stdout = Setup();
+
+        var _ = new StaticCtorTestDerived2();
+
+        var final = stdout.ToString();
+        var expected = new[]
+        {
+            "StaticField2Derived",
+            "StaticField2Derived2",
+            "StaticFieldBase",
+            "StaticFieldBase2",
+            "StaticCtorTest Base",
+            "Accessing StaticCtorTestBase 5",
+            "StaticCtorTest Derived 2"
+        };
+
+        Assert.Equal(expected, final.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries));
+    }
+}
