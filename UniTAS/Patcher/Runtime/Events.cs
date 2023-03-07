d5dd3374c2c01d489036d52fa49901db5d82612c
Eddio0141 <eddio0141@gmail.com>
2023-03-07 20:28:53 +0000

    OnAudioFilterRead to AudioListener

---
+++ UniTAS/Patcher/Runtime/Events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UniTAS.Patcher.Runtime;
+
+public class Events
+{
+    /// <summary>
+    /// Event that is invoked when an audio filter is read
+    /// </summary>
+    public static event Action<float[], int> OnAudioFilterRead;
+
+    internal static void OnAudioFilterReadInvoke(float[] data, int channels)
+    {
+        OnAudioFilterRead?.Invoke(data, channels);
+    }
+}
