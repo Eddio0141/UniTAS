85959b15d39d62daf398a22d0a1661b8884a5773
Eddio0141 <eddio0141@gmail.com>
2023-03-18 08:08:46 +0000

    added image resizing code

---
+++ UniTAS/Plugin.Tests/Utils/ImageOperationTests.cs
@@ -0,0 +1,54 @@
+using UniTAS.Plugin.Utils;
+
+namespace UniTAS.Plugin.Tests.Utils;
+
+public class ImageOperationTests
+{
+    [Fact]
+    public void ResizeNoChange()
+    {
+        var original = new byte[3];
+        var result = ImageOperation.Resize(original, 1, 1, 1, 1);
+        Assert.Equal(original, result);
+    }
+
+    [Fact]
+    public void Resize_1x1To2x2()
+    {
+        var original = Enumerable.Repeat((byte)1, 6).ToArray();
+        var resized = Enumerable.Repeat((byte)1, 12).ToArray();
+
+        var result = ImageOperation.Resize(original, 1, 1, 2, 2);
+        Assert.Equal(resized, result);
+    }
+
+    [Fact]
+    public void Resize_1x1To3x1()
+    {
+        // 1x1 -> 3x1
+        var original = Enumerable.Repeat((byte)1, 6).ToArray();
+        var resized = new byte[] { 0, 0, 0, 1, 1, 1, 0, 0, 0 };
+
+        var result = ImageOperation.Resize(original, 1, 1, 3, 1);
+        Assert.Equal(resized, result);
+    }
+
+    [Fact]
+    public void Resize_2x2To3x3()
+    {
+        var original = new byte[]
+        {
+            1, 2, 3, 4, 5, 6,
+            7, 8, 9, 10, 11, 12
+        };
+        var resized = new byte[]
+        {
+            1, 2, 3, 1, 2, 3, 4, 5, 6,
+            1, 2, 3, 1, 2, 3, 4, 5, 6,
+            7, 8, 9, 7, 8, 9, 10, 11, 12
+        };
+
+        var result = ImageOperation.Resize(original, 2, 2, 3, 3);
+        Assert.Equal(resized, result);
+    }
+}
