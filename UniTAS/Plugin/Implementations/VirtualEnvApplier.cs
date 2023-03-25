4d9f224d1c30f0c98ee501ccc1e89bc31058986a
Eddio0141 <eddio0141@gmail.com>
2023-03-25 18:42:52 +0000

    removed check on if virtual env is enabled

--- UniTAS/Plugin/Implementations/VirtualEnvApplier.cs
+++ UniTAS/Plugin/Implementations/VirtualEnvApplier.cs
@@ -11,26 +11,18 @@ namespace UniTAS.Plugin.Implementations;
 public class VirtualEnvApplier : IOnPreUpdates
 {
     private readonly ITimeEnv _timeEnv;
-    private readonly IVirtualEnvController _virtualEnvController;
     private readonly ITimeWrapper _timeWrap;
 
     private float _lastFrameTime;
 
-    public VirtualEnvApplier(ITimeEnv timeEnv, ITimeWrapper timeWrap, IVirtualEnvController virtualEnvController)
+    public VirtualEnvApplier(ITimeEnv timeEnv, ITimeWrapper timeWrap)
     {
         _timeEnv = timeEnv;
         _timeWrap = timeWrap;
-        _virtualEnvController = virtualEnvController;
     }
 
     public void PreUpdate()
     {
-        if (!_virtualEnvController.RunVirtualEnvironment)
-        {
-            _lastFrameTime = -1f;
-            return;
-        }
-
         // ReSharper disable once CompareOfFloatsByEqualityOperator
         if (_lastFrameTime != _timeEnv.FrameTime)
         {
