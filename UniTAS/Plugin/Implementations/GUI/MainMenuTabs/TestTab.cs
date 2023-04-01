58a50bab6e7b85139fcd6e898fe720e2c8a6a2a6
Eddio0141 <eddio0141@gmail.com>
2023-04-01 22:20:59 +0100

    added more test buttons

--- UniTAS/Plugin/Implementations/GUI/MainMenuTabs/TestTab.cs
+++ UniTAS/Plugin/Implementations/GUI/MainMenuTabs/TestTab.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using UniTAS.Plugin.Interfaces.GUI;
 using UniTAS.Plugin.Interfaces.TASRenderer;
+using UniTAS.Plugin.Services;
+using UniTAS.Plugin.Services.UnitySafeWrappers.Wrappers;
 using UnityEngine;
 
 namespace UniTAS.Plugin.Implementations.GUI.MainMenuTabs;
@@ -9,14 +12,23 @@ namespace UniTAS.Plugin.Implementations.GUI.MainMenuTabs;
 public class TestTab : IMainMenuTab
 {
     private readonly IGameRender _gameRender;
+    private readonly IGameRestart _gameRestart;
+    private readonly ISceneWrapper _sceneWrapper;
+    private readonly IMonoBehaviourController _monoBehaviourController;
 
-    public TestTab(IGameRender gameRender)
+    public TestTab(IGameRender gameRender, IGameRestart gameRestart, ISceneWrapper sceneWrapper,
+        IMonoBehaviourController monoBehaviourController)
     {
         _gameRender = gameRender;
+        _gameRestart = gameRestart;
+        _sceneWrapper = sceneWrapper;
+        _monoBehaviourController = monoBehaviourController;
     }
 
     public void Render(int windowID)
     {
+        GUILayout.BeginVertical();
+        GUILayout.BeginHorizontal();
         if (GUILayout.Button("Start record"))
         {
             _gameRender.Start();
@@ -26,6 +38,27 @@ public class TestTab : IMainMenuTab
         {
             _gameRender.Stop();
         }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Restart game"))
+        {
+            _gameRestart.SoftRestart(DateTime.Now);
+        }
+
+        if (GUILayout.Button("Scene 0"))
+        {
+            _sceneWrapper.LoadScene(0);
+        }
+
+        if (GUILayout.Button("Toggle MonoBehaviour pause"))
+        {
+            _monoBehaviourController.PausedExecution = !_monoBehaviourController.PausedExecution;
+        }
+
+        GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
     }
 
     public string Name => "Test";
