d43888df600891cc1c8379416b8fb3afeecab6a0
Eddio0141 <eddio0141@gmail.com>
2023-04-12 19:49:29 +0100

    added description of usage

--- UniTAS/Plugin/Interfaces/RuntimeTest/RuntimeTestAttribute.cs
+++ UniTAS/Plugin/Interfaces/RuntimeTest/RuntimeTestAttribute.cs
@@ -1,7 +1,16 @@
 using System;
+using System.Collections.Generic;
+using UniTAS.Plugin.Interfaces.Coroutine;
+using UniTAS.Plugin.Utils;
 
 namespace UniTAS.Plugin.Interfaces.RuntimeTest;
 
+/// <summary>
+/// Attribute for marking methods as runtime tests.
+/// If a method returns a bool and returns false, the test will be skipped.
+/// You can return an <see cref="IEnumerator{T}"/> with <see cref="CoroutineWait"/> to turn the test into a coroutine.
+/// In order to use multiple return types as defined above, use <see cref="Tuple{T1,T2}"/>.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, Inherited = false)]
 public class RuntimeTestAttribute : Attribute
 {
