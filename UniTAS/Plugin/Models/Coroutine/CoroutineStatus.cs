5027f6c1612aca7d8157b4359548c5e56c400f6f
Eddio0141 <eddio0141@gmail.com>
2023-04-14 05:19:10 +0100

    store exception when throw

--- UniTAS/Plugin/Models/Coroutine/CoroutineStatus.cs
+++ UniTAS/Plugin/Models/Coroutine/CoroutineStatus.cs
@@ -1,6 +1,9 @@
+using System;
+
 namespace UniTAS.Plugin.Models.Coroutine;
 
 public class CoroutineStatus
 {
     public bool IsRunning { get; set; } = true;
+    public Exception Exception { get; set; }
 }
