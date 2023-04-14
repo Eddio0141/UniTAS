1f48f11967fc6b878b8f6464ad1c23b4980dfd2d
Eddio0141 <eddio0141@gmail.com>
2023-04-14 18:38:00 +0100

    fixed tests

--- UniTAS/Plugin.Tests/RuntimeTest.cs
+++ UniTAS/Plugin.Tests/RuntimeTest.cs
@@ -1,6 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
+using UniTAS.Plugin.Implementations.Coroutine;
 using UniTAS.Plugin.Interfaces.Coroutine;
 using UniTAS.Plugin.Interfaces.RuntimeTest;
+using UniTAS.Plugin.Models.Coroutine;
+using UniTAS.Plugin.Services;
 using UniTAS.Plugin.Services.RuntimeTest;
 
 namespace UniTAS.Plugin.Tests;
@@ -37,21 +40,49 @@ public class RuntimeTest
         return new();
     }
 
+    [RuntimeTest]
+    public UniTAS.Plugin.Utils.Tuple<bool, IEnumerator<CoroutineWait>> CoroutineTest()
+    {
+        return new(true, CoroutineTestInner());
+    }
+
+    private static IEnumerator<CoroutineWait> CoroutineTestInner()
+    {
+        yield return new WaitForUpdateUnconditional();
+        yield return new WaitForUpdateUnconditional();
+    }
+
+    [RuntimeTest]
+    public IEnumerator<CoroutineWait> CoroutineTest2()
+    {
+        yield return new WaitForUpdateUnconditional();
+        yield return new WaitForUpdateUnconditional();
+    }
+
     [Fact]
+    [SuppressMessage("ReSharper", "ParameterOnlyUsedForPreconditionCheck.Local")]
     public void DiscoverTests()
     {
         var kernel = KernelUtils.Init();
         var processor = kernel.GetInstance<IRuntimeTestProcessor>();
+        var coroutineRunner = (CoroutineHandler)kernel.GetInstance<ICoroutine>();
+
+        processor.OnDiscoveredTests += count => Assert.Equal(7, count);
+
         processor.OnTestEnd += results =>
         {
-            processor.OnDiscoveredTests += count => Assert.Equal(2, count);
-
-            Assert.Equal(5, results.Count);
-            Assert.Equal(2, results.Count(x => x.Passed));
+            Assert.Equal(7, results.Count);
+            Assert.Equal(4, results.Count(x => x.Passed));
             Assert.Equal(1, results.Count(x => !x.Passed && !x.Skipped));
             Assert.Equal(2, results.Count(x => x.Skipped));
         };
         processor.Test<RuntimeTest>();
+
+        for (var i = 0; i < 2; i++)
+        {
+            coroutineRunner.PreUpdateUnconditional();
+            coroutineRunner.UpdateUnconditional();
+        }
     }
 
     [Fact]
@@ -59,6 +90,7 @@ public class RuntimeTest
     {
         var kernel = KernelUtils.Init();
         var processor = kernel.GetInstance<IRuntimeTestProcessor>();
+        var coroutineRunner = (CoroutineHandler)kernel.GetInstance<ICoroutine>();
 
         processor.OnTestEnd += results =>
         {
@@ -67,6 +99,12 @@ public class RuntimeTest
         };
 
         processor.Test<RuntimeTest>();
+
+        for (var i = 0; i < 2; i++)
+        {
+            coroutineRunner.PreUpdateUnconditional();
+            coroutineRunner.UpdateUnconditional();
+        }
     }
 
     [Fact]
@@ -74,6 +112,7 @@ public class RuntimeTest
     {
         var kernel = KernelUtils.Init();
         var processor = kernel.GetInstance<IRuntimeTestProcessor>();
+        var coroutineRunner = (CoroutineHandler)kernel.GetInstance<ICoroutine>();
 
         processor.OnTestEnd += results =>
         {
@@ -82,22 +121,36 @@ public class RuntimeTest
         };
 
         processor.Test<RuntimeTest>();
+
+        for (var i = 0; i < 2; i++)
+        {
+            coroutineRunner.PreUpdateUnconditional();
+            coroutineRunner.UpdateUnconditional();
+        }
     }
 
     [Fact]
+    [SuppressMessage("ReSharper", "ParameterOnlyUsedForPreconditionCheck.Local")]
     public void TestRunEvent()
     {
         var kernel = KernelUtils.Init();
         var processor = kernel.GetInstance<IRuntimeTestProcessor>();
+        var coroutineRunner = (CoroutineHandler)kernel.GetInstance<ICoroutine>();
 
         var testRunCount = 0;
         processor.OnTestRun += _ => testRunCount++;
         processor.OnTestEnd += results =>
         {
-            Assert.Equal(5, testRunCount);
+            Assert.Equal(7, testRunCount);
             Assert.Equal(testRunCount, results.Count);
         };
 
         processor.Test<RuntimeTest>();
+
+        for (var i = 0; i < 2; i++)
+        {
+            coroutineRunner.PreUpdateUnconditional();
+            coroutineRunner.UpdateUnconditional();
+        }
     }
 }
