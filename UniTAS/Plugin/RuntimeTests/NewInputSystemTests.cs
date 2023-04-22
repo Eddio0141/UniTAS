c109fb04e454642f842c4502f81e11b974b74457
Eddio0141 <eddio0141@gmail.com>
2023-04-22 04:59:31 +0100

    test to check new input system mouse

---
+++ UniTAS/Plugin/RuntimeTests/NewInputSystemTests.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UniTAS.Plugin.Interfaces.Coroutine;
+using UniTAS.Plugin.Interfaces.DependencyInjection;
+using UniTAS.Plugin.Interfaces.RuntimeTest;
+using UniTAS.Plugin.Models.Coroutine;
+using UniTAS.Plugin.Services.VirtualEnvironment;
+using UniTAS.Plugin.Services.VirtualEnvironment.Input;
+using UniTAS.Plugin.Utils;
+using UnityEngine.InputSystem;
+
+namespace UniTAS.Plugin.RuntimeTests;
+
+[Register]
+public class NewInputSystemTests
+{
+    private readonly IMouseStateEnv _mouseStateEnv;
+    private readonly IVirtualEnvController _virtualEnvController;
+
+    public NewInputSystemTests(IMouseStateEnv mouseStateEnv, IVirtualEnvController virtualEnvController)
+    {
+        _mouseStateEnv = mouseStateEnv;
+        _virtualEnvController = virtualEnvController;
+    }
+
+    [RuntimeTest]
+    public IEnumerator<CoroutineWait> MouseButtons()
+    {
+        _virtualEnvController.RunVirtualEnvironment = true;
+
+        yield return new WaitForUpdateUnconditional();
+
+        _mouseStateEnv.LeftClick = true;
+        var mouse = Mouse.current;
+
+        yield return new WaitForUpdateUnconditional();
+
+        RuntimeAssert.True(mouse.leftButton.isPressed, "left button check");
+
+        _mouseStateEnv.LeftClick = false;
+
+        yield return new WaitForUpdateUnconditional();
+
+        RuntimeAssert.False(mouse.leftButton.isPressed, "left button check");
+
+        _mouseStateEnv.RightClick = true;
+
+        yield return new WaitForUpdateUnconditional();
+
+        RuntimeAssert.True(mouse.rightButton.isPressed, "right button check");
+
+        _mouseStateEnv.RightClick = false;
+
+        yield return new WaitForUpdateUnconditional();
+
+        RuntimeAssert.False(mouse.rightButton.isPressed, "right button check");
+
+        _mouseStateEnv.MiddleClick = true;
+
+        yield return new WaitForUpdateUnconditional();
+
+        RuntimeAssert.True(mouse.middleButton.isPressed, "middle button check");
+
+        _mouseStateEnv.MiddleClick = false;
+
+        yield return new WaitForUpdateUnconditional();
+
+        RuntimeAssert.False(mouse.middleButton.isPressed, "middle button check");
+
+        _virtualEnvController.RunVirtualEnvironment = false;
+    }
+}
