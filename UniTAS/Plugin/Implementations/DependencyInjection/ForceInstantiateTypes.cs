e1d10649fa2647985aff7ed396d2be30a1b5d14e
Eddio0141 <eddio0141@gmail.com>
2023-04-22 20:15:34 +0100

    log force instantiation

--- UniTAS/Plugin/Implementations/DependencyInjection/ForceInstantiateTypes.cs
+++ UniTAS/Plugin/Implementations/DependencyInjection/ForceInstantiateTypes.cs
@@ -2,6 +2,7 @@ using HarmonyLib;
 using StructureMap;
 using UniTAS.Plugin.Interfaces.DependencyInjection;
 using UniTAS.Plugin.Services.DependencyInjection;
+using UniTAS.Plugin.Services.Logging;
 
 namespace UniTAS.Plugin.Implementations.DependencyInjection;
 
@@ -9,10 +10,12 @@ namespace UniTAS.Plugin.Implementations.DependencyInjection;
 public class ForceInstantiateTypes : IForceInstantiateTypes
 {
     private readonly IContainer _container;
+    private readonly ILogger _logger;
 
-    public ForceInstantiateTypes(IContainer container)
+    public ForceInstantiateTypes(IContainer container, ILogger logger)
     {
         _container = container;
+        _logger = logger;
     }
 
     public void InstantiateTypes()
@@ -24,6 +27,7 @@ public class ForceInstantiateTypes : IForceInstantiateTypes
             var attributes = type.GetCustomAttributes(typeof(ForceInstantiateAttribute), true);
             if (attributes.Length == 0) continue;
 
+            _logger.LogDebug($"Force instantiating {type}");
             _container.GetInstance(type);
         }
     }
