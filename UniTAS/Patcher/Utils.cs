63028dae66196d41025284d69ec292d96e8abce0
Eddio0141 <eddio0141@gmail.com>
2023-05-05 07:21:43 +0100

    include input system in the generic inclusion list

--- UniTAS/Patcher/Utils.cs
+++ UniTAS/Patcher/Utils.cs
@@ -27,7 +27,13 @@ public static class Utils
         "MonoMod.*",
         "0Harmony",
         "HarmonyXInterop",
-        "StructureMap"
+        "StructureMap",
+        "Newtonsoft.Json"
+    };
+
+    private static readonly string[] AssemblyIncludeRaw =
+    {
+        "Unity.InputSystem"
     };
 
     public static IEnumerable<string> AllTargetDllsWithGenericExclusions =>
@@ -36,6 +42,7 @@ public static class Utils
             {
                 var fileWithoutExtension = Path.GetFileNameWithoutExtension(x);
                 return fileWithoutExtension == null ||
+                       AssemblyIncludeRaw.Any(a => fileWithoutExtension.Like(a)) ||
                        !AssemblyExclusionsRaw.Any(a => fileWithoutExtension.Like(a));
             })
             // isolate the filename
