93b4cb9334059609382cea157837d6cabb69fdd7
Eddio0141 <eddio0141@gmail.com>
2023-05-12 15:41:59 +0100

    changed update method to movie update

--- UniTAS/Plugin/Interfaces/VirtualEnvironment/LegacyInputSystemDevice.cs
+++ UniTAS/Plugin/Interfaces/VirtualEnvironment/LegacyInputSystemDevice.cs
@@ -1,11 +1,12 @@
 ﻿using UniTAS.Plugin.Interfaces.Events.MonoBehaviourEvents.DontRunIfPaused;
+using UniTAS.Plugin.Interfaces.Events.Movie;
 
 namespace UniTAS.Plugin.Interfaces.VirtualEnvironment;
 
 /// <summary>
 /// Provides a way to create a device that uses legacy input system, or is also used in the new input system.
 /// </summary>
-public abstract class LegacyInputSystemDevice : InputState, IOnLastUpdateActual, IOnPreUpdatesActual
+public abstract class LegacyInputSystemDevice : InputState, IOnLastUpdateActual, IOnMovieUpdate
 {
     protected abstract void Update();
 
@@ -20,7 +21,7 @@ public abstract class LegacyInputSystemDevice : InputState, IOnLastUpdateActual,
         Update();
     }
 
-    public void PreUpdateActual()
+    public void MovieUpdate(bool fixedUpdate)
     {
         // this just needs to be flushed before legacy input system input is read so its fine to be pre-updates
         FlushBufferedInputs();
