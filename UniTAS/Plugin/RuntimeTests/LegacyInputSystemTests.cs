4111f986402e51bbbc430e805b6d9f5aaf891a49
Eddio0141 <eddio0141@gmail.com>
2023-05-15 00:08:06 +0100

    fixed test to match new thingy

--- UniTAS/Plugin/RuntimeTests/LegacyInputSystemTests.cs
+++ UniTAS/Plugin/RuntimeTests/LegacyInputSystemTests.cs
@@ -1,11 +1,9 @@
-using System.Collections.Generic;
-using UniTAS.Plugin.Interfaces.Coroutine;
 using UniTAS.Plugin.Interfaces.DependencyInjection;
 using UniTAS.Plugin.Interfaces.RuntimeTest;
-using UniTAS.Plugin.Models.Coroutine;
+using UniTAS.Plugin.Interfaces.VirtualEnvironment;
 using UniTAS.Plugin.Models.VirtualEnvironment;
 using UniTAS.Plugin.Services.VirtualEnvironment;
-using UniTAS.Plugin.Services.VirtualEnvironment.Input;
+using UniTAS.Plugin.Services.VirtualEnvironment.Input.LegacyInputSystem;
 using UniTAS.Plugin.Utils;
 using UnityEngine;
 
@@ -14,61 +12,64 @@ namespace UniTAS.Plugin.RuntimeTests;
 [Register]
 public class LegacyInputSystemTests
 {
-    private readonly IKeyboardStateEnvController _keyboardController;
-    private readonly IMouseStateEnvController _mouseController;
+    private readonly IKeyboardStateEnvLegacySystem _keyboardController;
+    private readonly LegacyInputSystemDevice _keyboardControllerBase;
+
+    private readonly IMouseStateEnvLegacySystem _mouseController;
+    private readonly LegacyInputSystemDevice _mouseControllerBase;
+
     private readonly IVirtualEnvController _virtualEnvController;
     private readonly IKeyFactory _keyFactory;
-    private readonly ITimeEnv _timeEnv;
 
-    public LegacyInputSystemTests(IKeyboardStateEnvController keyboardController,
-        IVirtualEnvController virtualEnvController, IMouseStateEnvController mouseController, IKeyFactory keyFactory,
-        ITimeEnv timeEnv)
+    public LegacyInputSystemTests(IKeyboardStateEnvLegacySystem keyboardController,
+        IVirtualEnvController virtualEnvController, IMouseStateEnvLegacySystem mouseController, IKeyFactory keyFactory)
     {
         _keyboardController = keyboardController;
+        _keyboardControllerBase = (LegacyInputSystemDevice)keyboardController;
         _virtualEnvController = virtualEnvController;
         _mouseController = mouseController;
+        _mouseControllerBase = (LegacyInputSystemDevice)mouseController;
         _keyFactory = keyFactory;
-        _timeEnv = timeEnv;
     }
 
     [RuntimeTest]
-    public IEnumerator<CoroutineWait> GetKeyTest()
+    public void GetKeyTest()
     {
         _virtualEnvController.RunVirtualEnvironment = true;
 
-        yield return new WaitForUpdateUnconditional();
-
         _keyboardController.Hold(_keyFactory.CreateKey(KeyCode.A));
-
-        yield return new WaitForUpdateUnconditional();
+        _keyboardControllerBase.MovieUpdate(false);
 
         RuntimeAssert.True(Input.GetKeyDown(KeyCode.A), "keycode down check");
         RuntimeAssert.True(Input.GetKeyDown("a"), "string down check");
         RuntimeAssert.True(Input.GetKey(KeyCode.A), "keycode check");
         RuntimeAssert.True(Input.GetKey("a"), "string check");
 
-        yield return new WaitForUpdateUnconditional();
+        _keyboardControllerBase.OnLastUpdateActual();
+        _keyboardControllerBase.MovieUpdate(false);
 
         RuntimeAssert.False(Input.GetKeyDown(KeyCode.A), "keycode down check");
         RuntimeAssert.False(Input.GetKeyDown("a"), "string down check");
         RuntimeAssert.True(Input.GetKey(KeyCode.A), "keycode 2 check");
         RuntimeAssert.True(Input.GetKey("a"), "string 2 check");
 
-        yield return new WaitForUpdateUnconditional();
+        _keyboardControllerBase.OnLastUpdateActual();
+        _keyboardControllerBase.MovieUpdate(false);
 
         RuntimeAssert.True(Input.GetKey(KeyCode.A), "keycode 3 check");
         RuntimeAssert.True(Input.GetKey("a"), "string 3 check");
 
+        _keyboardControllerBase.OnLastUpdateActual();
         _keyboardController.Release(_keyFactory.CreateKey(KeyCode.A));
-
-        yield return new WaitForUpdateUnconditional();
+        _keyboardControllerBase.MovieUpdate(false);
 
         RuntimeAssert.False(Input.GetKey(KeyCode.A), "keycode 4 check");
         RuntimeAssert.False(Input.GetKey("a"), "string 4 check");
         RuntimeAssert.True(Input.GetKeyUp(KeyCode.A), "keycode up check");
         RuntimeAssert.True(Input.GetKeyUp("a"), "string up check");
 
-        yield return new WaitForUpdateUnconditional();
+        _keyboardControllerBase.OnLastUpdateActual();
+        _keyboardControllerBase.MovieUpdate(false);
 
         RuntimeAssert.False(Input.GetKeyUp(KeyCode.A), "keycode up check");
         RuntimeAssert.False(Input.GetKeyUp("a"), "string up check");
@@ -77,128 +78,94 @@ public class LegacyInputSystemTests
     }
 
     [RuntimeTest]
-    public IEnumerator<CoroutineWait> KeyDownTest()
+    public void KeyDownTest()
     {
         _virtualEnvController.RunVirtualEnvironment = true;
 
-        var captureFt = _timeEnv.FrameTime;
-        var fixedDt = Time.fixedDeltaTime;
-
-        _timeEnv.FrameTime = 0.01;
-        Time.fixedDeltaTime = 0.02f;
-
-        yield return new WaitForFixedUpdateUnconditional();
-        yield return new WaitForUpdateUnconditional();
-        yield return new WaitForUpdateUnconditional();
-
-        Plugin.Log.LogDebug("press A");
         _keyboardController.Hold(_keyFactory.CreateKey(KeyCode.A));
+        _keyboardControllerBase.MovieUpdate(true);
 
-        yield return new WaitForFixedUpdateUnconditional();
-
-        Plugin.Log.LogDebug("check A press");
         RuntimeAssert.True(Input.GetKeyDown(KeyCode.A), "keycode down check 1");
 
-        yield return new WaitForUpdateUnconditional();
+        _keyboardControllerBase.MovieUpdate(false);
 
-        Plugin.Log.LogDebug("check A press 2");
         RuntimeAssert.True(Input.GetKeyDown(KeyCode.A), "keycode down check 2");
 
-        yield return new WaitForUpdateUnconditional();
+        _keyboardControllerBase.OnLastUpdateActual();
+        _keyboardControllerBase.MovieUpdate(false);
 
         RuntimeAssert.False(Input.GetKeyDown(KeyCode.A), "keycode down check 3");
 
-        yield return new WaitForFixedUpdateUnconditional();
+        _keyboardControllerBase.OnLastUpdateActual();
+        _keyboardControllerBase.MovieUpdate(true);
 
         RuntimeAssert.False(Input.GetKeyDown(KeyCode.A), "keycode down check 4");
 
         _keyboardController.Release(_keyFactory.CreateKey(KeyCode.A));
-        _timeEnv.FrameTime = captureFt;
-        Time.fixedDeltaTime = fixedDt;
         _virtualEnvController.RunVirtualEnvironment = false;
     }
 
     [RuntimeTest]
-    public IEnumerator<CoroutineWait> KeyDownTest2()
+    public void KeyDownTest2()
     {
         _virtualEnvController.RunVirtualEnvironment = true;
 
-        var captureFt = _timeEnv.FrameTime;
-        var fixedDt = Time.fixedDeltaTime;
-
-        _timeEnv.FrameTime = 0.01;
-        Time.fixedDeltaTime = 0.02f;
-
-        yield return new WaitForFixedUpdateUnconditional();
-        yield return new WaitForUpdateUnconditional();
-
-        Plugin.Log.LogDebug("press A");
         _keyboardController.Hold(_keyFactory.CreateKey(KeyCode.A));
+        _keyboardControllerBase.MovieUpdate(false);
 
-        yield return new WaitForUpdateUnconditional();
-
-        Plugin.Log.LogDebug("check A press");
         RuntimeAssert.True(Input.GetKeyDown(KeyCode.A), "keycode down check 1");
 
-        yield return new WaitForFixedUpdateUnconditional();
+        _keyboardControllerBase.OnLastUpdateActual();
+        _keyboardControllerBase.MovieUpdate(true);
 
         RuntimeAssert.False(Input.GetKeyDown(KeyCode.A), "keycode down check 2");
 
         _keyboardController.Release(_keyFactory.CreateKey(KeyCode.A));
-        _timeEnv.FrameTime = captureFt;
-        Time.fixedDeltaTime = fixedDt;
         _virtualEnvController.RunVirtualEnvironment = false;
     }
 
     [RuntimeTest]
-    public IEnumerator<CoroutineWait> GetMousePress()
+    public void GetMousePress()
     {
         _virtualEnvController.RunVirtualEnvironment = true;
 
-        yield return new WaitForUpdateUnconditional();
+        RuntimeAssert.False(Input.GetMouseButton(0), "check 1");
 
-        RuntimeAssert.False(Input.GetMouseButton(0));
-
+        _mouseControllerBase.OnLastUpdateActual();
         _mouseController.HoldButton(MouseButton.Left);
+        _mouseControllerBase.MovieUpdate(false);
 
-        yield return new WaitForUpdateUnconditional();
+        RuntimeAssert.True(Input.GetMouseButtonDown(0), "check 2");
+        RuntimeAssert.True(Input.GetMouseButton(0), "check 3");
 
-        RuntimeAssert.True(Input.GetMouseButtonDown(0));
-        RuntimeAssert.True(Input.GetMouseButton(0));
+        _mouseControllerBase.OnLastUpdateActual();
+        _mouseControllerBase.MovieUpdate(false);
 
-        yield return new WaitForUpdateUnconditional();
+        RuntimeAssert.False(Input.GetMouseButtonDown(0), "check 4");
+        RuntimeAssert.True(Input.GetMouseButton(0), "check 5");
 
-        RuntimeAssert.False(Input.GetMouseButtonDown(0));
-        RuntimeAssert.True(Input.GetMouseButton(0));
-
-        yield return new WaitForUpdateUnconditional();
-
+        _mouseControllerBase.OnLastUpdateActual();
         _mouseController.HoldButton(MouseButton.Middle);
+        _mouseControllerBase.MovieUpdate(false);
 
-        yield return new WaitForUpdateUnconditional();
+        RuntimeAssert.True(Input.GetMouseButtonDown(2), "check 6");
+        RuntimeAssert.True(Input.GetMouseButton(2), "check 7");
 
-        RuntimeAssert.True(Input.GetMouseButtonDown(2));
-        RuntimeAssert.True(Input.GetMouseButton(2));
-
-        yield return new WaitForUpdateUnconditional();
-
+        _mouseControllerBase.OnLastUpdateActual();
         _mouseController.ReleaseButton(MouseButton.Left);
         _mouseController.ReleaseButton(MouseButton.Middle);
+        _mouseControllerBase.MovieUpdate(false);
 
-        yield return new WaitForUpdateUnconditional();
+        RuntimeAssert.False(Input.GetMouseButtonDown(2), "check 8");
+        RuntimeAssert.False(Input.GetMouseButton(0), "check 9");
+        RuntimeAssert.True(Input.GetMouseButtonUp(0), "check 10");
+        RuntimeAssert.True(Input.GetMouseButtonUp(2), "check 13");
 
-        RuntimeAssert.False(Input.GetMouseButtonDown(0));
-        RuntimeAssert.False(Input.GetMouseButton(0));
-        RuntimeAssert.True(Input.GetMouseButtonUp(0));
+        _mouseControllerBase.OnLastUpdateActual();
+        _mouseControllerBase.MovieUpdate(false);
 
-        RuntimeAssert.False(Input.GetMouseButtonDown(2));
-        RuntimeAssert.False(Input.GetMouseButton(2));
-        RuntimeAssert.True(Input.GetMouseButtonUp(2));
-
-        yield return new WaitForUpdateUnconditional();
-
-        RuntimeAssert.False(Input.GetMouseButtonUp(0));
-        RuntimeAssert.False(Input.GetMouseButtonUp(2));
+        RuntimeAssert.False(Input.GetMouseButtonUp(0), "check 14");
+        RuntimeAssert.False(Input.GetMouseButtonUp(2), "check 15");
 
         _virtualEnvController.RunVirtualEnvironment = false;
     }
