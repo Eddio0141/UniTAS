3f0d04cbf6d85fb4547756531474396b97284c89
Eddio0141 <eddio0141@gmail.com>
2023-05-17 02:40:36 +0100

    fixed stupid update problem with UI

--- UniTAS/Plugin/Interfaces/VirtualEnvironment/LegacyInputSystemDevice.cs
+++ UniTAS/Plugin/Interfaces/VirtualEnvironment/LegacyInputSystemDevice.cs
@@ -1,13 +1,15 @@
-﻿using UniTAS.Plugin.Interfaces.Events.MonoBehaviourEvents.DontRunIfPaused;
-using UniTAS.Plugin.Interfaces.Events.Movie;
+﻿using UniTAS.Plugin.Interfaces.Events.Movie;
 
 namespace UniTAS.Plugin.Interfaces.VirtualEnvironment;
 
 /// <summary>
 /// Provides a way to create a device that uses legacy input system, or is also used in the new input system.
 /// </summary>
-public abstract class LegacyInputSystemDevice : InputState, IOnLastUpdateActual, IOnMovieUpdate
+public abstract class LegacyInputSystemDevice : InputState, IOnMovieUpdate
 {
+    /// <summary>
+    /// Function invoked when a frame advances.
+    /// </summary>
     protected abstract void Update();
 
     /// <summary>
@@ -16,13 +18,13 @@ public abstract class LegacyInputSystemDevice : InputState, IOnLastUpdateActual,
     /// </summary>
     protected abstract void FlushBufferedInputs();
 
-    public void OnLastUpdateActual()
-    {
-        Update();
-    }
-
     public void MovieUpdate(bool fixedUpdate)
     {
+        if (!fixedUpdate)
+        {
+            Update();
+        }
+
         // this just needs to be flushed before legacy input system input is read so its fine to be pre-updates
         FlushBufferedInputs();
     }
