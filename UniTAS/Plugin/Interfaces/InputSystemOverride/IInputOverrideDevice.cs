abf1a62991c158918b83cfae9758228e2375a032
Eddio0141 <eddio0141@gmail.com>
2023-05-18 02:30:16 +0100

    removed abstract

--- UniTAS/Plugin/Interfaces/InputSystemOverride/IInputOverrideDevice.cs
+++ UniTAS/Plugin/Interfaces/InputSystemOverride/IInputOverrideDevice.cs
@@ -8,10 +8,10 @@ public interface IInputOverrideDevice
     /// <summary>
     /// Update the state of the TAS device
     /// </summary>
-    public abstract void Update();
+    public void Update();
 
     /// <summary>
     /// Called when the device is to be added to the input system
     /// </summary>
-    public abstract void DeviceAdded();
+    public void DeviceAdded();
 }
