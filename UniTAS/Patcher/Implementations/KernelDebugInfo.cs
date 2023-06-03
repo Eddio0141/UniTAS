8a2dd3d0dfda4c09629f364673818340809dbc67
Eddio0141 <eddio0141@gmail.com>
2023-06-03 23:18:17 +0100

    split register info debug print to own

---
+++ UniTAS/Patcher/Implementations/KernelDebugInfo.cs
@@ -0,0 +1,15 @@
+using UniTAS.Patcher.Interfaces.DependencyInjection;
+using UniTAS.Patcher.Services.Logging;
+using UniTAS.Patcher.Utils;
+
+namespace UniTAS.Patcher.Implementations;
+
+[Register]
+[ForceInstantiate]
+public class KernelDebugInfo
+{
+    public KernelDebugInfo(ILogger logger)
+    {
+        logger.LogDebug($"Register info\n{ContainerStarter.Kernel.WhatDoIHave()}");
+    }
+}
