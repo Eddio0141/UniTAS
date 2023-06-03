1aa8cb6db611faed0a83b974f3e4ff27129b584b
Eddio0141 <eddio0141@gmail.com>
2023-06-03 23:10:18 +0100

    cleaned up kernel stuff for init

--- UniTAS/Patcher/Utils/ContainerStarter.cs
+++ UniTAS/Patcher/Utils/ContainerStarter.cs
@@ -1,22 +1,21 @@
-using BepInEx.Configuration;
 using StructureMap;
 using UniTAS.Patcher.Implementations.DependencyInjection;
 using UniTAS.Patcher.Implementations.Logging;
+using UniTAS.Patcher.Interfaces.Invoker;
 using UniTAS.Patcher.Services.DependencyInjection;
 using UniTAS.Patcher.Services.Logging;
 
 namespace UniTAS.Patcher.Utils;
 
-public static class ContainerRegister
+public static class ContainerStarter
 {
-    public static Container Init()
+    public static IContainer Kernel { get; private set; }
+
+    [InvokeOnPatcherFinish]
+    public static void Init()
     {
-        var container = new Container(c =>
+        Kernel = new Container(c =>
         {
-            c.ForSingletonOf<ConfigFile>().Use(_ => Plugin.PluginConfig);
-
-            c.ForSingletonOf<PluginWrapper>().Use<PluginWrapper>();
-
             c.ForSingletonOf<DiscoverAndRegister>().Use<DiscoverAndRegister>();
             c.For<IDiscoverAndRegister>().Use(x => x.GetInstance<DiscoverAndRegister>());
 
@@ -24,11 +23,9 @@ public static class ContainerRegister
             c.For<ILogger>().Use(x => x.GetInstance<Logger>());
         });
 
-        container.Configure(c => container.GetInstance<IDiscoverAndRegister>().Register<PluginWrapper>(c));
+        Kernel.Configure(c => Kernel.GetInstance<IDiscoverAndRegister>().Register<Logger>(c));
 
-        var forceInstantiateTypes = container.GetInstance<IForceInstantiateTypes>();
-        forceInstantiateTypes.InstantiateTypes<Plugin>();
-
-        return container;
+        var forceInstantiateTypes = Kernel.GetInstance<IForceInstantiateTypes>();
+        forceInstantiateTypes.InstantiateTypes<Logger>();
     }
 }
