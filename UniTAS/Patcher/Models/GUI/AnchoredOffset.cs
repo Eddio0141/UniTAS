9d34f93bd265514248d915369d8820d2042baffa
Eddio0141 <eddio0141@gmail.com>
2023-06-09 21:30:01 +0100

    added overlay drawing functionality

---
+++ UniTAS/Patcher/Models/GUI/AnchoredOffset.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace UniTAS.Patcher.Models.GUI;
+
+public struct AnchoredOffset
+{
+    private int AnchorX { get; }
+    private int AnchorY { get; }
+    private int OffsetX { get; }
+    private int OffsetY { get; }
+
+    /// <summary>
+    /// Creates a new AnchoredOffset.
+    /// </summary>
+    /// <param name="anchorX">Anchor X position. 0 is left, 1 is right.</param>
+    /// <param name="anchorY">Anchor Y position. 0 is top, 1 is bottom.</param>
+    /// <param name="offsetX">Offset X position.</param>
+    /// <param name="offsetY">Offset Y position.</param>
+    public AnchoredOffset(int anchorX, int anchorY, int offsetX, int offsetY)
+    {
+        AnchorX = anchorX;
+        AnchorY = anchorY;
+        OffsetX = offsetX;
+        OffsetY = offsetY;
+    }
+
+    /// <summary>
+    /// Gets the actual X position based on screen width.
+    /// </summary>
+    public int X => Screen.width * AnchorX + OffsetX;
+
+    /// <summary>
+    /// Gets the actual Y position based on screen height.
+    /// </summary>
+    public int Y => Screen.height * AnchorY + OffsetY;
+}
