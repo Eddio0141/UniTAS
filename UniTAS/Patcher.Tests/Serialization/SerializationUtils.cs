182349e528d8fa1cea4a35b2282b91cf782b91fd
Eddio0141 <eddio0141@gmail.com>
2023-06-11 17:46:39 +0100

    removed warn

--- UniTAS/Patcher.Tests/Serialization/SerializationUtils.cs
+++ UniTAS/Patcher.Tests/Serialization/SerializationUtils.cs
@@ -5,7 +5,9 @@ public static class SerializationUtils
     public class TestClassWithInts
     {
         public static int Int1 = 1;
-        private static int Int2 = 2;
+#pragma warning disable CS0414
+        private static int _int2 = 2;
+#pragma warning restore CS0414
         public int Int3 = 3;
     }
 }
