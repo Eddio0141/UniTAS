0c8e8e51374e7a0fdb9bedf0bad1e47124cbe289
Eddio0141 <eddio0141@gmail.com>
2023-06-16 00:15:24 +0100

    use of config entries to track offset data

--- UniTAS/Patcher/Interfaces/GUI/BuiltInOverlay.cs
+++ UniTAS/Patcher/Interfaces/GUI/BuiltInOverlay.cs
@@ -2,17 +2,19 @@ using BepInEx.Configuration;
 using UniTAS.Patcher.Interfaces.Events.MonoBehaviourEvents.RunEvenPaused;
 using UniTAS.Patcher.Models.GUI;
 using UniTAS.Patcher.Services;
-using UniTAS.Patcher.Utils;
 
 namespace UniTAS.Patcher.Interfaces.GUI;
 
 public abstract class BuiltInOverlay : IOnUpdateUnconditional
 {
-    private bool Enabled { get; set; }
-    private AnchoredOffset Offset { get; set; }
     protected abstract AnchoredOffset DefaultOffset { get; }
 
-    private readonly ConfigFile _config;
+    private ConfigEntry<int> _anchorX;
+    private ConfigEntry<int> _anchorY;
+    private ConfigEntry<int> _offsetX;
+    private ConfigEntry<int> _offsetY;
+    private ConfigEntry<bool> _enabled;
+
     protected abstract string ConfigValue { get; }
 
     private readonly IOverlayDrawing _overlayDrawing;
@@ -22,21 +24,26 @@ public abstract class BuiltInOverlay : IOnUpdateUnconditional
     protected BuiltInOverlay(IConfig config, IOverlayDrawing overlayDrawing)
     {
         _overlayDrawing = overlayDrawing;
-        _config = config.ConfigFile;
-        Init();
+        Init(config);
     }
 
-    private void Init()
+    private void Init(IConfig config)
     {
-        Offset = ConfigUtils.BindAnchoredOffset(_config, $"BuiltInOverlays.{ConfigValue}", DefaultOffset);
-        Enabled = _config.Bind($"BuiltInOverlays.{ConfigValue}", "Enabled", true).Value;
+        var entry = $"BuiltInOverlays.{ConfigValue}";
+        _anchorX = config.ConfigFile.Bind(entry, "AnchorX", DefaultOffset.AnchorX,
+            "Anchor X position. 0 is left, 1 is right.");
+        _anchorY = config.ConfigFile.Bind(entry, "AnchorY", DefaultOffset.AnchorY,
+            "Anchor Y position. 0 is top, 1 is bottom.");
+        _offsetX = config.ConfigFile.Bind(entry, "OffsetX", DefaultOffset.OffsetX, "Offset X position.");
+        _offsetY = config.ConfigFile.Bind(entry, "OffsetY", DefaultOffset.OffsetY, "Offset Y position.");
+        _enabled = config.ConfigFile.Bind(entry, "Enabled", true);
     }
 
     public void UpdateUnconditional()
     {
-        if (!Enabled) return;
+        if (!_enabled.Value) return;
         Update();
-        _overlayDrawing.DrawText(Offset, Text);
+        _overlayDrawing.DrawText(new(_anchorX.Value, _anchorY.Value, _offsetX.Value, _offsetY.Value), Text);
     }
 
     /// <summary>
