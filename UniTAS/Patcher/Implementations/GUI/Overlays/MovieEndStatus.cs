3a6ef44bf3a47a6c5eccd789514af99896df9c8f
Eddio0141 <eddio0141@gmail.com>
2023-06-16 01:54:06 +0100

    added movie end status

---
+++ UniTAS/Patcher/Implementations/GUI/Overlays/MovieEndStatus.cs
@@ -0,0 +1,36 @@
+using UniTAS.Patcher.Interfaces.DependencyInjection;
+using UniTAS.Patcher.Interfaces.Events.Movie;
+using UniTAS.Patcher.Interfaces.GUI;
+using UniTAS.Patcher.Models.GUI;
+using UniTAS.Patcher.Services;
+using UnityEngine;
+
+namespace UniTAS.Patcher.Implementations.GUI.Overlays;
+
+[Singleton]
+[ForceInstantiate]
+[ExcludeRegisterIfTesting]
+public class MovieEndStatus : BuiltInOverlay, IOnMovieRunningStatusChange
+{
+    private float _messageDisplayLeft;
+    private const float MESSAGE_DISPLAY_TIME = 1f;
+
+    public MovieEndStatus(IConfig config, IOverlayDrawing overlayDrawing) : base(config, overlayDrawing)
+    {
+    }
+
+    protected override AnchoredOffset DefaultOffset => new(1, 1, -80, -20);
+    protected override string ConfigValue => "MovieEndStatus";
+
+    protected override void Update()
+    {
+        _messageDisplayLeft -= Time.deltaTime;
+        Text = _messageDisplayLeft <= 0 ? "" : "Movie End";
+    }
+
+    public void OnMovieRunningStatusChange(bool running)
+    {
+        if (running) return;
+        _messageDisplayLeft = MESSAGE_DISPLAY_TIME;
+    }
+}
