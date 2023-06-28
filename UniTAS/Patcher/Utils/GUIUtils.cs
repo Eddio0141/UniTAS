ae7ddfa3f6cc66511dd187bef3920a65b4b04fe2
Eddio0141 <eddio0141@gmail.com>
2023-06-28 22:59:46 +0100

    method to get middle of screen rect

--- UniTAS/Patcher/Utils/GUIUtils.cs
+++ UniTAS/Patcher/Utils/GUIUtils.cs
@@ -5,4 +5,9 @@ namespace UniTAS.Patcher.Utils;
 public static class GUIUtils
 {
     public static readonly GUILayoutOption[] EmptyOptions = new GUILayoutOption[0];
+
+    public static Rect WindowRect(int width, int height)
+    {
+        return new(Screen.width / 2f - width / 2f, Screen.height / 2f - height / 2f, width, height);
+    }
 }
