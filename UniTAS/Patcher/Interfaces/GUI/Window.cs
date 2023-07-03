18c9ff8bba1255a8e2891d7e03882d9f875855ab
Eddio0141 <eddio0141@gmail.com>
2023-07-03 16:11:17 +0100

    added resizing

--- UniTAS/Patcher/Interfaces/GUI/Window.cs
+++ UniTAS/Patcher/Interfaces/GUI/Window.cs
@@ -17,7 +17,9 @@ public abstract class Window
     private int _windowId;
     private readonly UnityEngine.GUI.WindowFunction _windowUpdate;
     private bool _dragging;
-    private Vector2 _dragOffset;
+    private bool _resizing;
+    private Vector2 _windowClickOffset;
+    private Vector2 _resizeSize;
     private bool _showWindow;
 
     private readonly WindowConfig _config;
@@ -43,6 +45,7 @@ public abstract class Window
     private int ScreenWidth => _patchReverseInvoker.Invoke(() => Screen.width);
     private int ScreenHeight => _patchReverseInvoker.Invoke(() => Screen.height);
     private bool LeftMouseButton => _patchReverseInvoker.Invoke(() => UnityInput.Current.GetMouseButton(0));
+    private bool RightMouseButton => _patchReverseInvoker.Invoke(() => UnityInput.Current.GetMouseButton(1));
 
     private void Init()
     {
@@ -70,7 +73,7 @@ public abstract class Window
 
     private void WindowUpdate(int id)
     {
-        HandleDrag();
+        HandleDragResize();
 
         GUILayout.BeginVertical(GUIUtils.EmptyOptions);
 
@@ -88,19 +91,58 @@ public abstract class Window
 
         OnGUI();
 
-        CheckDrag();
+        CheckDragResize();
+    }
+
+    private void HandleDragResize()
+    {
+        if (_resizing)
+        {
+            HandleResize();
+            return;
+        }
+
+        if (_dragging)
+        {
+            HandleDrag();
+        }
+    }
+
+    private void HandleResize()
+    {
+        // handle resize
+        if (Event.current.type == EventType.Repaint)
+        {
+            var mousePos = MousePosition;
+
+            _windowRect.width = _resizeSize.x + (mousePos.x - _windowRect.x - _windowClickOffset.x);
+            _windowRect.height = _resizeSize.y + (ScreenHeight - mousePos.y - _windowRect.y - _windowClickOffset.y);
+
+            // just in case
+            if (!RightMouseButton)
+            {
+                _resizing = false;
+            }
+
+            ClampWindow();
+
+            return;
+        }
+
+        if (Event.current.type != EventType.layout)
+        {
+            Event.current.Use();
+        }
     }
 
     private void HandleDrag()
     {
-        if (!_dragging) return;
-
         // stupid hack to make window dragging smooth
         if (Event.current.type == EventType.Repaint)
         {
             var mousePos = MousePosition;
-            _windowRect.x = mousePos.x - _dragOffset.x;
-            _windowRect.y = ScreenHeight - mousePos.y - _dragOffset.y;
+            _windowRect.x = mousePos.x - _windowClickOffset.x;
+            _windowRect.y = ScreenHeight - mousePos.y - _windowClickOffset.y;
 
             // just in case
             if (!LeftMouseButton)
@@ -132,30 +174,58 @@ public abstract class Window
         }
 
         var screenWidth = ScreenWidth;
+        if (screenWidth < _windowRect.width)
+        {
+            _windowRect.width = screenWidth;
+        }
+
         if (_windowRect.xMax > screenWidth)
         {
             _windowRect.x = screenWidth - _windowRect.width;
         }
 
         var screenHeight = ScreenHeight;
+        if (screenHeight < _windowRect.height)
+        {
+            _windowRect.height = screenHeight;
+        }
+
         if (_windowRect.yMax > screenHeight)
         {
             _windowRect.y = screenHeight - _windowRect.height;
         }
     }
 
-    private void CheckDrag()
+    private void CheckDragResize()
     {
-        if (_dragging || Event.current.type != EventType.MouseDown) return;
+        if (Event.current.type != EventType.MouseDown) return;
 
-        var mousePos = MousePosition;
-        mousePos.y = ScreenHeight - mousePos.y;
+        if (!_resizing && Event.current.button == 1)
+        {
+            // are we resizing now?
+            var mousePos = MousePosition;
+            mousePos.y = ScreenHeight - mousePos.y;
+
+            if (_windowRect.Contains(mousePos))
+            {
+                _resizing = true;
+                _windowClickOffset = mousePos - new Vector2(_windowRect.x, _windowRect.y);
+                _resizeSize = new(_windowRect.width, _windowRect.height);
+
+                return;
+            }
+        }
+
+        if (_dragging || Event.current.button != 0) return;
+
+        var mousePos2 = MousePosition;
+        mousePos2.y = ScreenHeight - mousePos2.y;
 
         // are we dragging now?
-        if (_windowRect.Contains(mousePos))
+        if (_windowRect.Contains(mousePos2))
         {
             _dragging = true;
-            _dragOffset = mousePos - new Vector2(_windowRect.x, _windowRect.y);
+            _windowClickOffset = mousePos2 - new Vector2(_windowRect.x, _windowRect.y);
         }
     }
 
