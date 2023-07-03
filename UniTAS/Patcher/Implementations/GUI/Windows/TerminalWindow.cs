d53ebd3a2941d2aa6e0f44a7f7579d7baa6a4ec7
Eddio0141 <eddio0141@gmail.com>
2023-07-03 23:08:29 +0100

    changed way of getting bind keys

--- UniTAS/Patcher/Implementations/GUI/Windows/TerminalWindow.cs
+++ UniTAS/Patcher/Implementations/GUI/Windows/TerminalWindow.cs
@@ -1,9 +1,13 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using BepInEx;
 using UniTAS.Patcher.Exceptions.GUI;
 using UniTAS.Patcher.Interfaces.DependencyInjection;
 using UniTAS.Patcher.Interfaces.GUI;
+using UniTAS.Patcher.Models.Customization;
 using UniTAS.Patcher.Models.GUI;
+using UniTAS.Patcher.Services;
+using UniTAS.Patcher.Services.Customization;
 using UniTAS.Patcher.Services.GUI;
 using UniTAS.Patcher.Utils;
 using UnityEngine;
@@ -15,6 +19,7 @@ namespace UniTAS.Patcher.Implementations.GUI.Windows;
 public class TerminalWindow : Window, ITerminalWindow
 {
     public TerminalEntry[] TerminalEntries { get; }
+    private readonly IPatchReverseInvoker _patchReverseInvoker;
 
     private string _terminalOutput = string.Empty;
     private string _terminalInput = string.Empty;
@@ -25,10 +30,17 @@ public class TerminalWindow : Window, ITerminalWindow
 
     private bool _initialFocus = true;
 
-    public TerminalWindow(WindowDependencies windowDependencies, TerminalEntry[] terminalEntries) : base(
+    private readonly Bind _terminalBind;
+
+    public const string TERMINAL_INPUT_BIND_NAME = "NewTerminal";
+
+    public TerminalWindow(WindowDependencies windowDependencies, TerminalEntry[] terminalEntries, IBinds binds) : base(
         windowDependencies,
         new(defaultWindowRect: GUIUtils.WindowRect(Screen.width - 100, Screen.height - 100), windowName: "Terminal"))
     {
+        _patchReverseInvoker = windowDependencies.PatchReverseInvoker;
+        _terminalBind = binds.Get(TERMINAL_INPUT_BIND_NAME);
+
         // check dupes
         var dupes = terminalEntries.GroupBy(x => x.Command).Where(x => x.Count() > 1).Select(x => x.Key).ToArray();
         if (dupes.Any()) throw new DuplicateTerminalEntryException(dupes);
@@ -50,27 +62,17 @@ public class TerminalWindow : Window, ITerminalWindow
 
         GUILayout.BeginHorizontal(GUIUtils.EmptyOptions);
 
-        if (_initialFocus)
-        {
-            UnityEngine.GUI.SetNextControlName("TerminalInput");
-        }
+        UnityEngine.GUI.SetNextControlName("TerminalInput");
 
         _terminalInput = GUILayout.TextField(_terminalInput, _terminalInputOptions);
-
-        // check enter
-        if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return &&
-            UnityEngine.GUI.GetNameOfFocusedControl() == "TerminalInput")
-        {
-            // hold shift to split input
-            Submit(Event.current.shift);
-            Event.current.Use();
-        }
 
         if (_initialFocus)
         {
             _initialFocus = false;
             UnityEngine.GUI.FocusControl("TerminalInput");
         }
+
+        CheckTerminalInputBinds();
 
         if (GUILayout.Button("Submit", _submitOptions))
         {
@@ -79,6 +81,52 @@ public class TerminalWindow : Window, ITerminalWindow
 
         GUILayout.EndHorizontal();
         GUILayout.EndVertical();
+    }
+
+    private bool GetKeyDown(KeyCode keyCode)
+    {
+        return _patchReverseInvoker.Invoke(key => UnityInput.Current.GetKeyDown(key), keyCode);
+    }
+
+    private bool GetKey(KeyCode keyCode)
+    {
+        return _patchReverseInvoker.Invoke(key => UnityInput.Current.GetKey(key), keyCode);
+    }
+
+    private bool _usedInputEvent;
+    private bool _waitForTerminalBindRelease = true;
+
+    private void CheckTerminalInputBinds()
+    {
+        // eat input if used bind
+        if (_usedInputEvent && Event.current.type != EventType.Repaint && Event.current.type != EventType.Layout)
+        {
+            _usedInputEvent = false;
+            Event.current.Use();
+            return;
+        }
+
+        if (UnityEngine.GUI.GetNameOfFocusedControl() != "TerminalInput" ||
+            Event.current.type != EventType.Repaint) return;
+
+        if (GetKeyDown(KeyCode.Return))
+        {
+            // hold shift to split input
+            Submit(GetKey(KeyCode.LeftShift) | GetKey(KeyCode.RightShift));
+            _usedInputEvent = true;
+        }
+
+        if (_terminalBind.IsPressed() && _hijackingEntry == null)
+        {
+            if (_waitForTerminalBindRelease)
+            {
+                _waitForTerminalBindRelease = false;
+                return;
+            }
+
+            Close();
+            _usedInputEvent = true;
+        }
     }
 
     private void Submit(bool split)
