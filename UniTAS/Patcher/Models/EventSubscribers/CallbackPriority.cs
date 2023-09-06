27bda094b37942665cf7b94a056b70e0bac7d6df
Eddio0141 <eddio0141@gmail.com>
2023-09-06 23:51:33 +0100

    UpdateInvokeOffset priority is highest

--- UniTAS/Patcher/Models/EventSubscribers/CallbackPriority.cs
+++ UniTAS/Patcher/Models/EventSubscribers/CallbackPriority.cs
@@ -3,6 +3,7 @@ namespace UniTAS.Patcher.Models.EventSubscribers;
 public enum CallbackPriority
 {
     // the higher you place it, the lower the value, the earlier it gets invoked
+    UpdateInvokeOffset,
     InputUpdateUnconditional,
     FirstUpdateSkipOnRestart,
     Default,
--- UniTAS/Patcher/Utils/UpdateInvokeOffset.cs
+++ UniTAS/Patcher/Utils/UpdateInvokeOffset.cs
@@ -1,4 +1,5 @@
 using UniTAS.Patcher.Interfaces.Invoker;
+using UniTAS.Patcher.Models.EventSubscribers;
 using UniTAS.Patcher.Models.Invoker;
 using UnityEngine;
 
@@ -8,15 +9,26 @@ public static class UpdateInvokeOffset
 {
     public static double Offset { get; private set; }
 
+    private static bool _updated;
+
     [InvokeOnUnityInit(Priority = InvokerPriority.UpdateInvokeOffset)]
     public static void Init()
     {
-        MonoBehaviourEvents.OnUpdateUnconditional += UpdateUnconditionalOffset;
+        // to make sure this is called before any other update events, we register on both Update and InputUpdate
+        MonoBehaviourEvents.UpdatesUnconditional.Add(UpdateOffset, (int)CallbackPriority.UpdateInvokeOffset);
+        InputSystemEvents.InputUpdatesUnconditional.Add((_, _) => UpdateOffset(),
+            (int)CallbackPriority.UpdateInvokeOffset);
+
+        MonoBehaviourEvents.OnLastUpdateUnconditional += () => _updated = false;
     }
 
-    private static void UpdateUnconditionalOffset()
+    private static void UpdateOffset()
     {
+        if (_updated) return;
+        _updated = true;
+
         Offset += Time.deltaTime;
         Offset %= Time.fixedDeltaTime;
+        StaticLogger.Trace($"New update offset: {Offset}, dt: {Time.deltaTime}");
     }
 }
