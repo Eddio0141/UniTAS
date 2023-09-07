3a9615efcec9a99f2c3f49aaf4f0cecf51fb2a82
Eddio0141 <eddio0141@gmail.com>
2023-09-07 22:58:54 +0100

    added help for specific command arg

--- UniTAS/Patcher/Implementations/GUI/TerminalCommands/Help.cs
+++ UniTAS/Patcher/Implementations/GUI/TerminalCommands/Help.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UniTAS.Patcher.Interfaces.GUI;
 using UniTAS.Patcher.Services.GUI;
 
@@ -7,10 +8,25 @@ public class Help : TerminalEntry
 {
     public override string Command => "help";
 
-    public override string Description => "Prints all available commands";
+    public override string Description =>
+        "Prints all available commands. Arg 0 (string) (optional): command to get help for";
 
     public override bool Execute(string[] args, ITerminalWindow terminalWindow)
     {
+        var command = args.Length > 0 ? args[0] : null;
+
+        if (command != null)
+        {
+            // display help for a command
+            var terminalEntry = terminalWindow.TerminalEntries.FirstOrDefault(x => x.Command == command);
+
+            terminalWindow.TerminalPrintLine(terminalEntry == null
+                ? $"Command {command} not found"
+                : $"{terminalEntry.Command} - {terminalEntry.Description}");
+
+            return false;
+        }
+
         var terminalEntries = terminalWindow.TerminalEntries;
         terminalWindow.TerminalPrintLine("Available commands:");
 
