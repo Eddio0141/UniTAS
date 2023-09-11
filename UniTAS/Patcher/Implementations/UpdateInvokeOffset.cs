f2568f19e35d11f13a0ab96165cf69f81f9edc23
Eddio0141 <eddio0141@gmail.com>
2023-09-11 04:45:18 +0100

    use of interface updates instead

--- UniTAS/Patcher/Implementations/UpdateInvokeOffset.cs
+++ UniTAS/Patcher/Implementations/UpdateInvokeOffset.cs
@@ -1,33 +1,38 @@
+using System.Collections.Generic;
 using UniTAS.Patcher.Interfaces.DependencyInjection;
+using UniTAS.Patcher.Interfaces.Events.UnityEvents;
+using UniTAS.Patcher.Interfaces.Events.UnityEvents.RunEvenPaused;
 using UniTAS.Patcher.Models.DependencyInjection;
 using UniTAS.Patcher.Models.EventSubscribers;
+using UniTAS.Patcher.Models.Utils;
 using UniTAS.Patcher.Services;
-using UniTAS.Patcher.Services.UnityEvents;
 using UniTAS.Patcher.Utils;
 using UnityEngine;
 
 namespace UniTAS.Patcher.Implementations;
 
 [Singleton(timing: RegisterTiming.Entry)]
-public class UpdateInvokeOffset : IUpdateInvokeOffset
+public class UpdateInvokeOffset : IUpdateInvokeOffset, IUnityEventPriority, IOnUpdateUnconditional,
+    IOnInputUpdateUnconditional, IOnLateUpdateUnconditional
 {
     public double Offset { get; private set; }
 
     private bool _updated;
 
-    public UpdateInvokeOffset(IUpdateEvents updateEvents)
+    public void UpdateUnconditional()
     {
-        // to make sure this is called before any other update events, we register on both Update and InputUpdate
-        updateEvents.AddPriorityCallback(CallbackUpdate.UpdateUnconditional, UpdateOffset,
-            CallbackPriority.UpdateInvokeOffset);
-        updateEvents.AddPriorityCallback(CallbackInputUpdate.InputUpdateUnconditional, (fixedUpdate, _) =>
-        {
-            if (!fixedUpdate)
-            {
-                UpdateOffset();
-            }
-        }, CallbackPriority.UpdateInvokeOffset);
-        updateEvents.OnLateUpdateUnconditional += () => _updated = false;
+        UpdateOffset();
+    }
+
+    public void InputUpdateUnconditional(bool fixedUpdate, bool newInputSystemUpdate)
+    {
+        if (fixedUpdate) return;
+        UpdateOffset();
+    }
+
+    public void OnLateUpdateUnconditional()
+    {
+        _updated = false;
     }
 
     private void UpdateOffset()
@@ -39,4 +44,10 @@ public class UpdateInvokeOffset : IUpdateInvokeOffset
         Offset %= Time.fixedDeltaTime;
         StaticLogger.Trace($"New update offset: {Offset}, dt: {Time.deltaTime}");
     }
+
+    public Dictionary<Either<CallbackUpdate, CallbackInputUpdate>, CallbackPriority> Priorities { get; } = new()
+    {
+        { new(CallbackUpdate.UpdateUnconditional), CallbackPriority.UpdateInvokeOffset },
+        { new(CallbackInputUpdate.InputUpdateUnconditional), CallbackPriority.UpdateInvokeOffset }
+    };
 }
