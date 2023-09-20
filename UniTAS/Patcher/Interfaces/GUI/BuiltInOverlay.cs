f2e196c3e8cbe9fb6c32c7c77c0acd3b1ac5111c
Eddio0141 <eddio0141@gmail.com>
2023-09-20 23:41:23 +0100

    updated api to make sense

--- UniTAS/Patcher/Interfaces/GUI/BuiltInOverlay.cs
+++ UniTAS/Patcher/Interfaces/GUI/BuiltInOverlay.cs
@@ -18,11 +18,9 @@ public abstract class BuiltInOverlay : IOnUpdateUnconditional, IOverlayVisibleTo
     private ConfigEntry<bool> _enabled;
     private ConfigEntry<int> _fontSize;
 
-    protected abstract string ConfigValue { get; }
+    protected abstract string ConfigName { get; }
 
     private readonly IDrawing _drawing;
-
-    protected string Text { get; set; }
 
     public bool Enabled { get; set; } = true;
 
@@ -34,7 +32,7 @@ public abstract class BuiltInOverlay : IOnUpdateUnconditional, IOverlayVisibleTo
 
     private void Init(IConfig config)
     {
-        var entry = $"BuiltInOverlays.{ConfigValue}";
+        var entry = $"BuiltInOverlays.{ConfigName}";
         _anchorX = config.ConfigFile.Bind(entry, "AnchorX", DefaultOffset.AnchorX,
             "Anchor X position. 0 is left, 1 is right.");
         _anchorY = config.ConfigFile.Bind(entry, "AnchorY", DefaultOffset.AnchorY,
@@ -48,13 +46,14 @@ public abstract class BuiltInOverlay : IOnUpdateUnconditional, IOverlayVisibleTo
     public void UpdateUnconditional()
     {
         if (!Enabled || !_enabled.Value) return;
-        Update();
-        _drawing.PrintText(new(_anchorX.Value, _anchorY.Value, _offsetX.Value, _offsetY.Value), Text,
-            _fontSize.Value);
+        var text = Update();
+        if (text == null) return;
+        _drawing.PrintText(new(_anchorX.Value, _anchorY.Value, _offsetX.Value, _offsetY.Value), text, _fontSize.Value);
     }
 
     /// <summary>
     /// Update that happens before the text is drawn
     /// </summary>
-    protected abstract void Update();
+    /// <returns>Text to draw. Return null to skip drawing.</returns>
+    protected abstract string Update();
 }
