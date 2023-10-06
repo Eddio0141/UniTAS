045e12b097faa77b2b0410d5a7c9f9899012fa44
Eddio0141 <eddio0141@gmail.com>
2023-10-06 18:18:56 +0100

    fixed logic error

--- UniTAS/Patcher/Extensions/FieldInfoExtensions.cs
+++ UniTAS/Patcher/Extensions/FieldInfoExtensions.cs
@@ -10,6 +10,6 @@ public static class FieldInfoExtensions
     public static bool IsFieldUnitySerializable(this FieldInfo field)
     {
         var attrs = field.GetCustomAttributes(true);
-        return (field.IsPublic && attrs.Any(x => x is NonSerializedAttribute)) || attrs.Any(x => x is SerializeField);
+        return (field.IsPublic && !attrs.Any(x => x is NonSerializedAttribute)) || attrs.Any(x => x is SerializeField);
     }
 }
