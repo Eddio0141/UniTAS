4277ce2169890977f31bf4654578ebf5ee475a1a
Eddio0141 <eddio0141@gmail.com>
2023-10-14 08:05:58 +0100

    Revert "cleaned up target dll info to be merged with TargetPatcherDlls"
    This reverts commit 2b89617c208a83cadda3052da27f9e22006297f8.

--- UniTAS/Patcher/Utils/TargetPatcherDlls.cs
+++ UniTAS/Patcher/Utils/TargetPatcherDlls.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using BepInEx;
-using UniTAS.Patcher.Extensions;
 
 namespace UniTAS.Patcher.Utils;
 
@@ -11,40 +10,4 @@ public static class TargetPatcherDlls
     public static IEnumerable<string> AllDLLs =>
         Directory.GetFiles(Paths.ManagedPath, "*.dll", SearchOption.TopDirectoryOnly).Select(Path.GetFileName)
             .ToArray();
-
-
-    public static string[] TargetDllsWithExclusions { get; } = AllDLLs.Where(x =>
-    {
-        var fileWithoutExtension = Path.GetFileNameWithoutExtension(x);
-        return fileWithoutExtension == null ||
-               AssemblyIncludeRaw.Any(a => fileWithoutExtension.Like(a)) ||
-               !AssemblyExclusionsRaw.Any(a => fileWithoutExtension.Like(a));
-    }).ToArray();
-
-    private static string[] AssemblyExclusionsRaw { get; } =
-    {
-        "UnityEngine.*",
-        "UnityEngine",
-        "Unity.*",
-        "System.*",
-        "System",
-        "netstandard",
-        "mscorlib",
-        "Mono.*",
-        "Mono",
-        "MonoMod.*",
-        "BepInEx.*",
-        "BepInEx",
-        "MonoMod.*",
-        "0Harmony",
-        "HarmonyXInterop",
-        "StructureMap",
-        "Newtonsoft.Json"
-    };
-
-    private static string[] AssemblyIncludeRaw { get; } =
-    {
-        "Unity.InputSystem",
-        "UnityEngine.InputModule"
-    };
 }
