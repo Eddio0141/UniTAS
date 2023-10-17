7ad90f0c5e4d6d69b3cf714777b41c15759175e0
Eddio0141 <eddio0141@gmail.com>
2023-10-17 08:28:09 +0100

    cleaned up target dll property

--- UniTAS/Patcher/Utils/TargetPatcherDlls.cs
+++ UniTAS/Patcher/Utils/TargetPatcherDlls.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using BepInEx;
+using UniTAS.Patcher.Extensions;
 
 namespace UniTAS.Patcher.Utils;
 
@@ -10,4 +11,49 @@ public static class TargetPatcherDlls
     public static IEnumerable<string> AllDLLs =>
         Directory.GetFiles(Paths.ManagedPath, "*.dll", SearchOption.TopDirectoryOnly).Select(Path.GetFileName)
             .ToArray();
+
+    // lazy initialization, for some reason directly initializing to the property breaks this
+    private static IEnumerable<string> _allExcludedDLLs;
+
+    public static IEnumerable<string> AllExcludedDLLs
+    {
+        get
+        {
+            _allExcludedDLLs ??= AllDLLs.Where(x =>
+            {
+                var fileWithoutExtension = Path.GetFileNameWithoutExtension(x);
+                return fileWithoutExtension == null ||
+                       AssemblyIncludeRaw.Any(a => fileWithoutExtension.Like(a)) ||
+                       !AssemblyExclusionsRaw.Any(a => fileWithoutExtension.Like(a));
+            });
+            return _allExcludedDLLs;
+        }
+    }
+
+    private static string[] AssemblyExclusionsRaw { get; } =
+    {
+        "UnityEngine.*",
+        "UnityEngine",
+        "Unity.*",
+        "System.*",
+        "System",
+        "netstandard",
+        "mscorlib",
+        "Mono.*",
+        "Mono",
+        "MonoMod.*",
+        "BepInEx.*",
+        "BepInEx",
+        "MonoMod.*",
+        "0Harmony",
+        "HarmonyXInterop",
+        "StructureMap",
+        "Newtonsoft.Json"
+    };
+
+    private static string[] AssemblyIncludeRaw { get; } =
+    {
+        "Unity.InputSystem",
+        "UnityEngine.InputModule"
+    };
 }
