76c0984f3fc78503f5b5962e0c29cbbd375bb64c
Eddio0141 <eddio0141@gmail.com>
2024-07-30 10:15:02 +0100

    restore tracking

--- UniTAS/Patcher/Patches/Harmony/ScriptableObjectPatch.cs
+++ UniTAS/Patcher/Patches/Harmony/ScriptableObjectPatch.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Reflection;
 using HarmonyLib;
+using UniTAS.Patcher.Extensions;
 using UniTAS.Patcher.Interfaces.Patches.PatchTypes;
+using UniTAS.Patcher.Services.Trackers.UpdateTrackInfo;
 using UniTAS.Patcher.Utils;
 using UnityEngine;
 
@@ -14,8 +18,8 @@ namespace UniTAS.Patcher.Patches.Harmony;
 [SuppressMessage("ReSharper", "UnusedMember.Local")]
 public class ScriptableObjectPatch
 {
-    // private static readonly INewScriptableObjectTracker NewScriptableObjectTracker =
-    //     ContainerStarter.Kernel.GetInstance<INewScriptableObjectTracker>();
+    private static readonly INewScriptableObjectTracker NewScriptableObjectTracker =
+        ContainerStarter.Kernel.GetInstance<INewScriptableObjectTracker>();
 
     [HarmonyPatch]
     private class CreateInstanceTrackerString
@@ -35,7 +39,20 @@ public class ScriptableObjectPatch
 
         private static void Postfix(ScriptableObject __result)
         {
-            // NewScriptableObjectTracker.NewScriptableObject(__result);
+            var st = new StackTrace();
+            var frames = st.GetFrames()?.Skip(2)?.ToArray();
+            foreach (var frame in frames)
+            {
+                var method = frame.GetMethod();
+                var declType = method.DeclaringType;
+
+                if (declType.Namespace == "UnityEngine.InputSystem")
+                {
+                    StaticLogger.Log.LogInfo($"ScriptableObject.CreateInstance was invoked from {declType.SaneFullName()}.{method.Name}, skipping tracking this instance");
+                    return;
+                }
+            }
+            NewScriptableObjectTracker.NewScriptableObject(__result);
         }
     }
 }
