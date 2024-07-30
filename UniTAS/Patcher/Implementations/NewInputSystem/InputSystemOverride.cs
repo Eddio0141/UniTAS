1d00eb78a7a7fd7516d7e9b44a5d98a1e53a61d7
Eddio0141 <eddio0141@gmail.com>
2024-07-30 20:05:34 +0100

    immediately connect TAS devices for new input system

--- UniTAS/Patcher/Implementations/NewInputSystem/InputSystemOverride.cs
+++ UniTAS/Patcher/Implementations/NewInputSystem/InputSystemOverride.cs
@@ -5,92 +5,55 @@ using UniTAS.Patcher.Interfaces.InputSystemOverride;
 using UniTAS.Patcher.Services;
 using UniTAS.Patcher.Services.InputSystemOverride;
 using UniTAS.Patcher.Services.Logging;
-using UniTAS.Patcher.Services.Movie;
-using UniTAS.Patcher.Services.NewInputSystem;
 using UniTAS.Patcher.Services.UnityEvents;
+using UniTAS.Patcher.Services.VirtualEnvironment;
 using UnityEngine.InputSystem;
 
 namespace UniTAS.Patcher.Implementations.NewInputSystem;
 
 [Singleton]
 [ForceInstantiate]
-public class InputSystemOverride : IInputSystemOverride
+public class InputSystemOverride
 {
-    private readonly IInputOverrideDevice[] _devices;
-
-    private readonly ILogger _logger;
     private readonly bool _hasInputSystem;
 
-    private bool _override;
+    private readonly IInputOverrideDevice[] _devices;
+    private readonly ILogger _logger;
+    private readonly IVirtualEnvController _virtualEnv;
 
     [SuppressMessage("ReSharper", "ConditionIsAlwaysTrueOrFalse")]
     public InputSystemOverride(ILogger logger, IInputOverrideDevice[] devices,
-        INewInputSystemExists newInputSystemExists, IUpdateEvents updateEvents, IMovieRunnerEvents movieRunnerEvents,
-        IGameRestart gameRestart)
+        IInputSystemState newInputSystemExists, IUpdateEvents updateEvents, IGameRestart gameRestart, IVirtualEnvController virtualEnv)
     {
-        _hasInputSystem = newInputSystemExists.HasInputSystem;
+        _hasInputSystem = newInputSystemExists.HasNewInputSystem;
         logger.LogMessage($"Has unity new input system: {_hasInputSystem}");
 
         if (!_hasInputSystem) return;
 
         _logger = logger;
         _devices = devices;
+        _virtualEnv = virtualEnv;
         updateEvents.OnInputUpdateActual += UpdateDevices;
-        movieRunnerEvents.OnMovieStart += OnMovieStart;
-        movieRunnerEvents.OnMovieEnd += OnMovieEnd;
-    }
 
-    public bool Override
-    {
-        set
+        _logger.LogDebug("Adding TAS devices to InputSystem");
+
+        foreach (var device in _devices)
         {
-            if (!_hasInputSystem) return;
-            _override = value;
-
-            if (value)
-            {
-                _logger.LogDebug("Adding TAS devices to InputSystem");
-
-                foreach (var device in _devices)
-                {
-                    device.AddDevice();
-                }
-
-                _logger.LogDebug("Added TAS devices to InputSystem");
-                _logger.LogDebug(
-                    $"Connected devices:\n{string.Join("\n", InputSystem.devices.Select(x => $"name: {x.name}, type: {x.GetType().FullName}").ToArray())}");
-            }
-            else
-            {
-                _logger.LogDebug($"Removing TAS devices from InputSystem");
-
-                foreach (var device in _devices)
-                {
-                    device.RemoveDevice();
-                }
-
-                _logger.LogDebug("Removed TAS devices from InputSystem");
-            }
+            device.AddDevice();
         }
+
+        _logger.LogDebug("Added TAS devices to InputSystem");
+        _logger.LogDebug(
+            $"Connected devices:\n{string.Join("\n", InputSystem.devices.Select(x => $"name: {x.name}, type: {x.GetType().FullName}").ToArray())}");
     }
 
     private void UpdateDevices(bool fixedUpdate, bool newInputSystemUpdate)
     {
-        if (!newInputSystemUpdate || !_override) return;
+        if (!newInputSystemUpdate || !_virtualEnv.RunVirtualEnvironment) return;
 
         foreach (var device in _devices)
         {
             device.Update();
         }
-    }
-
-    private void OnMovieStart()
-    {
-        Override = true;
-    }
-
-    private void OnMovieEnd()
-    {
-        Override = false;
     }
 }
