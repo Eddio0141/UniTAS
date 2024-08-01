f30e05aa41d70c58f864eead8052fbd38dd046da
Eddio0141 <eddio0141@gmail.com>
2024-08-01 18:38:29 +0100

    mouse delta calculation respects cursor lock state

--- UniTAS/Patcher/Implementations/VirtualEnvironment/InputState/NewInputSystem/MouseStateEnvNewSystem.cs
+++ UniTAS/Patcher/Implementations/VirtualEnvironment/InputState/NewInputSystem/MouseStateEnvNewSystem.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections.Generic;
 using UniTAS.Patcher.Interfaces.DependencyInjection;
+using UniTAS.Patcher.Models.UnitySafeWrappers;
 using UniTAS.Patcher.Models.VirtualEnvironment;
+using UniTAS.Patcher.Services.UnitySafeWrappers.Wrappers;
 using UniTAS.Patcher.Services.VirtualEnvironment.Input.NewInputSystem;
+using UniTAS.Patcher.Utils;
 using UnityEngine;
 
 namespace UniTAS.Patcher.Implementations.VirtualEnvironment.InputState.NewInputSystem;
 
 [Singleton]
-public class MouseStateEnvNewSystem : Interfaces.VirtualEnvironment.InputState, IMouseStateEnvNewSystem
+public class MouseStateEnvNewSystem(ICursorWrapper cursorWrapper) : Interfaces.VirtualEnvironment.InputState, IMouseStateEnvNewSystem
 {
     public Vector2 Position { get; set; }
     public Vector2 Delta { get; private set; }
@@ -29,6 +33,23 @@ public class MouseStateEnvNewSystem : Interfaces.VirtualEnvironment.InputState, 
 
         Delta = Position - _prevPos;
         _prevPos = Position;
+
+        // simulate cursor warping in different cursor lock state
+        // note: i've seen the mouse cursor being able to stay at a non-center coordinate, which is why this operation is done first
+        if (Delta != Vector2.zero && cursorWrapper.LockState == CursorLockMode.Locked)
+        {
+            // div by float, decimal point is possible in mouse cursor pos
+            var center = new Vector2(Screen.width / 2f, Screen.height / 2f);
+            Position = center;
+            _prevPos = center;
+
+            var warpAmount = center - Position;
+            var warpAmountAbs = new Vector2(Math.Abs(warpAmount.x), Math.Abs(warpAmount.y));
+            var deltaAbs = new Vector2(Math.Abs(Delta.x), Math.Abs(Delta.y));
+            if (warpAmountAbs.x <= deltaAbs.x && warpAmountAbs.y <= deltaAbs.y) return;
+
+            Delta += warpAmount;
+        }
     }
 
     public void HoldButton(MouseButton button)
