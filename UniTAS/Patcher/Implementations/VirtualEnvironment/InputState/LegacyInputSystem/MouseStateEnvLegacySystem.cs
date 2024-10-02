d9d548956f74b5ec1e163172a7aa9f5da494dbd4
Eddio0141 <eddio0141@gmail.com>
2024-10-02 05:58:48 +0100

    properly apply screen locking constraints

--- UniTAS/Patcher/Implementations/VirtualEnvironment/InputState/LegacyInputSystem/MouseStateEnvLegacySystem.cs
+++ UniTAS/Patcher/Implementations/VirtualEnvironment/InputState/LegacyInputSystem/MouseStateEnvLegacySystem.cs
@@ -1,6 +1,8 @@
 ﻿using UniTAS.Patcher.Interfaces.DependencyInjection;
 using UniTAS.Patcher.Interfaces.VirtualEnvironment;
+using UniTAS.Patcher.Models.UnitySafeWrappers;
 using UniTAS.Patcher.Models.VirtualEnvironment;
+using UniTAS.Patcher.Services.UnitySafeWrappers.Wrappers;
 using UniTAS.Patcher.Services.VirtualEnvironment.Input.LegacyInputSystem;
 using UnityEngine;
 
@@ -8,17 +10,34 @@ namespace UniTAS.Patcher.Implementations.VirtualEnvironment.InputState.LegacyInp
 
 [Singleton]
 [ExcludeRegisterIfTesting]
-public class MouseStateEnvLegacySystem : LegacyInputSystemButtonBasedDevice<MouseButtonWrap>, IMouseStateEnvLegacySystem
+public class MouseStateEnvLegacySystem(ICursorWrapper cursorWrapper)
+    : LegacyInputSystemButtonBasedDevice<MouseButtonWrap>, IMouseStateEnvLegacySystem
 {
     public bool MousePresent => true;
     public Vector2 Position { get; set; }
+    private Vector2 _prevPosition;
     public Vector2 Scroll { get; set; }
 
     protected override void ResetState()
     {
         base.ResetState();
         Position = Vector2.zero;
+        _prevPosition = Vector2.zero;
         Scroll = Vector2.zero;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        // simulate cursor warping in different cursor lock state
+        // note: i've seen the mouse cursor being able to stay at a non-center coordinate, which is why this operation is done first
+        if (_prevPosition != Position && cursorWrapper.LockState == CursorLockMode.Locked)
+        {
+            Position = new(Screen.width / 2f, Screen.height / 2f);
+        }
+
+        _prevPosition = Position;
     }
 
     public bool IsButtonHeld(MouseButton button)
