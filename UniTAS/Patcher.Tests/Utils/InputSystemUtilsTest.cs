0ea95d2d4d86d9ed727fe6f530792a916aa33ef8
Eddio0141 <eddio0141@gmail.com>
2024-10-17 22:52:19 +0100

    cover both legacy format and new format strings

--- UniTAS/Patcher.Tests/Utils/InputSystemUtilsTest.cs
+++ UniTAS/Patcher.Tests/Utils/InputSystemUtilsTest.cs
@@ -8,11 +8,17 @@ namespace Patcher.Tests.Utils;
 public class InputSystemUtilsTest
 {
     [Fact]
-    public void Number()
+    public void NumberRowParsing()
     {
         for (var i = 0; i < 10; i++)
         {
             InputSystemUtils.KeyStringToKeys($"Digit{i}", out var keyCode, out var newKey);
+            Assert.NotNull(keyCode);
+            Assert.Equal(Enum.Parse(typeof(KeyCode), $"Alpha{i}"), keyCode.Value);
+            Assert.NotNull(newKey);
+            Assert.Equal(Enum.Parse(typeof(Key), $"Digit{i}"), newKey.Value);
+            
+            InputSystemUtils.KeyStringToKeys($"Alpha{i}", out keyCode, out newKey);
             Assert.NotNull(keyCode);
             Assert.Equal(Enum.Parse(typeof(KeyCode), $"Alpha{i}"), keyCode.Value);
             Assert.NotNull(newKey);
