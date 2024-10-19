d94900870accd7df30396b903eb102083dc0f2e8
Eddio0141 <eddio0141@gmail.com>
2024-10-19 05:11:00 +0100

    test alphabet keys

--- UniTAS/Patcher.Tests/Utils/InputSystemUtilsTest.cs
+++ UniTAS/Patcher.Tests/Utils/InputSystemUtilsTest.cs
@@ -17,12 +17,36 @@ public class InputSystemUtilsTest
             Assert.Equal(Enum.Parse(typeof(KeyCode), $"Alpha{i}"), keyCode.Value);
             Assert.NotNull(newKey);
             Assert.Equal(Enum.Parse(typeof(Key), $"Digit{i}"), newKey.Value);
-            
+
             InputSystemUtils.KeyStringToKeys($"Alpha{i}", out keyCode, out newKey);
             Assert.NotNull(keyCode);
             Assert.Equal(Enum.Parse(typeof(KeyCode), $"Alpha{i}"), keyCode.Value);
             Assert.NotNull(newKey);
             Assert.Equal(Enum.Parse(typeof(Key), $"Digit{i}"), newKey.Value);
+        }
+    }
+
+    [Fact]
+    public void AlphabetKeys()
+    {
+        char[] alphaBet =
+        [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'x', 'y', 'z'
+        ];
+
+        foreach (var c in alphaBet)
+        {
+            var correctKeyCode = (KeyCode)Enum.Parse(typeof(KeyCode), char.ToUpper(c).ToString());
+            var correctKey = (Key)Enum.Parse(typeof(Key), char.ToUpper(c).ToString());
+
+            InputSystemUtils.KeyStringToKeys(c.ToString(), out var keyCode, out var newKey);
+            Assert.Equal(correctKeyCode, keyCode);
+            Assert.Equal(correctKey, newKey);
+
+            InputSystemUtils.KeyStringToKeys(char.ToUpper(c).ToString(), out keyCode, out newKey);
+            Assert.Equal(correctKeyCode, keyCode);
+            Assert.Equal(correctKey, newKey);
         }
     }
 }
