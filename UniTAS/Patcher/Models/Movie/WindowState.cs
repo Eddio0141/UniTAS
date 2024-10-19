272d45c88f2de9d168b74b9f3fc91bf4c096e516
Eddio0141 <eddio0141@gmail.com>
2024-10-19 04:48:06 +0100

    removed thingy (literally)

--- UniTAS/Patcher/Models/Movie/WindowState.cs
+++ UniTAS/Patcher/Models/Movie/WindowState.cs
@@ -1,6 +1,5 @@
 using UniTAS.Patcher.Services.UnitySafeWrappers.Wrappers;
 using UniTAS.Patcher.Services.VirtualEnvironment;
-using UniTAS.Patcher.Utils;
 
 namespace UniTAS.Patcher.Models.Movie;
 
@@ -9,7 +8,6 @@ public class WindowState(IResolutionWrapper currentResolution, IResolutionWrappe
     public void SetWindowEnv(IWindowEnv windowEnv)
     {
         windowEnv.CurrentResolution = CurrentResolution;
-        StaticLogger.LogDebug($"thingy: {ExtraResolutions}");
         windowEnv.ExtraSupportedResolutions = ExtraResolutions;
     }
 
