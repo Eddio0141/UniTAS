9e6f5b14dd77db2d89da17df342a46249fb463b8
Eddio0141 <eddio0141@gmail.com>
2024-11-08 23:57:24 +0000

    now game is pretty much running at real-time

--- UniTAS/Patcher/Implementations/MainThreadSpeedController.cs
+++ UniTAS/Patcher/Implementations/MainThreadSpeedController.cs
@@ -1,71 +1,100 @@
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using UniTAS.Patcher.Interfaces.DependencyInjection;
 using UniTAS.Patcher.Interfaces.Events.Movie;
+using UniTAS.Patcher.Interfaces.Events.UnityEvents;
 using UniTAS.Patcher.Interfaces.Events.UnityEvents.RunEvenPaused;
-using UniTAS.Patcher.Services;
 using UniTAS.Patcher.Services.GameExecutionControllers;
 using UniTAS.Patcher.Services.VirtualEnvironment;
-using UnityEngine;
 
 namespace UniTAS.Patcher.Implementations;
 
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 [Singleton]
 [ExcludeRegisterIfTesting]
-public class MainThreadSpeedControl : IMainThreadSpeedControl, IOnUpdateUnconditional, IOnMovieRunningStatusChange
+public class MainThreadSpeedControl(ITimeEnv timeEnv)
+    : IMainThreadSpeedControl, IOnUpdateUnconditional, IOnMovieRunningStatusChange, IOnSceneLoad
 {
-    private readonly ITimeEnv _timeEnv;
-    private readonly IPatchReverseInvoker _patchReverseInvoker;
-
     public float SpeedMultiplier
     {
         get => _speedMultiplier;
         set
         {
             _speedMultiplier = value < 0f ? 0f : value;
-            _lastTime = CurrentTime;
-            _remainingTime = 0f;
+            _actualTime = Stopwatch.StartNew();
+            _gameTime = 0.0;
         }
     }
 
-    private float _lastTime;
+    private Stopwatch _actualTime = Stopwatch.StartNew();
+    private double _gameTime;
     private float _speedMultiplier = 1f;
 
-    private double _remainingTime;
-
-    public MainThreadSpeedControl(IPatchReverseInvoker patchReverseInvoker, ITimeEnv timeEnv)
+    public void OnSceneLoad()
     {
-        _patchReverseInvoker = patchReverseInvoker;
-        _timeEnv = timeEnv;
+        _actualTime = Stopwatch.StartNew();
+        _gameTime = 0.0;
     }
-
-    private float CurrentTime => _patchReverseInvoker.Invoke(() => Time.realtimeSinceStartup);
 
     public void UpdateUnconditional()
     {
         if (_speedMultiplier == 0) return;
 
-        var timeSinceLastUpdate = CurrentTime - _lastTime;
-        _lastTime = CurrentTime;
+        _gameTime += timeEnv.FrameTime / _speedMultiplier;
+        var actualTime = _actualTime.Elapsed.TotalSeconds;
 
         // if the actual time passed is less than the time that should have passed, wait
-        var waitTime = _timeEnv.FrameTime / _speedMultiplier - timeSinceLastUpdate + _remainingTime;
-        if (waitTime <= 0)
+        var waitTime = _gameTime - actualTime;
+
+        if (waitTime > 0)
         {
+            SleepPrecise(waitTime);
+        }
+    }
+
+    /// <summary>
+    /// Precisely sleeps for the specified duration using a hybrid spin-wait approach
+    /// </summary>
+    /// <param name="seconds">The desired sleep duration</param>
+    /// <returns>The actual time slept in milliseconds</returns>
+    private static void SleepPrecise(double seconds)
+    {
+        if (seconds <= 0) return;
+
+        var sw = Stopwatch.StartNew();
+
+        // For very short durations, just spin
+        if (seconds <= 0.001)
+        {
+            while (sw.Elapsed.TotalSeconds < seconds)
+            {
+                Thread.SpinWait(1);
+            }
+
+            sw.Stop();
             return;
         }
 
-        var waitMilliseconds = waitTime * 1000.0;
-        var waitMillisecondsInt = (int)waitMilliseconds;
+        // For longer durations, use a hybrid approach:
+        // 1. Sleep for most of the time
+        // 2. Spin for the remainder
 
-        if (waitMillisecondsInt > 0)
+        var remainingTime = seconds;
+        // Sleep until 1ms before target
+        while (remainingTime > 0.001)
         {
-            Thread.Sleep(waitMillisecondsInt);
+            Thread.Sleep(1);
+            remainingTime = seconds - sw.Elapsed.TotalSeconds;
         }
 
-        // lost time is added to the remaining time
-        _remainingTime = (waitMilliseconds - waitMillisecondsInt) / 1000.0;
+        // Spin for the final sub-millisecond precision
+        while (sw.Elapsed.TotalSeconds < seconds)
+        {
+            Thread.SpinWait(1);
+        }
+
+        sw.Stop();
     }
 
     public void OnMovieRunningStatusChange(bool running)
