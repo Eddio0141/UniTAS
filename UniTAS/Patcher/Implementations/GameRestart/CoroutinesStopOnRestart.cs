ced02b1902ef5ad9eb8f9e2b1ea645d6c3896554
Eddio0141 <eddio0141@gmail.com>
2024-12-26 22:02:32 +0000

    extra logging

--- UniTAS/Patcher/Implementations/GameRestart/CoroutinesStopOnRestart.cs
+++ UniTAS/Patcher/Implementations/GameRestart/CoroutinesStopOnRestart.cs
@@ -1,19 +1,22 @@
 using System.Collections.Generic;
+using UniTAS.Patcher.Extensions;
 using UniTAS.Patcher.Interfaces.DependencyInjection;
 using UniTAS.Patcher.Interfaces.Events.SoftRestart;
+using UniTAS.Patcher.Services.Logging;
 using UniTAS.Patcher.Services.Trackers.UpdateTrackInfo;
 using UnityEngine;
 
 namespace UniTAS.Patcher.Implementations.GameRestart;
 
 [Singleton]
-public class CoroutinesStopOnRestart : ICoroutineRunningObjectsTracker, IOnPreGameRestart
+public class CoroutinesStopOnRestart(ILogger logger) : ICoroutineRunningObjectsTracker, IOnPreGameRestart
 {
     private readonly HashSet<MonoBehaviour> _instances = [];
 
     public void NewCoroutine(MonoBehaviour instance)
     {
         if (instance == null) return;
+        logger.LogDebug($"new coroutine for {instance.GetType().SaneFullName()}");
         _instances.Add(instance);
     }
 
