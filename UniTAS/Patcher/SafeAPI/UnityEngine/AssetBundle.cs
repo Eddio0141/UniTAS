5f4c5fc4ac5c9782da3ee46425b398698009a60c
Eddio0141 <eddio0141@gmail.com>
2024-12-27 21:45:49 +0000

    fixed using the wrong type

--- UniTAS/Patcher/SafeAPI/UnityEngine/AssetBundle.cs
+++ UniTAS/Patcher/SafeAPI/UnityEngine/AssetBundle.cs
@@ -1,11 +1,12 @@
 using System;
 using HarmonyLib;
 using UniTAS.Patcher.Extensions;
+using ue = UnityEngine;
 
 namespace UniTAS.Patcher.SafeAPI.UnityEngine;
 
 public static class AssetBundle
 {
     public static readonly Action<bool> UnloadAllAssetBundles =
-        AccessTools.Method(typeof(AssetBundle), "UnloadAllAssetBundles")?.MethodDelegate<Action<bool>>();
+        AccessTools.Method(typeof(ue.AssetBundle), "UnloadAllAssetBundles")?.MethodDelegate<Action<bool>>();
 }
