58353212769d2afedc2d5902056506720e1c9039
Eddio0141 <eddio0141@gmail.com>
2025-02-12 22:06:29 +0000

    test callback in both ways

--- unitas-rs/src/lib.rs
+++ unitas-rs/src/lib.rs
@@ -1,2 +1,17 @@
+type Callback = extern "C" fn(i32) -> i32;
 
+static mut CALLBACK: Option<Callback> = None;
+
+#[no_mangle]
+pub extern "C" fn set_callback(cb: Callback) {
+    unsafe {
+        CALLBACK = Some(cb);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world() {
+    let value = unsafe { CALLBACK.unwrap()(123) };
+    println!("hello world, {value}");
+}
 
