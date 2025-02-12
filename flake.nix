0886de74fdffaf2f8317ee77f19cf3f3ffe0f075
Eddio0141 <eddio0141@gmail.com>
2025-02-12 22:05:57 +0000

    setup library project

--- flake.nix
+++ flake.nix
@@ -1,21 +1,66 @@
 {
-  description = "A tool that lets you TAS unity games";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, flake-parts, ... } @ inputs:
+  outputs =
+    {
+      flake-parts,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, ... }: {
-        devShells.default =
-          pkgs.mkShell {
-            packages = with pkgs; [
-              dotnet-sdk_8
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem =
+        {
+          inputs',
+          system,
+          pkgs,
+          ...
+        }:
+        let
+          rust-doc = pkgs.writeShellApplication {
+            name = "rust-doc";
+            text = ''
+              xdg-open "${inputs'.fenix.packages.stable.rust-docs}/share/doc/rust/html/index.html"
+            '';
+          };
+
+          rust = pkgs.rust-bin.stable.latest.default;
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (import rust-overlay)
             ];
           };
-      };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              dotnet-sdk_8
+              (rust.override {
+                extensions = [
+                  "rust-analyzer"
+                  "rust-src"
+                ];
+              })
+              rust-doc
+            ];
+          };
+        };
     };
 }
+
