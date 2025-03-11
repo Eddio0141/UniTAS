8af86441f246c88f0ad6a92fc15d4eef7ffa45ba
Eddio0141 <eddio0141@gmail.com>
2025-03-11 02:25:36 +0000

    detours allowed to fail and log fail instead

--- unitas-rs/src/hook.rs
+++ unitas-rs/src/hook.rs
@@ -11,3 +11,24 @@ fn install_detours() {
     #[cfg(unix)]
     hooks::libc::install_detours();
 }
+
+#[macro_export]
+macro_rules! detour_setup_log_fail {
+    ($detour: expr, $before: expr, $after: expr) => {
+        let detour = &$detour;
+        let before = $before;
+        let after = $after;
+        if let Err(err) = unsafe { detour.initialize(before, after) } {
+            log::warn!(
+                "detour: failed to init `{}`, reason: {err:?}",
+                stringify!($detour)
+            );
+        }
+        if let Err(err) = unsafe { detour.enable() } {
+            log::warn!(
+                "detour: failed to enable `{}`, reason: {err:?}",
+                stringify!($detour)
+            );
+        }
+    };
+}
--- unitas-rs/src/hook/hooks/libc/time.rs
+++ unitas-rs/src/hook/hooks/libc/time.rs
@@ -1,24 +1,18 @@
 use libc::*;
 use retour::static_detour;
 
-use crate::hook::hooks::REVERSE_INVOKE;
+use crate::{detour_setup_log_fail, hook::hooks::REVERSE_INVOKE};
 
 static_detour! {
     static clock_gettime_detour: unsafe extern "C" fn(clockid_t, *mut timespec) -> c_int;
 }
 
 pub fn install_detours() {
-    unsafe {
-        clock_gettime_detour
-            .initialize(clock_gettime, |clk_id, tp| {
-                if REVERSE_INVOKE.get() {
-                    return clock_gettime_detour.call(clk_id, tp);
-                }
-                // println!("{clk_id}: {tp:?}");
-                clock_gettime_detour.call(clk_id, tp)
-            })
-            .unwrap()
-            .enable()
-    }
-    .unwrap();
+    detour_setup_log_fail!(clock_gettime_detour, clock_gettime, |clk_id, tp| {
+        if REVERSE_INVOKE.get() {
+            return unsafe { clock_gettime_detour.call(clk_id, tp) };
+        }
+        // println!("{clk_id}: {tp:?}");
+        unsafe { clock_gettime_detour.call(clk_id, tp) }
+    });
 }
