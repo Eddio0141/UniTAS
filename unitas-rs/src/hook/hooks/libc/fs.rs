10e34eda5e8c1038efbfb336eb05e6fa7c85cfd8
yuu0141 <yuu0141@proton.me>
2026-08-09 03:08:24 +0100

    wip

--- unitas-rs/src/hook/hooks/libc/fs.rs
+++ unitas-rs/src/hook/hooks/libc/fs.rs
@@ -10,17 +10,43 @@ static_detour! {
 }
 
 pub fn install_detours() {
-    detour_setup_log_fail!(open_detour, open, |secs| {
-        // let ret = unsafe { sleep_detour.call(secs) };
-        //
-        // if REVERSE_INVOKE.get() {
-        //     return ret;
-        // }
-        //
-        // // update time based on how many seconds was slept
-        // TIME.lock()
-        //     .unwrap()
-        //     .update(Duration::from_secs((secs - ret) as u64));
-        // ret
-    });
+    // detour_setup_log_fail!(open_detour, open, |a, b| {
+    //     todo!()
+    //     // let ret = unsafe { sleep_detour.call(secs) };
+    //     //
+    //     // if REVERSE_INVOKE.get() {
+    //     //     return ret;
+    //     // }
+    //     //
+    //     // // update time based on how many seconds was slept
+    //     // TIME.lock()
+    //     //     .unwrap()
+    //     //     .update(Duration::from_secs((secs - ret) as u64));
+    //     // ret
+    // });
+
+    let detour = &open_detour;
+    let before = open;
+    let after = |a, b| todo!();
+    if let Err(err) = unsafe {
+        detour.initialize(
+            std::mem::transmute::<*const (), unsafe extern "C" fn(*const i8, i32) -> i32>(
+                before as *const (),
+            ),
+            after,
+        )
+    } {
+        log::warn!(
+            "detour: failed to init `{}`, reason: {err:?}",
+            stringify!($detour)
+        );
+    }
+    if let Err(err) = unsafe { detour.enable() } {
+        log::warn!(
+            "detour: failed to enable `{}`, reason: {err:?}",
+            stringify!($detour)
+        );
+    }
 }
+
+extern "C" fn thing(...) {}
